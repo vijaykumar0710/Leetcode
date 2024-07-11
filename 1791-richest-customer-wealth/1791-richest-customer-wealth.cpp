741a12a26a6f62b89ff4552c0919c7ec4a354c8f
Vijay Kumar <vijaykumar20cy@gmail.com>
2024-07-11 14:05:09 +0530

    Time: 3 ms (78.65%), Space: 10.3 MB (12.12%) - LeetHub

--- 1791-richest-customer-wealth/1791-richest-customer-wealth.cpp
+++ 1791-richest-customer-wealth/1791-richest-customer-wealth.cpp
@@ -4,8 +4,8 @@ public:
         int maxisum=0;
         int sum=0;
         for(auto &vec:accounts){
-           for(int i=0;i<vec.size();i++){
-            sum+=vec[i]; 
+           for(int &x:vec){
+            sum+=x; 
            }
            maxisum=max(maxisum,sum);
            sum=0;
