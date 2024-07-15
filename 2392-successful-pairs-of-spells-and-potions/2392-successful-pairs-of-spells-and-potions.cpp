687d61c021125e93180d718f2dde4e00342d20eb
Vijay Kumar <vijaykumar20cy@gmail.com>
2024-07-15 17:57:06 +0530

    Time: 165 ms (21.16%), Space: 103.7 MB (7.72%) - LeetHub

--- 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.cpp
+++ 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.cpp
@@ -1,5 +1,19 @@
 class Solution {
 public:
+int lowerBound(int l,int r,vector<int>& potions,int minpotion){
+    int possibleIndex=-1;
+    int mid=0;
+    while(l<=r){
+        mid=l+(r-l)/2;
+        if(potions[mid]>=minpotion){
+            possibleIndex=mid;
+            r=mid-1;
+        }else{
+            l=mid+1;
+        }
+    }
+    return possibleIndex;
+}
     vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
        int m=spells.size();
        int n=potions.size();
@@ -17,7 +31,7 @@ public:
                 continue;
             }
 
-            int index=lower_bound(begin(potions),end(potions),minpotion)-begin(potions);
+            int index=lowerBound(0,n-1,potions,minpotion);
             int count=n-index;
             answer.push_back(count);
         }
