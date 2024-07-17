926597948d1e423069d8687799fc23040043f2c5
Vijay Kumar <vijaykumar20cy@gmail.com>
2024-07-17 21:09:27 +0530

    Time: 15 ms (81.53%), Space: 16.4 MB (29.82%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
@@ -4,7 +4,7 @@ public:
         int n = graph.size();
         vector<int> color(n, -1);  // -1 means uncolored, 0 and 1 are the two colors
         
-        for (int i = 0; i < n; ++i) {
+        for (int i = 0; i < n; i++) {
             if (color[i] == -1) {  // not yet colored
                 if (!dfs(graph, color, i, 0)) {
                     return false;
