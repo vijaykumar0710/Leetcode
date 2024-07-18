107a78bf2b4ba746eb8194b58c5a7dc53d6f2cac
Vijay Kumar <vijaykumar20cy@gmail.com>
2024-07-18 21:40:37 +0530

    Time: 102 ms (37.94%), Space: 84.9 MB (17.86%) - LeetHub

--- 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.cpp
+++ 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.cpp
@@ -2,19 +2,15 @@ class Solution {
 public:
  int good=0;
  int dfs(TreeNode* node, int maxVal) {
-        if (!node) {
+        if (node==NULL) {
             return 0;
-        }
-        
+        }  
         // Check if the current node is a good node
-       
         if(node->val>=maxVal){
             good++;
         }
-        
         // Update the maximum value on the path to the current node
         maxVal =max(maxVal, node->val);
-        
         // Continue DFS traversal for left and right children
          dfs(node->left, maxVal);
          dfs(node->right, maxVal);
