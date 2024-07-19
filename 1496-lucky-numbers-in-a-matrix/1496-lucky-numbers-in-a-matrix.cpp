033e94f8ddb77fa295515f4b3e25b578b58c22cc
Vijay Kumar <vijaykumar20cy@gmail.com>
2024-07-19 11:01:10 +0530

    Time: 16 ms (56.65%), Space: 14.2 MB (27.66%) - LeetHub

--- 1496-lucky-numbers-in-a-matrix/1496-lucky-numbers-in-a-matrix.cpp
+++ 1496-lucky-numbers-in-a-matrix/1496-lucky-numbers-in-a-matrix.cpp
@@ -1,35 +1,28 @@
 class Solution {
 public:
     vector<int> luckyNumbers (vector<vector<int>>& matrix) {
-        int m = matrix.size();
-        int n = matrix[0].size();
-        vector<int> result;
-        
-        for(int i = 0; i < m; i++) {
-            // Find the minimum element in the ith row
-            int minRow = matrix[i][0];
-            int colIndex = 0;
-            for(int j = 1; j < n; j++) {
-                if(matrix[i][j] < minRow) {
-                    minRow = matrix[i][j];
-                    colIndex = j;
-                }
+        int m=matrix.size();
+        int n=matrix[0].size();
+
+        int rMinMax=INT_MIN;
+        for(int row=0;row<m;row++){
+            int rMin=INT_MAX;
+            for(int col=0;col<n;col++){
+                rMin=min(rMin,matrix[row][col]);
             }
-            
-            // Check if the minimum element in the ith row is the maximum in its column
-            bool isLucky = true;
-            for(int k = 0; k < m; k++) {
-                if(matrix[k][colIndex] > minRow) {
-                    isLucky = false;
-                    break;
-                }
-            }
-            
-            if(isLucky) {
-                result.push_back(minRow);
-            }
+            rMinMax=max(rMinMax,rMin);
         }
-        
-        return result;
+        int cMaxMin=INT_MAX;
+        for(int col=0;col<n;col++){
+            int cMax=INT_MIN;
+            for(int row=0;row<m;row++){
+                cMax=max(cMax,matrix[row][col]);
+            }
+            cMaxMin=min(cMaxMin,cMax);
+        }
+        if(rMinMax==cMaxMin){
+             return {cMaxMin};
+        }
+       return {};
     }
 };
