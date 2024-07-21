260f120d62d26e3fc6c9650387bf6f2cdce1e09d
Vijay Kumar <vijaykumar20cy@gmail.com>
2024-07-21 23:18:01 +0530

    Time: 94 ms (75.49%), Space: 61.3 MB (74.08%) - LeetHub

--- 2472-build-a-matrix-with-conditions/2472-build-a-matrix-with-conditions.cpp
+++ 2472-build-a-matrix-with-conditions/2472-build-a-matrix-with-conditions.cpp
@@ -1,43 +1,41 @@
 class Solution {
 public:
-void dfs(int u,vector<int> &visited,unordered_map<int,vector<int>>& adj,stack<int>& st,bool& cycle){
-visited[u]=1; // dfs is ongoing from this node
-for(int &v:adj[u]){
-    if(visited[v]==0){
-        dfs(v,visited,adj,st,cycle);
-    }else if(visited[v]==1){
-         cycle=true;
-         return;
-    }
-  }
-  visited[u]=2; //visited
-  st.push(u);
-}
 vector<int> topoSort(vector<vector<int>>& edges,int &n){
     unordered_map<int,vector<int>> adj;
+    vector<int> indegree(n+1,0);
     for(vector<int> &edge:edges){
         int u=edge[0];
         int v=edge[1];
         adj[u].push_back(v);
+        indegree[v]++;
     }
-    vector<int> visited(n+1,0);
-    stack<int> st;
-    vector<int> order;
-    bool cycle=false;
-    for(int i=1;i<=n;i++){
-        if(visited[i]==0){
-            dfs(i,visited,adj,st,cycle);
-            if(cycle==true){
-                return {};
-            }
+   queue<int> que;
+   int count=0;
+   for(int i=1;i<=n;i++){
+    if(indegree[i]==0){
+        que.push(i);
+        count++;
+    }
+   }
+   vector<int> topoOrder;
+   while(!que.empty()){
+    int u=que.front();
+    que.pop();
+    topoOrder.push_back(u);
+    for(int &v: adj[u]){
+        indegree[v]--;
+        if(indegree[v]==0){
+            que.push(v);
+            count++;
         }
+      }
     }
-    while(!st.empty()){
-        order.push_back(st.top());
-        st.pop();
+    if(count!=n){
+       return {};
+       }
+       return topoOrder;
     }
-    return order;
-}
+    
     vector<vector<int>> buildMatrix(int k, vector<vector<int>>& rowConditions, vector<vector<int>>& colConditions) {
         vector<int> topoRow=topoSort(rowConditions,k);
         vector<int> topoCol=topoSort(colConditions,k);
