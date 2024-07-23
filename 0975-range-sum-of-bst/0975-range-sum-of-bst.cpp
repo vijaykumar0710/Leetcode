3134d2ed61000fc1ca79d4fbb9b42c872ebe62d9
Vijay Kumar <vijaykumar20cy@gmail.com>
2024-07-23 16:41:07 +0530

    Time: 75 ms (95.63%), Space: 63.2 MB (16.9%) - LeetHub

--- 0975-range-sum-of-bst/0975-range-sum-of-bst.cpp
+++ 0975-range-sum-of-bst/0975-range-sum-of-bst.cpp
@@ -7,8 +7,8 @@ public:
         if(root->val>=low && root->val<=high){
             sum+=root->val;
         }
-         rangeSumBST(root->left,low,high);
-            rangeSumBST(root->right,low,high);
+            if(root->val>=low) rangeSumBST(root->left,low,high);
+           if(root->val<=high) rangeSumBST(root->right,low,high);
             return sum;
     }
 };
