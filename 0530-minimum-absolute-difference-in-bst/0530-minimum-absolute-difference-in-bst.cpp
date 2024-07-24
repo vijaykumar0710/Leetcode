35d1821f1607a00638821ea54a7d03d0ec0ba8aa
Vijay Kumar <vijaykumar20cy@gmail.com>
2024-07-24 09:30:37 +0530

    Time: 8 ms (89.06%), Space: 23.9 MB (52.5%) - LeetHub

--- 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.cpp
+++ 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.cpp
@@ -1,34 +1,19 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    void helper(TreeNode* root,vector<int>& vec){
+  int minDiff=INT_MAX;
+    void helper(TreeNode* root,TreeNode* &prev){
       if(root==NULL) return;
-        helper(root->left,vec);
-        vec.push_back(root->val);
-        helper(root->right,vec);
+        helper(root->left,prev);
+        if(prev!=NULL){
+     minDiff=min(minDiff,root->val-prev->val);
+        }
+        prev=root;
+        helper(root->right,prev);
     }
    
     int getMinimumDifference(TreeNode* root) {
-     vector<int>vec;
-     helper(root,vec);
-
-     int n=vec.size();
-     int minSum=INT_MAX;
-
-     for(int i=1;i<n;i++){
-       int diff=abs(vec[i]-vec[i-1]);
-       minSum=min(minSum,diff);
-  }
-  return minSum;
+     TreeNode* prev=NULL;
+     helper(root,prev);
+      return minDiff;
     }
 };
