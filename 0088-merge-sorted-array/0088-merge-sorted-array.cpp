1c494d4ab1be8da4b8036117d79f6bc6af7c6b03
Vijay Kumar <vijaykumar20cy@gmail.com>
2024-08-04 02:21:37 +0530

    Time: 6 ms (7.62%), Space: 11.8 MB (9.99%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        priority_queue<int,vector<int>,greater<int>>pq;
+        for(int i=0;i<m;i++){
+            pq.push(nums1[i]);
+        }
+         for(int i=0;i<n;i++){
+            pq.push(nums2[i]);
+        }
+        for(int i=0;i<m+n;i++){
+            nums1[i]=pq.top();
+            pq.pop();
+        }
+    }
+};
