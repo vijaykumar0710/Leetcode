3531ec3351ae16d4ada4e4e7f91cea2f10ff6fb4
Vijay Kumar <vijaykumar20cy@gmail.com>
2024-08-04 16:09:49 +0530

    Time: 70 ms (88.54%), Space: 11.4 MB (83.82%) - LeetHub

--- 1615-range-sum-of-sorted-subarray-sums/1615-range-sum-of-sorted-subarray-sums.cpp
+++ 1615-range-sum-of-sorted-subarray-sums/1615-range-sum-of-sorted-subarray-sums.cpp
@@ -1,20 +1,31 @@
 class Solution {
 public:
+    typedef pair<int,int> p;
+    int M=1e9+7;
     int rangeSum(vector<int>& nums, int n, int left, int right) {
-        vector<int>vec;
-        for(int i=0;i<n;i++){
-            int sum=0;
-            for(int j=i;j<n;j++){
-                sum+=nums[j];
-                vec.push_back(sum);
-            }
-        }
-        sort(begin(vec),end(vec));
-        int ans=0;
-        for(int i=left-1;i<right;i++){
-            ans+=vec[i];
-            ans=ans%(1000000007);
-        }
-        return ans;
+         priority_queue<p,vector<p>,greater<p>>pq;
+         for(int i=0;i<n;i++){
+            pq.push({nums[i],i});
+         }
+         int result=0;
+         for(int count=1;count<=right;count++){
+        auto p=pq.top();
+        pq.pop();
+
+        int sum=p.first;
+        int idx=p.second;
+
+        if(count>=left){
+            result=(result+sum)%M;
+             }
+             int new_idx=idx+1;
+            //   p new_pair;
+             if(new_idx<n){  
+                // new_pair.first=sum+nums[new_idx];
+                // new_pair.second=new_idx;
+                pq.push({sum+nums[new_idx],new_idx});
+             }
+         }
+         return result;
     }
 };
