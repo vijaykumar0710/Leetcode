80b1d872e427039a5c13ec61485e6c19d50bde6b
Vijay Kumar <vijaykumar20cy@gmail.com>
2024-08-05 01:56:34 +0530

    Time: 24 ms (49.71%), Space: 79.9 MB (9.47%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.cpp
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
-    ListNode* reverseLL(ListNode* head){
-        if(!head || !head->next){
-            return head;
+       ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        stack<int> st1,st2;
+        while(l1!=NULL){
+            st1.push(l1->val);
+            l1=l1->next;
         }
-        ListNode* last=reverseLL(head->next);
-        head->next->next=head;
-        head->next=NULL;
-        return last;
-    }
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        l1=reverseLL(l1);
-        l2=reverseLL(l2);
+        while(l2!=NULL){
+            st2.push(l2->val);
+            l2=l2->next;
+        }
        int sum=0;
        int carry=0;
        ListNode* ans=new ListNode();
-       while(l1!=NULL || l2!=NULL){
-        if(l1){
-            sum+=l1->val;
-            l1=l1->next;
+       while(!st1.empty() || !st2.empty()){
+        if(!st1.empty()){
+            sum+=st1.top();
+            st1.pop();
         }
-        if(l2){
-            sum+=l2->val;
-            l2=l2->next;
+        if(!st2.empty()){
+            sum+=st2.top();
+            st2.pop();
          }
          ans->val=sum%10;
          carry=sum/10;
