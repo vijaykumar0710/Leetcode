fcdb8503febf606ba81ba0b281eb07323d0f1291
Vijay Kumar <vijaykumar20cy@gmail.com>
2024-08-08 20:09:53 +0530

    Time: 3 ms (36.75%), Space: 8.3 MB (22.26%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> res(numRows);
-        
-        for(int i = 0; i < numRows; i++) {
-            res[i].resize(i + 1);  // Resize the vector to hold i+1 elements
-            res[i][0] = res[i][i] = 1;  // The first and last elements are always 1
-            
-            for(int j = 1; j < i; j++) {
-                // Each element is the sum of the two elements above it
-                res[i][j] = res[i-1][j-1] + res[i-1][j];
+        vector<vector<int>> vec;
+        for(int i=0;i<numRows;i++){
+            vector<int>a(i+1);
+            vec.push_back(a);
+        }
+        for(int i=0;i<numRows;i++){
+            for(int j=0;j<=i;j++){
+                if(j==0 || i==j){
+                    vec[i][j]=1;
+                }
+                else{
+                    vec[i][j]=vec[i-1][j-1]+vec[i-1][j];
+                }
             }
         }
-        
-        return res;
+        return vec;
     }
 };
