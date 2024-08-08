97609bca3c4f903441d952c5175c5946d886b335
Vijay Kumar <vijaykumar20cy@gmail.com>
2024-08-09 01:38:48 +0530

    Time: 2 ms (49.55%), Space: 11.2 MB (34.33%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,16 +1,29 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        priority_queue<int,vector<int>,greater<int>>pq;
-        for(int i=0;i<m;i++){
-            pq.push(nums1[i]);
+        // Pointers to the end of initialized elements
+        int p1 = m - 1;
+        int p2 = n - 1;
+        // Pointer to the end of nums1
+        int p = m + n - 1;
+
+        // Iterate until we have elements in nums2 to process
+        while (p1 >= 0 && p2 >= 0) {
+            if (nums1[p1] > nums2[p2]) {
+                nums1[p] = nums1[p1];
+                p1--;
+            } else {
+                nums1[p] = nums2[p2];
+                p2--;
+            }
+            p--;
         }
-         for(int i=0;i<n;i++){
-            pq.push(nums2[i]);
-        }
-        for(int i=0;i<m+n;i++){
-            nums1[i]=pq.top();
-            pq.pop();
+
+        // If there are remaining elements in nums2, copy them
+        while (p2 >= 0) {
+            nums1[p] = nums2[p2];
+            p--;
+            p2--;
         }
     }
 };
