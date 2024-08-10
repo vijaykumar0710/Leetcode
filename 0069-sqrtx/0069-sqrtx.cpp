fd8eaee49ebea98e286501b9379eb0ad5cc954cf
Vijay Kumar <vijaykumar20cy@gmail.com>
2024-08-11 01:23:33 +0530

    Time: 1 ms (51.64%), Space: 8.4 MB (9.31%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,6 +1,14 @@
 class Solution {
 public:
     int mySqrt(int x) {
-     return pow(x,0.5);   
+      int l=0;
+      int r=x;
+      while(l<=r){
+        long long mid=l+(r-l)/2;
+        if(mid*mid==x) return mid;
+        else if(mid*mid>x) r=mid-1;
+        else if(mid*mid<x) l=mid+1;
+      }
+      return l-1;
     }
 };
