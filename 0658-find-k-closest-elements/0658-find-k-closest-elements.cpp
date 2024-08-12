135821123417d1f6ad2f4bd419afcaab17fa5f71
Vijay Kumar <vijaykumar20cy@gmail.com>
2024-08-12 18:30:36 +0530

    Time: 31 ms (60.41%), Space: 34.8 MB (52.58%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -1,19 +1,46 @@
 class Solution {
 public:
-typedef pair<int,int> pi;
+    int inArray(vector<int>& arr, int x) {
+        int l = 0, r = arr.size() - 1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (arr[mid] == x) {
+                return mid;
+            } else if (arr[mid] > x) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return -1;
+    }
+
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        priority_queue<pi> pq;
-        for(int ele:arr){
-            int distance=abs(x-ele);
-            pq.push({distance,ele});
-            if(pq.size()>k) pq.pop();
+        int n = arr.size();
+        int idx = inArray(arr, x);
+
+        if (idx == -1) {
+            int left = 0, right = n - 1;
+            while (right - left >= k) {
+                if (abs(arr[left] - x) > abs(arr[right] - x)) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+            return vector<int>(arr.begin() + left, arr.begin() + right + 1);
+        } else {
+            int left = idx - 1, right = idx + 1;
+            vector<int> res = {arr[idx]};
+            while (res.size() < k) {
+           if (left >= 0 && (right >= n || abs(arr[left] - x) <= abs(arr[right] - x))) {
+                    res.push_back(arr[left--]);
+                } else {
+                    res.push_back(arr[right++]);
+                }
+            }
+            sort(res.begin(), res.end());
+            return res;
         }
-        vector<int> ans;
-        while(pq.size()>0){
-            ans.push_back(pq.top().second);
-            pq.pop();
-        }
-        sort(ans.begin(),ans.end());
-        return ans;
     }
 };
