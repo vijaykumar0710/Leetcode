e887bc3558cc8106b37b4e7f6f736cf30f14fbc4
Vijay Kumar <vijaykumar20cy@gmail.com>
2024-08-15 13:15:33 +0530

    Time: 0 ms (100%), Space: 8 MB (40.79%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     int lengthOfLastWord(string s) {
         int n=s.length();
-        //if(n==1) return 1;
         int i=n-1;
         int sum=0;
         while(s[i]!=' '|| sum==0){
