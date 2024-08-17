45befdf364aa7f3ded145af8ca11750f21965c70
Vijay Kumar <vijaykumar20cy@gmail.com>
2024-08-17 20:41:53 +0530

    Time: 19 ms (100%), Space: 32.9 MB (100%) - LeetHub

--- 3522-find-the-power-of-k-size-subarrays-i/3522-find-the-power-of-k-size-subarrays-i.cpp
+++ 3522-find-the-power-of-k-size-subarrays-i/3522-find-the-power-of-k-size-subarrays-i.cpp
@@ -2,27 +2,23 @@ class Solution {
 public:
     vector<int> resultsArray(vector<int>& nums, int k) {
         int n = nums.size();
-        vector<int> res;
-
+        vector<int> res(n - k + 1, -1);
+        
         for (int i = 0; i <= n - k; i++) {
             bool isConseSort = true;
-            int maxElement = nums[i];
-
-            for (int j = i + 1; j < i + k; j++) {
-                if (nums[j] != nums[j - 1] + 1) {
+            
+            for (int j = i; j < i + k-1 ; j++) {
+                if (nums[j] + 1 != nums[j + 1]) {
                     isConseSort = false;
                     break;
                 }
-                maxElement = max(maxElement, nums[j]);
             }
-
+            
             if (isConseSort) {
-                res.push_back(maxElement);
-            } else {
-                res.push_back(-1);
+                res[i] = nums[i + k - 1];
             }
         }
-
+        
         return res;
     }
 };
