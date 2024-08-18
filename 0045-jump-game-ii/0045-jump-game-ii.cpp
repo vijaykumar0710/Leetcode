b9348b8c810c1345321952c41ca9c190fe9d0322
Vijay Kumar <vijaykumar20cy@gmail.com>
2024-08-18 22:54:40 +0530

    Time: 7 ms (87.92%), Space: 19.2 MB (60.82%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -3,7 +3,7 @@ public:
     int jump(vector<int>& nums) {
         int n=nums.size();
         int jumps=0;
-         int l=0,r=0;
+        int l=0,r=0;
          while(r<n-1){
             int farthest_go=0;
             for(int i=l;i<=r;i++){
