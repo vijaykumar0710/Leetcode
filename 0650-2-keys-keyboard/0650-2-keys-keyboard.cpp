ad591d1bc7f3a61f4d07f1d79df635125c238aa9
Vijay Kumar <vijaykumar20cy@gmail.com>
2024-08-19 18:53:17 +0530

    Time: 17 ms (31.47%), Space: 11.9 MB (25.96%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+int t[1001][1001];
 int solve(int currA,int clipA,int n){
     if(currA==n){
         return 0;
@@ -7,11 +8,13 @@ int solve(int currA,int clipA,int n){
     if(currA>n){
         return 100000;
     }
+     if(t[currA][clipA]!=-1) return t[currA][clipA];
     int copyALLpaste=1+1+solve(currA+currA,currA,n);
     int paste=1+solve(currA+clipA,clipA,n);
-    return min(paste,copyALLpaste);
+    return t[currA][clipA]=min(paste,copyALLpaste);
 }
     int minSteps(int n) {
+        memset(t,-1,sizeof(t));
         if(n==1) return 0;
         return 1+solve(1,1,n);
     }
