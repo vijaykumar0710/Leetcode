5b3eab8d0f9c0607cd6d53ab5b4484a62b0cfeab
Vijay Kumar <vijaykumar20cy@gmail.com>
2024-08-20 08:43:23 +0530

    Time: 2 ms (69.05%), Space: 10.1 MB (42.25%) - LeetHub

--- 0909-stone-game/0909-stone-game.cpp
+++ 0909-stone-game/0909-stone-game.cpp
@@ -1,6 +1,18 @@
 class Solution {
 public:
     bool stoneGame(vector<int>& piles) {
-        return true;
+        int n=piles.size();
+        priority_queue<int>pq;
+        for(auto &pile:piles){
+          pq.push(pile);
+        }
+        int alice=0,bob=0;
+        while(!pq.empty()){
+             alice+=pq.top();
+             pq.pop();
+             bob+=pq.top();
+             pq.pop();
+        }
+        return alice>bob;
     }
 };
