fb382632eaef52735b0852e7e440893ef72a7c12
Vijay Kumar <vijaykumar20cy@gmail.com>
2024-08-22 16:33:10 +0530

    Time: 3 ms (41.07%), Space: 7.5 MB (57.13%) - LeetHub

---
+++ 0476-number-complement/0476-number-complement.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+  string comp="";
+    int findComplement(int num) {
+        while(num>=1){ 
+       if(num%2==1) comp+='1';
+       else{
+        comp+='0';
+       }
+       num=num/2; 
+        }
+        reverse(begin(comp),end(comp));
+
+        long long n=comp.length();
+        long long sum=0;
+        long long p=1;
+        for(int i=n-1;i>=0;i--){
+         if(comp[i]=='0'){
+            sum+=p;
+         }else{
+            sum+=0;
+         }
+          p=p*2;
+        }
+        return sum;
+    }
+};
