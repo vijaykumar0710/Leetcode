49ef82a306b74171c91923f8a8e3f9c5ac0db973
Vijay Kumar <vijaykumar20cy@gmail.com>
2024-08-22 22:47:34 +0530

    Time: 3 ms (42.31%), Space: 7.7 MB (30.25%) - LeetHub

--- 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
@@ -2,6 +2,12 @@ class Solution {
 public:
     int minBitFlips(int start, int goal) {
         int n=start^goal;
-        return __builtin_popcount(n);
+        int count=0;
+        for(int i=0;i<32;i++){
+            if((n&(1<<i))!=0){
+                count++;
+            }
+        }  
+        return count;
     }
 };
