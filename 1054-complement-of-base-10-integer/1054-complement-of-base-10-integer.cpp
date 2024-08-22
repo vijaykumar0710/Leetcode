1eff83a9c021dc48dbd58ddaaf4c3c7b3e8dbbab
Vijay Kumar <vijaykumar20cy@gmail.com>
2024-08-22 16:37:21 +0530

    Time: 0 ms (100%), Space: 7.4 MB (60.58%) - LeetHub

---
+++ 1054-complement-of-base-10-integer/1054-complement-of-base-10-integer.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+  string comp="";
+    int bitwiseComplement(int num) {
+        if(num==0) return 1;
+        while(num>=1){ 
+       if(num%2==1) comp+='1';
+       else{
+        comp+='0';
+       }
+       num=num/2; 
+        }
+        reverse(begin(comp),end(comp));
+
+        long long n=comp.length();
+        long long sum=0;
+        long long p=1;
+        for(int i=n-1;i>=0;i--){
+         if(comp[i]=='0'){
+            sum+=p;
+         }else{
+            sum+=0;
+         }
+          p=p*2;
+        }
+        return sum;
+    }
+};
