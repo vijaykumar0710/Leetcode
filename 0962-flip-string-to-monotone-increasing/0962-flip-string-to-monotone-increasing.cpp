a0d2c8a9862887e8cbd8fa4ed09b182e23ddcd04
Vijay Kumar <vijaykumar20cy@gmail.com>
2024-08-25 22:37:01 +0530

    Time: 60 ms (25.93%), Space: 21 MB (38.85%) - LeetHub

---
+++ 0962-flip-string-to-monotone-increasing/0962-flip-string-to-monotone-increasing.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+int n;
+int t[100001][2];
+int solve(string &s,int currIdx,int prev){
+   if(currIdx>=n) return 0;
+   int flip=INT_MAX;
+   int no_flip=INT_MAX;
+   if(t[currIdx][prev]!=-1) return t[currIdx][prev];
+   if(s[currIdx]=='0'){
+    if(prev==0){ 
+     flip=1+solve(s,currIdx+1,1);
+     no_flip=solve(s,currIdx+1,0);
+    }else{
+         flip=1+solve(s,currIdx+1,1);
+     }
+   }
+    else{
+       if(prev==0){
+         flip=1+solve(s,currIdx+1,0);
+         no_flip=solve(s,currIdx+1,1);
+       }else{
+         no_flip=solve(s,currIdx+1,1);
+     }
+   }
+   return t[currIdx][prev]=min(flip,no_flip);
+}
+    int minFlipsMonoIncr(string s) {
+        n=s.length();
+        memset(t,-1,sizeof(t));
+        return solve(s,0,0);
+    }
+};
