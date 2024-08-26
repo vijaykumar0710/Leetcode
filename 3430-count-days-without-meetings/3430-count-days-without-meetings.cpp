b9020e16c630c29f27fc353c49467ec3ac1a45eb
Vijay Kumar <vijaykumar20cy@gmail.com>
2024-08-26 22:53:28 +0530

    Time: 331 ms (47.02%), Space: 126.2 MB (56.05%) - LeetHub

--- 3430-count-days-without-meetings/3430-count-days-without-meetings.cpp
+++ 3430-count-days-without-meetings/3430-count-days-without-meetings.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     int countDays(int days, vector<vector<int>>& meetings) {
-        if (meetings.empty()) return days; // If there are no meetings, all days are free
+        if (meetings.empty()) return days; 
         
-        // Sort meetings by starting day
         sort(meetings.begin(), meetings.end());
         
         int freeDays = 0;
@@ -12,21 +11,14 @@ public:
         for (auto& meeting : meetings) {
             int start = meeting[0];
             int end = meeting[1];
-            
-            // Count the days between the end of the last meeting and the start of the current one
             if (start > currentEnd) {
                 freeDays += start - currentEnd - 1;
             }
-            
-            // Update the end of the current meeting range
             currentEnd = max(currentEnd, end);
         }
-        
-        // Count the days after the last meeting
         if (currentEnd < days) {
             freeDays += days - currentEnd;
         }
-        
         return freeDays;
     }
 };
