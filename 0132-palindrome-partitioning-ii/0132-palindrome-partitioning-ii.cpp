8a5e9807e50703bd5959304318e003a448ffcef0
Vijay Kumar <vijaykumar20cy@gmail.com>
2024-08-29 01:59:42 +0530

    Time: 1533 ms (41.11%), Space: 7.9 MB (80.52%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -8,21 +8,22 @@ bool isPalindrome(int i,int j,string &s){
     }
     return true;
 } 
-int f(int i,int n,string &s,vector<int>&t){
-  if(i==n) return 0;
-  int min_partition=INT_MAX;
-  if(t[i]!=-1) return t[i];
-  for(int j=i;j<n;j++){
-    if(isPalindrome(i,j,s)){
-     int partition=1+f(j+1,n,s,t);
-     min_partition=min(min_partition,partition);
-    }
-  }
-  return t[i]=min_partition;
-}
     int minCut(string s) {
-        int n=s.length();
-       vector<int>t(n,-1);
-        return f(0,n,s,t)-1;
+        int n=s.size();
+       vector<int>t(n+1,0);
+       t[n]=0;
+      for(int i=n-1;i>=0;i--){
+      
+      int min_partition=INT_MAX; 
+
+      for(int j=i;j<n;j++){
+      if(isPalindrome(i,j,s)){
+      int partition=1+t[j+1];
+      min_partition=min(min_partition,partition);
+      }
+    }
+     t[i]=min_partition;
+       }
+        return t[0]-1;
     }
 };
