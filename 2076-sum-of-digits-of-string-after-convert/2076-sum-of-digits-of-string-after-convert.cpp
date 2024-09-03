7a1dd2d5cfc4c53952ec581c0c6b9a04f52865c7
Vijay Kumar <vijaykumar20cy@gmail.com>
2024-09-03 08:53:04 +0530

    Time: 3 ms (51.28%), Space: 10.1 MB (21.06%) - LeetHub

---
+++ 2076-sum-of-digits-of-string-after-convert/2076-sum-of-digits-of-string-after-convert.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int getLucky(string s, int k) {
+        int n=s.length();
+        string numstr="";
+        for(auto &ch:s){
+            numstr+=to_string(ch-'a'+1);
+        }
+        int sum=0;
+            while(k>0){
+                sum=0;
+            for(auto ch:numstr){
+               sum+=ch-'0';
+            }
+            k--;
+            numstr=to_string(sum);
+        }
+        return sum;
+    }
+};
