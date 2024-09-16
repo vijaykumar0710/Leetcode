be5c9f28fa48e46cfd2c523d4ba9df0d36f33231
Vijay Kumar <vijaykumar20cy@gmail.com>
2024-09-17 01:38:17 +0530

    Time: 3 ms (41.21%), Space: 7.4 MB (69.09%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -1,18 +1,25 @@
 class Solution {
 public:
+    int getNext(int n) {
+        int sum = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            sum += digit * digit;
+            n /= 10;
+        }
+        return sum;
+    }
+
     bool isHappy(int n) {
-        unordered_set<int>sta;
-        while(n!=1){ 
-        if(sta.count(n)) return false;
-        sta.insert(n);
-        int sum=0;
-        while(n>0){
-            int digit=n%10;
-            sum+=digit*digit;
-            n=n/10;
+        int slow = n;
+        int fast = getNext(n);
+        
+        // Use two pointers, fast moves twice as fast as slow
+        while (fast != 1 && slow != fast) {
+            slow = getNext(slow);          // Move slow pointer one step
+            fast = getNext(getNext(fast)); // Move fast pointer two steps
         }
-        n=sum;
-        }
-        return true;
+        
+        return fast == 1; // If fast pointer reaches 1, n is a happy number
     }
 };
