48c667fee77f5c759d7a7706572ec397f57c2efd
Vijay Kumar <vijaykumar20cy@gmail.com>
2024-09-17 09:32:37 +0530

    Time: 0 ms (100%), Space: 8.8 MB (72.35%) - LeetHub

---
+++ 0920-uncommon-words-from-two-sentences/0920-uncommon-words-from-two-sentences.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<string> uncommonFromSentences(string s1, string s2) {
+        vector<string>res;
+        unordered_map<string,int>mp;
+        string word;
+
+        stringstream ss1(s1);
+            while(ss1>>word){
+                mp[word]++;
+            }
+            stringstream ss2(s2);
+                while(ss2>>word){
+                    mp[word]++;
+            }
+        for(auto &ch:mp){
+          if(ch.second==1){
+            res.push_back(ch.first);
+          }
+        }
+        return res;
+    }
+};
