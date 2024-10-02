b9233c8db6b6a99e2b23a05c4c7d51607bb2543e
Vijay Kumar <vijaykumar20cy@gmail.com>
2024-10-02 12:50:48 +0530

    Time: 0 ms (100%), Space: 7.8 MB (60.89%) - LeetHub

--- 2128-reverse-prefix-of-word/2128-reverse-prefix-of-word.cpp
+++ 2128-reverse-prefix-of-word/2128-reverse-prefix-of-word.cpp
@@ -1,14 +1,7 @@
 class Solution {
 public:
     string reversePrefix(string word, char ch) {
-        int idx=-1;
-        for(int i=0;i<word.size();i++){
-            if(word[i]==ch){
-              idx=i;
-              break;
-            }
-        }
-        reverse(word.begin(),word.begin()+idx+1);
+        reverse(word.begin(),word.begin()+word.find(ch)+1);
         return word;
     }
 };
