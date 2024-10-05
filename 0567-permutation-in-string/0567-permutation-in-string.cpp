2518ace4a035ebd1fe2a21cf36462384bed999e1
Vijay Kumar <vijaykumar20cy@gmail.com>
2024-10-05 12:42:13 +0530

    Time: 2509 ms (5.06%), Space: 123.7 MB (5.45%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        sort(s1.begin(),s1.end());
+        for(int i=0;i<s2.length();i++){
+         string str=s2.substr(i,s1.length());
+         sort(str.begin(),str.end());
+          if(s1==str)  return true;
+        }
+        return false;
+    }
+};
