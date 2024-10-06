771d5d86dddac1f9fe42d8dc17678adf1528aa93
Vijay Kumar <vijaykumar20cy@gmail.com>
2024-10-06 13:02:02 +0530

    Time: 4 ms (40.09%), Space: 8.6 MB (35.48%) - LeetHub

--- 1923-sentence-similarity-iii/1923-sentence-similarity-iii.cpp
+++ 1923-sentence-similarity-iii/1923-sentence-similarity-iii.cpp
@@ -4,31 +4,28 @@ public:
      if(sentence1.length()<sentence2.length()){
         swap(sentence1,sentence2);
      }
-        vector<string>tokens1;
+        deque<string>deq1;
         stringstream ss1(sentence1);
         string token1;
        while(getline(ss1,token1,' ')){
-        tokens1.push_back(token1);
+        deq1.push_back(token1);
        }
 
-       vector<string>tokens2;
+       deque<string>deq2;
        stringstream ss2(sentence2);
        string token2;
         while(getline(ss2,token2,' ')){
-            tokens2.push_back(token2);
+            deq2.push_back(token2);
         }
 
-          int n1=tokens1.size();
-          int n2=tokens2.size();
-          int i=0,j=n1-1,k=0,l=n2-1;
-          while(k<n2 && i<n1 && tokens1[i]==tokens2[k]){
-                 i++;
-                 k++;
+          while(!deq1.empty() && !deq2.empty() && deq1.front()==deq2.front()){
+            deq1.pop_front();
+            deq2.pop_front();
           }
-            while(l>=k && tokens1[j]==tokens2[l]){ 
-                    l--;
-                    j--;
-                }
-          return l<k;
+          while(!deq1.empty() && !deq2.empty() && deq1.back()==deq2.back()){
+            deq1.pop_back();
+            deq2.pop_back();
+          }
+          return deq2.empty();
       }
 };
