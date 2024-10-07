a119765e56e251c90cd44eba58409e8c1224b115
Vijay Kumar <vijaykumar20cy@gmail.com>
2024-10-07 09:25:17 +0530

    Time: 8 ms (42.58%), Space: 11.4 MB (76.7%) - LeetHub

---
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minLength(string s) {
+        stack<char>st;
+        int i=0;
+        while(i<s.size()){
+            if(!st.empty()){
+                if(st.top()=='A' && s[i]=='B'){
+                    st.pop();
+                }
+                else if(st.top()=='C' && s[i]=='D'){
+                    st.pop();
+                 }else{
+                    st.push(s[i]);
+                 }
+            }
+            else{
+                st.push(s[i]);
+            }
+            i++;
+        }
+        return st.size();
+    }
+};
