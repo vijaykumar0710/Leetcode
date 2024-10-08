84df0fec086b46a14fe319509536c2a7772fbc14
Vijay Kumar <vijaykumar20cy@gmail.com>
2024-10-08 08:47:08 +0530

    Time: 6 ms (24.96%), Space: 15.2 MB (60.94%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -7,7 +7,7 @@ void generate(vector<string>&ans,string str,int open,int close,int n){
     }
     if(open<n) generate(ans,str+'(',open+1,close,n);
     if(open>close) generate(ans,str+')',open,close+1,n);
-}
+    }
     vector<string> generateParenthesis(int n) {
         vector<string>ans;
         generate(ans,{},0,0,n);
