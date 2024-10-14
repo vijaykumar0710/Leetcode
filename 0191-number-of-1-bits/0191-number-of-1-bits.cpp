7eedd7da015fb08a4605b4674ce1830719b6f0be
Vijay Kumar <vijaykumar20cy@gmail.com>
2024-10-15 00:13:00 +0530

    Time: 0 ms (100%), Space: 7.8 MB (77.07%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -1,12 +1,5 @@
 class Solution {
 public:
-// void helper(int n,int set){
-//     if(n==0){
-//         return;
-//     }
-//     set+=n%2;
-//     helper(n/2,set);
-// }
 int set=0;
     int hammingWeight(int n) {
         if(n==0) return 0;
