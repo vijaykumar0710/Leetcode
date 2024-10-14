18e28f0599e0ba93b8c6afbc95d68f2a92a64c52
Vijay Kumar <vijaykumar20cy@gmail.com>
2024-10-15 00:55:10 +0530

    Time: 4 ms (30.7%), Space: 8 MB (34.48%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -1,10 +1,6 @@
 class Solution {
 public:
-int set=0;
     int hammingWeight(int n) {
-        if(n==0) return 0;
-        set+=n%2;
-        hammingWeight(n/2);
-        return set;
+        return __builtin_popcount(n);
     }
 };
