576b980a4c37fdd41922b9bb7f81d6fa4ba19984
Vijay Kumar <vijaykumar20cy@gmail.com>
2024-10-15 09:53:01 +0530

    Time: 33 ms (53.47%), Space: 15.7 MB (14.11%) - LeetHub

--- 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.cpp
+++ 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.cpp
@@ -3,10 +3,10 @@ public:
     long long minimumSteps(string s) {
         long long swaps=0;
         int n=s.length();
-        int zeros=0;
-        for(int i=n-1;i>=0;i--){
-            if(s[i]=='0') zeros++;
-            if(s[i]=='1') swaps+=zeros;
+        int ones=0;
+        for(int i=0;i<n;i++){
+            if(s[i]=='1') ones++;
+            if(s[i]=='0') swaps+=ones;
         }
         return swaps;
     }
