97a4cd74cd43909f2a50cf5d0e7ae256d2e4b884
Vijay Kumar <vijaykumar20cy@gmail.com>
2024-10-17 11:42:04 +0530

    Time: 3 ms (38.18%), Space: 7.4 MB (89.76%) - LeetHub

---
+++ 0670-maximum-swap/0670-maximum-swap.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    typedef pair<int, int> p;
+    
+    int maximumSwap(int num) {
+        string st = to_string(num);
+        int n = st.size();
+        vector<int> last(10, -1);  // To store the last occurrence of each digit
+        
+        // Record the last occurrence of each digit
+        for (int i = 0; i < n; i++) {
+            last[st[i] - '0'] = i;
+        }
+        
+        // Traverse through each digit and try to find a larger digit in the remaining part
+        for (int i = 0; i < n; i++) {
+            for (int d = 9; d > st[i] - '0'; d--) {
+                if (last[d] > i) {
+                    // If a larger digit exists later, swap and return
+                    swap(st[i], st[last[d]]);
+                    return stoi(st);  // Convert the modified string back to an integer
+                }
+            }
+        }
+        
+        return num;  // If no swap is done, return the original number
+    }
+};
