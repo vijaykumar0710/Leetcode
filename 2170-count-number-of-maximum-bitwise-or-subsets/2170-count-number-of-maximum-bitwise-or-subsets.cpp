4d2cb66ea58a91445ce41a0c5d63839b03887cee
Vijay Kumar <vijaykumar20cy@gmail.com>
2024-10-18 12:30:24 +0530

    Time: 10 ms (83.65%), Space: 10.2 MB (31.38%) - LeetHub

--- 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -1,33 +1,22 @@
 class Solution {
 public:
+int countSubsets(int idx,int currOr,vector<int>&nums,int maxim){
+    if(idx==nums.size()){
+        if(currOr==maxim) return 1;
+        return 0;
+    }
+    int take=countSubsets(idx+1,currOr | nums[idx],nums,maxim);
+    int notTake=countSubsets(idx+1,currOr,nums,maxim);
+
+    return take+notTake;
+}
     int countMaxOrSubsets(vector<int>& nums) {
         int n=nums.size();
         int maxim=nums[0];
         for(auto &num:nums){
           maxim=(maxim | num);
         }
-        int subsets=1<<n;
-        vector<vector<int>>res;
-        for(int num=0;num<subsets;num++){
-          vector<int>vec;
-          for(int i=0;i<n;i++){
-            if((num &(1<<i))){
-                vec.push_back(nums[i]);
-            }
-          }
-          res.push_back(vec);
-        }
-
-          int count=0;
-        for(int vec=1;vec<res.size();vec++){
-            int biti=res[vec][0];
-          for(auto &num:res[vec]){
-              biti=(biti | num);
-           }
-          if(biti==maxim){
-            count++;
-          }
-        }
-        return count;
+       int currOr=0;
+        return countSubsets(0,currOr,nums,maxim);
     }
 };
