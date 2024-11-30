c2670fb86f5f739f83a9f6b9b93b25d1beb060ff
Vijay Kumar <vijaykumar20cy@gmail.com>
2024-11-30 12:22:58 +0530

    Time: 0 ms (100%), Space: 8.9 MB (25.04%) - LeetHub

--- 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.cpp
+++ 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.cpp
@@ -3,12 +3,13 @@ public:
     bool checkIfPangram(string sentence) {
         vector<int>counter(26,0);
         int n=sentence.size();
+        int count=0;
         for(int i=0;i<n;i++){
+            if(counter[sentence[i]-'a']==0){ 
             counter[sentence[i]-'a']++;
+            count++;
+            }
         }
-        for(auto &num:counter){
-            if(num==0) return false;
-        }
-        return true;
+       return count==26;
     }
 };
