85533a72e9014a3f65bddfeb4b236c482ca14655
Vijay Kumar <vijaykumar20cy@gmail.com>
2024-12-02 20:00:45 +0530

    Time: 0 ms (100%), Space: 18.1 MB (6.65%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -2,10 +2,12 @@ class Solution {
 public:
 int m,n;
     void solve(vector<vector<int>>& image, int sr, int sc, int color,int initial){
+
         if(sr<0 || sc<0 || sr>=m || sc>=n || image[sr][sc]==color) return;
-        // visited[sr][sc]=color;
+
         if(image[sr][sc]==initial){
          image[sr][sc]=color;
+
          solve(image,sr+1,sc,color,initial);
          solve(image,sr-1,sc,color,initial);
          solve(image,sr,sc+1,color,initial);
@@ -13,10 +15,12 @@ int m,n;
         }
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+
          m=image.size();
          n=image[0].size();
-        // vector<vector<int>> visited(m,vector<int>(n,0));
+
         int initial=image[sr][sc];
+
          solve(image,sr,sc,color,initial);
          return image;
     }
