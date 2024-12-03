f2b430f69cb55d19c3c11039fcebb98dd6470605
Vijay Kumar <vijaykumar20cy@gmail.com>
2024-12-03 22:06:55 +0530

    Time: 100 ms (32.76%), Space: 50.7 MB (38.43%) - LeetHub

--- 1112-find-words-that-can-be-formed-by-characters/1112-find-words-that-can-be-formed-by-characters.cpp
+++ 1112-find-words-that-can-be-formed-by-characters/1112-find-words-that-can-be-formed-by-characters.cpp
@@ -1,30 +1,25 @@
 class Solution {
 public:
     int countCharacters(vector<string>& words, string chars) {
-        unordered_map<char, int> charFreq;
-        for (char ch : chars) {
-            charFreq[ch]++;
+        unordered_map<char,int>mp;
+        for(auto &ch:chars){
+            mp[ch]++;
         }
-
-        int totalLength = 0;
-        for (string& word : words) {
-            unordered_map<char, int> tempFreq = charFreq;
-            bool canForm = true;
-            
-            for (char ch : word) {
-                if (tempFreq[ch] > 0) {
-                    tempFreq[ch]--;
-                } else {
-                    canForm = false;
-                    break;
-                }
-            }
-
-            if (canForm) {
-                totalLength += word.size();
-            }
+        int sum=0;
+        for(auto &word:words){
+            unordered_map<char,int> temp=mp;
+            int n=word.size();
+            bool ok=true;
+            for(auto &ch:word){ 
+            if(temp[ch]>0){
+                temp[ch]--;
+             }else{
+                ok=false;
+                break;
+             }
+           }
+           if(ok==true) sum+=n;
         }
-
-        return totalLength;
+        return sum;
     }
 };
