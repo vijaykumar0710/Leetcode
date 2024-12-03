cf0185f8cd400a33952793ee168f1c40b092d4dc
Vijay Kumar <vijaykumar20cy@gmail.com>
2024-12-03 13:32:44 +0530

    Time: 2 ms (25.16%), Space: 9.2 MB (23.3%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        string newS=s+t;
         char rem=0;
-        for(auto &ch:newS){
-             rem=rem^ch;
+        for(auto &ch:s){
+             rem^=ch;
+        }
+        for(auto &ch:t){
+             rem^=ch;
         }
         return rem;
     }
