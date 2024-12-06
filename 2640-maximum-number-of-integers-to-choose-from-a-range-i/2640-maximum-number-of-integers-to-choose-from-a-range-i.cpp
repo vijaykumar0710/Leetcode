98b416aefe3aee8d2ab4adec2a2a4971c1d03b54
Vijay Kumar <vijaykumar20cy@gmail.com>
2024-12-06 09:19:02 +0530

    Time: 182 ms (63.36%), Space: 177.8 MB (67.05%) - LeetHub

---
+++ 2640-maximum-number-of-integers-to-choose-from-a-range-i/2640-maximum-number-of-integers-to-choose-from-a-range-i.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxCount(vector<int>& banned, int n, int maxSum) {
+        int sum=0;
+        int maxint=0;
+        unordered_set<int>st;
+        for(auto &num:banned){
+            st.insert(num);
+        }
+        int i=1;
+        while(i<=n){
+            if(st.find(i)==st.end() && sum<=maxSum){
+                sum+=i;
+                if(sum<=maxSum){ 
+                maxint++;
+                }
+            }
+            i++;
+        }
+        return maxint;
+    }
+};
