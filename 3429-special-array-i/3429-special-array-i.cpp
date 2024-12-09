5a7bef042a0686fe0efa7940a673852b29010d5d
Vijay Kumar <vijaykumar20cy@gmail.com>
2024-12-09 09:37:32 +0530

    Time: 0 ms (100%), Space: 27.7 MB (6.87%) - LeetHub

--- 3429-special-array-i/3429-special-array-i.cpp
+++ 3429-special-array-i/3429-special-array-i.cpp
@@ -3,18 +3,10 @@ public:
     bool isArraySpecial(vector<int>& nums) {
         int n=nums.size();
         if(n==1) return true;
-        bool flag=true;
-        if(nums[0]%2!=0){
-            flag=false;
-        }
-        if(flag==true){
-            for(int i=1;i<n;i++){
-                if(i%2!=0 && nums[i]%2==0) return false;       
-            }
-        }else{
-            for(int i=1;i<n;i++){
-               if(i%2!=0 && nums[i]%2==0) return false;
-            }
+        int XOR=nums[0];
+        for(int i=1;i<n;i++){
+          if((XOR^nums[i])%2==0) return false;
+          XOR=nums[i];
         }
         return true;
     }
