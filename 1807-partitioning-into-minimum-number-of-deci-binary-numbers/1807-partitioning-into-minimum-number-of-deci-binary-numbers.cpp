f5ed36f734dd01c415b67febf8ce3b1a6118475a
Vijay Kumar <vijaykumar20cy@gmail.com>
2024-12-10 21:12:58 +0530

    Time: 6 ms (20.65%), Space: 16.5 MB (11.85%) - LeetHub

--- 1807-partitioning-into-minimum-number-of-deci-binary-numbers/1807-partitioning-into-minimum-number-of-deci-binary-numbers.cpp
+++ 1807-partitioning-into-minimum-number-of-deci-binary-numbers/1807-partitioning-into-minimum-number-of-deci-binary-numbers.cpp
@@ -4,8 +4,7 @@ public:
        int size=n.length();
        int deci_binary=0;
        for(int i=0;i<size;i++){
-         int num=n[i]-'0';
-          deci_binary=max(deci_binary,num); 
+          deci_binary=max(deci_binary,n[i]-'0'); 
        }
        return deci_binary;
     }
