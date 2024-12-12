0f44f857c865db12d78781d2459692b35a9fa899
Vijay Kumar <vijaykumar20cy@gmail.com>
2024-12-12 07:52:22 +0530

    Time: 5 ms (19.36%), Space: 13.1 MB (78.35%) - LeetHub

--- 2692-take-gifts-from-the-richest-pile/2692-take-gifts-from-the-richest-pile.cpp
+++ 2692-take-gifts-from-the-richest-pile/2692-take-gifts-from-the-richest-pile.cpp
@@ -3,7 +3,7 @@ public:
     long long pickGifts(vector<int>& gifts, int k) {
         int n=gifts.size();
         priority_queue<long>pq;
-        long no_gifts=0;
+        long rem_gifts=0;
         for(auto &gift:gifts){
             pq.push(gift);
         }
@@ -13,9 +13,9 @@ public:
             pq.push(x);
         }
         while(!pq.empty()){
-            no_gifts+=pq.top();
+            rem_gifts+=pq.top();
             pq.pop();
         }
-        return no_gifts;
+        return rem_gifts;
     }
 };
