6381e71ab5bf24d9f620dfff5275f6e84dbd8b26
Vijay Kumar <vijaykumar20cy@gmail.com>
2024-12-16 08:50:30 +0530

    Time: 143 ms (55.31%), Space: 95 MB (44.03%) - LeetHub

--- 2254-check-if-every-row-and-column-contains-all-numbers/2254-check-if-every-row-and-column-contains-all-numbers.cpp
+++ 2254-check-if-every-row-and-column-contains-all-numbers/2254-check-if-every-row-and-column-contains-all-numbers.cpp
@@ -4,18 +4,20 @@ public:
         int n=matrix.size();
         for(int i=0;i<n;i++){
             unordered_set<int>row;
+             unordered_set<int>col;
             for(int j=0;j<n;j++){
                  row.insert(matrix[i][j]);
+                  col.insert(matrix[j][i]);
             }
-            if(row.size()!=n) return false;
+            if(row.size()!=n || col.size()!=n) return false;
         }
-        for(int i=0;i<n;i++){
-            unordered_set<int>col;
-            for(int j=0;j<n;j++){
-                 col.insert(matrix[j][i]);
-            }
-            if(col.size()!=n) return false;
-        }
+        // for(int i=0;i<n;i++){
+        //     unordered_set<int>col;
+        //     for(int j=0;j<n;j++){
+        //          col.insert(matrix[j][i]);
+        //     }
+        //     if(col.size()!=n) return false;
+        // }
         return true;
     }
 };
