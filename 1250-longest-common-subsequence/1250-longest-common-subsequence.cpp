acbce2394a54a0767d002b400d2d70c126fa86ca
Vijay Kumar <vijaykumar20cy@gmail.com>
2024-12-17 19:48:41 +0530

    Time: 30 ms (39.74%), Space: 12.8 MB (80.63%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -1,41 +1,20 @@
 class Solution {
 public:
-    int longestCommonSubsequence(string s1, string s2) {
-       int m=s1.length();
-       int  n=s2.length();
-      vector<vector<int>> t(m+1,vector<int>(n+1));
-      for(int row=0;row<m+1;row++){
-        t[row][0]=0;
+    int m,n;
+    int t[1001][1001];
+    int solver(string &s1,string &s2,int i,int j){
+        if(i>=m || j>=n) return 0;
+        if(t[i][j]!=-1) return t[i][j];
+        if(s1[i]==s2[j]){
+            return t[i][j]=1+solver(s1,s2,i+1,j+1);
+         }
+            return t[i][j]=max(solver(s1,s2,i+1,j),solver(s1,s2,i,j+1));
       }
-      for(int col=0;col<n+1;col++){
-        t[0][col]=0;
-      }
-    for(int i=1;i<m+1;i++){
-        for(int j=1;j<n+1;j++){
-            if(s1[i-1]==s2[j-1]){
-                t[i][j]=1+t[i-1][j-1];
-            }else{
-                t[i][j]=max(t[i-1][j],t[i][j-1]);
-            }
-        }
-      }
-      string lcs="";
-      int i=m,j=n;
-      while(i>0 && j>0){
-        if(s1[i-1]==s2[j-1]){
-            lcs.push_back(s1[i-1]);
-            i--;
-            j--;
-        }else{
-            if(t[i-1][j]>t[i][j-1]){
-                i--;
-            }else{
-                j--;
-            }
-        }
-      }
-      reverse(begin(lcs),end(lcs));
-      cout<<lcs<<endl;
-      return t[m][n];
+    int longestCommonSubsequence(string text1, string text2) {
+         m=text1.size();
+         n=text2.size();
+
+        memset(t,-1,sizeof(t));
+        return solver(text1,text2,0,0);
     }
 };
