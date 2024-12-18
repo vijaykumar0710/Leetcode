b97801ac325f83015e39b2f1cf8958f02ae9ff4e
Vijay Kumar <vijaykumar20cy@gmail.com>
2024-12-18 09:07:59 +0530

    Time: 0 ms (100%), Space: 11.9 MB (33.9%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
@@ -1,17 +1,11 @@
 class Solution {
 public:
-int solver(TreeNode* root,int num){
-    if(!root) return 0;
-     num=num*10+root->val;
-     if(root->right==NULL && root->left==NULL){
-       return num;    
-    }
-    int leftNum=solver(root->left,num);
-    int rightNum=solver(root->right,num);
-
-    return leftNum+rightNum;
-}
-    int sumNumbers(TreeNode* root) {
-        return solver(root,0);
+    int sumNumbers(TreeNode* root,int num=0) {
+        if(!root) return 0;
+        num=num*10+root->val;
+         if(root->right==NULL && root->left==NULL){
+          return num;    
+        }
+       return sumNumbers(root->left,num)+sumNumbers(root->right,num);
     }
 };
