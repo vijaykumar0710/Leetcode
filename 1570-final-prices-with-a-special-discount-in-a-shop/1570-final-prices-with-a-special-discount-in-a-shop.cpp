06da0b11f2eaaf3c3ec22488fa7a0ad851d1ba1a
Vijay Kumar <vijaykumar20cy@gmail.com>
2024-12-18 08:03:15 +0530

    Time: 0 ms (100%), Space: 13.9 MB (28.27%) - LeetHub

---
+++ 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> finalPrices(vector<int>& prices) {
+        int n=prices.size();
+        int i=0,j=0;
+        while(i<n){
+            j=i+1;
+            while(j<n){
+                if(prices[i]>=prices[j]){
+                    prices[i]=prices[i]-prices[j];
+                    break;
+                }else{
+                    j++;
+                }
+            }
+            i++;
+        }
+        return prices;
+    }
+};
