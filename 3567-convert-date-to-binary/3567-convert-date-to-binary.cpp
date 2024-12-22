2da44d24f8b0c64d3709791821b49f5d530f5229
Vijay Kumar <vijaykumar20cy@gmail.com>
2024-12-22 11:43:12 +0530

    Time: 4 ms (10.52%), Space: 10.9 MB (11.49%) - LeetHub

--- 3567-convert-date-to-binary/3567-convert-date-to-binary.cpp
+++ 3567-convert-date-to-binary/3567-convert-date-to-binary.cpp
@@ -1,41 +1,17 @@
 class Solution {
 public:
+void conversion(int num,string &binary){
+    if(num>1){
+        conversion(num/2,binary);
+    }
+    binary+=to_string(num%2);
+}
     string convertDateToBinary(string date) {
-     
-        string result=""; 
-        string year=date.substr(0,4);
-        string month=date.substr(5,2);
-        string day=date.substr(8,2);
+        string year,month,day;
+        conversion(stoi(date.substr(0,4)),year);
+        conversion(stoi(date.substr(5,2)),month);
+        conversion(stoi(date.substr(8,2)),day);
 
-        int y=stoi(year);
-        int m=stoi(month);
-        int d=stoi(day);
-
-        bitset<16> binaryYear(y);
-        bitset<8> binaryMonth(m);
-        bitset<8> binaryDay(d);
-
-       string fY=binaryYear.to_string();
-         int i=0;
-         while(fY[i]!='1'){
-            i++; 
-         }
-          result+=fY.substr(i)+"-";
-
-       string fM=binaryMonth.to_string();
-       int j=0;
-         while(fM[j]!='1'){
-            j++;
-         }
-          result+=fM.substr(j)+"-";
-
-          string fD=binaryDay.to_string();
-          int k=0;
-         while(fD[k]!='1'){
-            k++;
-         }
-          result+=fD.substr(k);
-
-        return result;
+      return year+"-"+month+"-"+day;       
     }
 };
