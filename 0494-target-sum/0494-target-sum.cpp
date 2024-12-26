ec7b29de01401b24ae6cb95191ae52398e5ac9e0
Vijay Kumar <vijaykumar20cy@gmail.com>
2024-12-26 18:37:57 +0530

    Time: 396 ms (32.65%), Space: 90 MB (6.35%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
 int n;
-int solve(vector<int>&nums,int i,long target,long long sum){
+int solve(vector<int>&nums,int i,long target,long long sum,unordered_map<string ,int>&mp){
     if (i == n) { 
     return sum == target ? 1 : 0; 
 }
-
-   return solve(nums,i+1,target,(sum+nums[i]))+solve(nums,i+1,target,(sum-nums[i]));
-    
+   string key=to_string(i)+"_"+to_string(sum);
+   if(mp.count(key)) return mp[key];
+   return mp[key]=solve(nums,i+1,target,(sum+nums[i]),mp)+solve(nums,i+1,target,(sum-nums[i]),mp);   
 }
     int findTargetSumWays(vector<int>& nums, int target) {
         n=nums.size();
-        
-        return solve(nums,0,target,0);
+        unordered_map<string ,int>mp;
+        return solve(nums,0,target,0,mp);
     }
 };
