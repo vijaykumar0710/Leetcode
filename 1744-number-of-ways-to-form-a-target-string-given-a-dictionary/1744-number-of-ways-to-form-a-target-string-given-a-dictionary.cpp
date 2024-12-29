7957f349b018d24551b425b6c5f281a175e84b60
Vijay Kumar <vijaykumar20cy@gmail.com>
2024-12-29 09:48:49 +0530

    Time: 83 ms (54.75%), Space: 37.3 MB (79.81%) - LeetHub

---
+++ 1744-number-of-ways-to-form-a-target-string-given-a-dictionary/1744-number-of-ways-to-form-a-target-string-given-a-dictionary.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+   int m;
+   int k;
+   int t[1001][1001];
+   const int MOD=1e9+7;
+   int solve(int i,int j,vector<vector<long long>>&freq,string &target){
+       if(i==m) return 1;
+       if(j==k) return 0;
+        
+       if(t[i][j]!=-1) return t[i][j];
+       int not_taken=solve(i,j+1,freq,target)%MOD;
+       int taken=freq[target[i]-'a'][j]*solve(i+1,j+1,freq,target)%MOD;
+
+       return t[i][j]=(not_taken+taken)%MOD;
+   }
+    int numWays(vector<string>& words, string target) {
+        m=target.size();
+        k=words[0].size();
+        vector<vector<long long>>freq(26,vector<long long>(k));
+        for(int col=0;col<k;col++){
+            for(string &word:words){
+                char ch=word[col];
+                freq[ch-'a'][col]++;
+            }
+        }
+        memset(t,-1,sizeof(t));
+        return solve(0,0,freq,target);
+    }
+};
