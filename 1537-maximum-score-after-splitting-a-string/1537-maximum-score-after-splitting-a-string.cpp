adb43c7fd42ccab3498159c720c1ec692376a677
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-01-01 10:07:58 +0530

    Time: 0 ms (100%), Space: 9.6 MB (8.02%) - LeetHub

--- 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.cpp
+++ 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.cpp
@@ -2,17 +2,38 @@ class Solution {
 public:
     int maxScore(string s) {
         int n=s.length();
-        int maxi=0;
-        for(int i=0;i<n-1;i++){
-            int zeros=0,ones=0;
-            for(int j=i;j>=0;j--){
-                if(s[j]=='0') zeros++;
-            }
-             for(int k=i+1;k<n;k++){
-                if(s[k]=='1') ones++;
-            }
-            maxi=max(maxi,(zeros+ones));
+        vector<int>prefixSum_0(n);
+        vector<int>suffixSum_1(n);
+
+        if(s[0]=='0'){
+            prefixSum_0[0]=1;
+        }else{
+            prefixSum_0[0]=0;
         }
-        return maxi;
+        for(int i=1;i<n;i++){
+         if(s[i]=='0'){
+            prefixSum_0[i]=prefixSum_0[i-1]+1;
+        }else{
+            prefixSum_0[i]=prefixSum_0[i-1];
+           }
+        }
+
+          if(s[n-1]=='1'){
+            suffixSum_1[n-1]=1;
+        }else{
+            suffixSum_1[n-1]=0;
+        }
+        for(int i=n-2;i>=0;i--){
+         if(s[i]=='1'){
+            suffixSum_1[i]=suffixSum_1[i+1]+1;
+        }else{
+            suffixSum_1[i]=suffixSum_1[i+1];
+          }
+        }
+         int maxi=0;
+         for(int i=0;i<n-1;i++){
+            maxi=max(maxi,(prefixSum_0[i]+suffixSum_1[i+1]));
+         }
+         return maxi;
     }
 };
