7e86f2fd8bc9ee9cec0dced25963632407fb7730
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-01-03 17:54:35 +0530

    Time: 0 ms (100%), Space: 9.1 MB (43.12%) - LeetHub

--- 0728-self-dividing-numbers/0728-self-dividing-numbers.cpp
+++ 0728-self-dividing-numbers/0728-self-dividing-numbers.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
-  bool solve(int num){
-    int original=num;
-    while(num>0){ 
+  bool solve(int num,int number){
       int divider=num%10;
-      if(divider==0 || original%divider!=0){
+      if(divider==0 || number%divider!=0){
         return false;
       }
-      num/=10;
-    }
+      if(num/10>0){
+     return solve(num/10,number);
+      }
       return true;
   }
     vector<int> selfDividingNumbers(int left, int right) {
         vector<int>result;
         for(int i=left;i<=right;i++){
-           if(solve(i)) result.push_back(i);
+           if(solve(i,i)){
+            result.push_back(i);
+           }
         }
         return result;
     }
