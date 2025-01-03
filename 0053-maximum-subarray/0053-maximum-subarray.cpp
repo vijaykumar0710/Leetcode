854a9326c0d6e78255bf53a7c26e9dd454ce6fab
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-01-03 13:49:08 +0530

    Time: 0 ms (100%), Space: 74.5 MB (5.26%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -4,9 +4,12 @@ public:
         int n=nums.size();
         vector<int>dp(n);
         dp[0]=nums[0];
+        int max_sum=INT_MIN;
         for(int i=1;i<n;i++){
            dp[i]=max(dp[i-1]+nums[i],nums[i]);
+           max_sum=max(max_sum,dp[i]);
         }
-        return *max_element(dp.begin(),dp.end());
+         max_sum=max(max_sum,dp[0]);
+        return max_sum;
     }
 };
