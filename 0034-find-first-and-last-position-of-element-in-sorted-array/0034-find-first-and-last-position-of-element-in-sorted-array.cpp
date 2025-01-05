b87b758fdad3991464c1ee263918e3e2467149d3
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-01-05 23:40:14 +0530

    Time: 0 ms (100%), Space: 17.5 MB (59.17%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,23 +1,42 @@
 class Solution {
 public:
+int solveLeft(vector<int>& nums,int target,int n){
+    int l=0,r=n-1;
+    int left_most=-1;
+    while(l<=r){
+        int mid=l+(r-l)/2;
+        if(nums[mid]==target){
+            left_most=mid;
+            r=mid-1;
+        }else if(nums[mid]<target){
+            l=mid+1;
+        }else{
+            r=mid-1;
+        }
+    }
+    return left_most;
+}
+
+int solveLRight(vector<int>& nums,int target,int n){
+    int l=0,r=n-1;
+    int right_most=-1;
+    while(l<=r){
+        int mid=l+(r-l)/2;
+        if(nums[mid]==target){
+            right_most=mid;
+            l=mid+1;
+        }else if(nums[mid]<target){
+            l=mid+1;
+        }else{
+            r=mid-1;
+        }
+    }
+    return right_most;
+}
     vector<int> searchRange(vector<int>& nums, int target) {
         int n=nums.size();
-        int i=0,j=n-1;
-         vector<int>res(2,-1);
-        while(i<n){
-            if(nums[i]==target){
-                res[0]=i;
-                break;
-            }
-            i++;
-        }
-         while(j>=0){
-            if(nums[j]==target){
-                res[1]=j;
-                break;
-            }
-            j--;
-        }
-        return res;
+        int left_most=solveLeft(nums,target,n);
+        int right_most=solveLRight(nums,target,n);
+        return {left_most,right_most};
     }
 };
