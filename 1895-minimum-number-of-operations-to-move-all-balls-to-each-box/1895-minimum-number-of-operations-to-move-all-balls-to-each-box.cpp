dbe41363b2582415ac8f3b8925cfb3cb91ba1075
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-01-06 20:50:11 +0530

    Time: 0 ms (100%), Space: 13.5 MB (6.09%) - LeetHub

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -2,19 +2,29 @@ class Solution {
 public:
     vector<int> minOperations(string boxes) {
         int n=boxes.size();
-        vector<int>have_balls;
-        for(int i=0;i<n;i++){
-            if(boxes[i]=='1'){
-                have_balls.push_back(i);
-            }
+        vector<int>prefix(n);
+        prefix[0]=boxes[0]=='1'?1:0;
+        for(int i=1;i<n;i++){
+        prefix[i]=prefix[i-1]+(boxes[i]=='1'?1:0);
+        }
+        vector<int>suffix(n);
+        suffix[n-1]=boxes[n-1]=='1'?1:0;
+        for(int i=n-2;i>=0;i--){
+        suffix[i]=suffix[i+1]+(boxes[i]=='1'?1:0);
+        }
+        vector<int>prefix_sum(n);
+        prefix_sum[0]=0;
+        for(int i=1;i<n;i++){
+        prefix_sum[i]=prefix_sum[i-1]+prefix[i-1];
+        }
+        vector<int>suffix_sum(n);
+        suffix_sum[n-1]=0;
+        for(int i=n-2;i>=0;i--){
+        suffix_sum[i]=suffix[i+1]+suffix_sum[i+1];
         }
         vector<int>result;
-        for(int j=0;j<n;j++){
-            int diff=0;
-            for(int k=0;k<have_balls.size();k++){
-                diff+=abs(j-have_balls[k]);
-            }
-            result.push_back(diff);
+        for(int i=0;i<n;i++){
+            result.push_back(prefix_sum[i]+suffix_sum[i]);
         }
         return result;
     }
