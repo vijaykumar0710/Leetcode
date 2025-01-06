cb2620ea84908256d29dac9fdb5f0e196ee0f24b
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-01-06 23:39:51 +0530

    Time: 0 ms (100%), Space: 15.3 MB (22.7%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,43 +1,42 @@
 class Solution {
 public:
-   int findPivot(vector<int>& nums,int n){
+  int findPivot(vector<int>& nums,int n){
     int l=0,r=n-1;
-
     while(l<r){
         int mid=l+(r-l)/2;
         if(nums[mid]>nums[r]){
             l=mid+1;
-        }else r=mid;
+        }else{
+            r=mid;
+        }
     }
     return r;
-   }
+  }
 
-    int binarySearch(int l,int r,vector<int>& nums,int target){
-        int idx=-1;
-
-        while(l<=r){
-            int mid=l+(r-l)/2;
-
-            if(nums[mid]==target){
-                idx=mid;
-                break;
-            }else if(nums[mid]<target){
-                l=mid+1;
-            }else r=mid-1;
+  int binarySearch(int l,int r,vector<int>& nums,int target){
+      int idx=-1;
+      while(l<=r){
+        int mid=l+(r-l)/2;
+        if(nums[mid]==target){
+            idx=mid;
+            break;
+        }else if(nums[mid]>target){
+            r=mid-1;
+        }else{
+            l=mid+1;
         }
-        return idx;
-    }
-   
+      }
+      return idx;
+  }
     int search(vector<int>& nums, int target) {
         int n=nums.size();
-
-        int pivot_index=findPivot(nums,n);
-
-        int idx=binarySearch(0,pivot_index-1,nums,target);
-
-        if(idx!=-1) return idx;
-
-         idx=binarySearch(pivot_index,n-1,nums,target);
+        int pivot_idx=findPivot(nums,n);
+        int idx=binarySearch(0,pivot_idx-1,nums,target);
+        if(idx!=-1){
+            return idx;
+        }else{
+            idx=binarySearch(pivot_idx,n-1,nums,target);
+        }
         return idx;
     }
 };
