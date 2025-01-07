c1eb5735c60a53b9830feadbabb31163ced4ac63
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-01-07 23:14:50 +0530

    Time: 35 ms (5.35%), Space: 20.4 MB (5.35%) - LeetHub

--- 1524-string-matching-in-an-array/1524-string-matching-in-an-array.cpp
+++ 1524-string-matching-in-an-array/1524-string-matching-in-an-array.cpp
@@ -1,17 +1,66 @@
 class Solution {
 public:
+ void computelps(string pat,vector<int>&lps){
+      int m=pat.size();
+      int len=0;
+
+      lps[0]=0;
+      int i=1;
+      while(i<m){
+        if(pat[i]==pat[len]){
+            len++;
+            lps[i]=len;
+            i++;
+        }else{
+            if(len!=0){
+                len=lps[len-1];
+            }else{
+                lps[i]=0;
+                i++;
+            }
+        }
+      }
+    }
+  
+bool searchKMP(string pat,string txt){
+    int n=txt.size();
+    int m=pat.size();
+
+    vector<int>lps(m,0);
+    computelps(pat,lps);
+
+     int i=0,j=0;
+
+    while(i<n){
+        if(txt[i]==pat[j]){
+            i++;
+            j++;
+        }
+        if(j==m){
+            return true;
+        }else if(i<n && pat[j]!=txt[i]){
+            if(j!=0){
+                j=lps[j-1];
+            }else{
+                i++;
+            }
+        }
+    }
+    return false;
+}
     vector<string> stringMatching(vector<string> &words) {
         int size = words.size();
-        set<string> result; // Use a set to store unique subsequences
+        vector<string>result;
 
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
                 if(i==j) continue;
-                if(words[j].find(words[i])!=string::npos){
-                    result.insert(words[i]);
+                if(searchKMP(words[i],words[j])){
+                    result.push_back(words[i]);
+                    break;
                 }
             }
         }
-        return vector<string>(result.begin(), result.end()); // Convert set to vector
+        return result;
     }
 };
