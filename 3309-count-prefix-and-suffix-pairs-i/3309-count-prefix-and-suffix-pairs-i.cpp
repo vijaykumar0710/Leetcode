28276e9af39114bff9bdc4b412f7915b51448d7b
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-01-09 00:23:15 +0530

    Time: 15 ms (12.5%), Space: 45.8 MB (5.08%) - LeetHub

--- 3309-count-prefix-and-suffix-pairs-i/3309-count-prefix-and-suffix-pairs-i.cpp
+++ 3309-count-prefix-and-suffix-pairs-i/3309-count-prefix-and-suffix-pairs-i.cpp
@@ -1,17 +1,69 @@
+class Trie{
+    public:
+    struct trieNode{
+    trieNode *children[26];
+    bool isEndofWord;
+ };
+trieNode *getNode(){
+    trieNode *newNode=new trieNode();
+    newNode->isEndofWord=false;
+    for(int i=0;i<26;i++){
+        newNode->children[i]=NULL;
+    }
+    return newNode;
+}
+   trieNode *root;
+   Trie(){
+    root=getNode();
+   }
+ void insert(string word){
+    trieNode *crawler=root;
+    for(int i=0;i<word.length();i++){
+        int idx=word[i]-'a';
+        if(!crawler->children[idx]){
+            crawler->children[idx]=getNode();
+        }
+        crawler=crawler->children[idx];
+    }
+    crawler->isEndofWord=true;
+   }
+   bool serachPrefix(string prefix){
+    trieNode *crawler=root;
+    for(int i=0;i<prefix.length();i++){
+        int idx=prefix[i]-'a';
+        if(!crawler->children[idx]){
+            return false;
+        }
+        crawler=crawler->children[idx];
+    }
+    return true;
+   }
+};
 class Solution {
 public:
     int countPrefixSuffixPairs(vector<string>& words) {
         int n=words.size();
-        int cnt=0;
-        for(int i=0;i<n-1;i++){
-            int len=words[i].size();
-            for(int j=i+1;j<n;j++){
-                int size=words[j].size();
-               if(words[j].substr(0,len)==words[i] && words[j].substr(size-len,len)==words[i]){
-                cnt++;
-               }
+        int count=0;
+        for(int j=0;j<n;j++){
+            Trie prefixTrie;
+            Trie suffixTrie;
+            prefixTrie.insert(words[j]);
+
+            string reversed=words[j];
+            reverse(begin(reversed),end(reversed));
+            suffixTrie.insert(reversed);
+
+            for(int i=0;i<j;i++){
+                if(words[i].length()>words[j].length()){
+                    continue;
+                }
+                string rev=words[i];
+                reverse(begin(rev),end(rev));
+                if(prefixTrie.serachPrefix(words[i])==true && suffixTrie.serachPrefix(rev)==true){
+                    count++;
+                }
             }
         }
-        return cnt;
+        return count;
     }
 };
