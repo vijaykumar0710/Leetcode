2a6f965cb8b62326a2c471dbc2bd7b6f28c229a9
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-01-08 08:59:02 +0530

    Time: 0 ms (100%), Space: 18.5 MB (57.42%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -3,21 +3,24 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int m=matrix.size();
         int n=matrix[0].size();
-        vector<pair<int,int>>vec;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                  if(matrix[i][j]==0){
-                    vec.push_back({i,j});
+                    for(int k=0;k<n;k++){
+                       if(matrix[i][k]!=0)matrix[i][k]=2e9;
+                    }
+                    for(int v=0;v<m;v++){
+                        if(matrix[v][j]!=0)matrix[v][j]=2e9;
+                    }
                  }
             }
         }
-    for(auto idx:vec){
+       for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                matrix[idx.first][j]=0;
+                 if(matrix[i][j]==2e9){
+                    matrix[i][j]=0;
+                 }
+            }
         }
-            for(int i=0;i<m;i++){
-                matrix[i][idx.second]=0;
-        }
-      }
     }
 };
