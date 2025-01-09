bdbcb34e26e3769f9e50c59616eb2bc92f03f18e
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-01-09 22:49:45 +0530

    Time: 59 ms (5.04%), Space: 111 MB (5.04%) - LeetHub

--- 2292-counting-words-with-a-given-prefix/2292-counting-words-with-a-given-prefix.cpp
+++ 2292-counting-words-with-a-given-prefix/2292-counting-words-with-a-given-prefix.cpp
@@ -50,7 +50,7 @@ class Trie{
         int cnt=0;
         for(int i=0;i<n;i++){
             Trie prefixTrie;
-           prefixTrie.insert(words[i]);
+            prefixTrie.insert(words[i]);
             if(prefixTrie.startsWith(pref)){
                cnt++;
             }
