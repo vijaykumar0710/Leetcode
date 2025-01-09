35a1d665371c8d90f67fd9eed37f0344c7836413
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-01-09 22:13:32 +0530

    Time: 1 ms (51.94%), Space: 18.3 MB (6.56%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
 bool isOperator(string& token) {
-    // Define a list of valid operators
-    vector<string> operators = {"+", "-", "*", "/"};
-
-    // Check if the token matches any operator
-    return find(operators.begin(), operators.end(), token) != operators.end();
-}
+        static unordered_set<string> operators = {"+", "-", "*", "/"};
+        return operators.find(token) != operators.end();
+    }
     int evalRPN(vector<string>& tokens) {
         int n=tokens.size();
         stack<int>st;
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n;i++){ 
             string token=tokens[i];
             if(isOperator(token)){
                 int x=st.top();
@@ -18,20 +15,16 @@ bool isOperator(string& token) {
                 int y=st.top();
                 st.pop();
                 if(token=="+"){
-                    y=y+x;
-                    st.push(y);
+                    st.push(y+x);
                 }
-                if(token=="-"){
-                    y=y-x;
-                    st.push(y);
+                else if(token=="-"){
+                    st.push(y-x);
                 }
-                if(token=="*"){
-                    y=y*x;
-                    st.push(y);
+                else if(token=="*"){
+                    st.push(y*x);
                 }
-                if(token=="/"){
-                    y=y/x;
-                    st.push(y);
+                else{
+                    st.push(y/x);
                 }
             }
             else{
