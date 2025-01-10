51dcb4fc903d41d9e822018ab3dd169a09b476e4
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-01-10 15:25:31 +0530

    Time: 34 ms (10.63%), Space: 17.3 MB (20.18%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -1,32 +1,28 @@
 class Solution {
 public:
 int m,n,p;
+int t[101][101][201];
 bool solver(string &s1,string &s2,string &s3,int i,int j,int k){
     if(i==m && j==n && k==p)return true;
-
+    if(t[i][j][k]!=-1)return t[i][j][k];
     if(i<m && k<p && s3[k]==s1[i]){
         if(solver(s1,s2,s3,i+1,j,k+1)){
-            return true;
+           return t[i][j][k]=true;
         }
     }
     if(j<n && k<p && s3[k]==s2[j]){
         if(solver(s1,s2,s3,i,j+1,k+1)){
-            return true;
+           return t[i][j][k]=true;
         }
     }
-    return false;
+    return t[i][j][k]=false;
 }
     bool isInterleave(string s1, string s2, string s3) {
-        if(s1=="abababababababababababababababababababababababababababababababababababababababababababababababababbb"
- && s2=="babababababababababababababababababababababababababababababababababababababababababababababababaaaba"
- && s3=="abababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababbb"
-){
-    return false;
-}
         m=s1.size();
         n=s2.size();
         p=s3.size();
         if(m+n!=p) return false;
+        memset(t,-1,sizeof(t));
         return solver(s1,s2,s3,0,0,0);
     }
 };
