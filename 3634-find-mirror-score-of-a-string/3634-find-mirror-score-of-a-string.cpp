5e09433f781510b4e83508f6a0a5e7fc6b211375
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-01-10 23:03:03 +0530

    Time: 22 ms (77.76%), Space: 16.8 MB (80.34%) - LeetHub

--- 3634-find-mirror-score-of-a-string/3634-find-mirror-score-of-a-string.cpp
+++ 3634-find-mirror-score-of-a-string/3634-find-mirror-score-of-a-string.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     long long calculateScore(string s) {
-      int n = s.size();
-        long long score = 0;
-        unordered_map<char, vector<int>> unmarked;
-        for (int i = 0; i < n; i++) {
-            char mirrorChar = 'z' - (s[i] - 'a');
-            if (!unmarked[mirrorChar].empty()) {
-                int j = unmarked[mirrorChar].back();  
-                unmarked[mirrorChar].pop_back(); 
-                score += (i - j);
-            } else {
-                unmarked[s[i]].push_back(i);
+        int n=s.size();
+        unordered_map<char,vector<int>>mp;
+        long long score=0;
+        for(int i=0;i<n;i++){
+            if(mp.find('z'-(s[i]-'a'))!=mp.end() && !mp['z'-(s[i]-'a')].empty()){
+               int j=mp['z'-(s[i]-'a')].back();
+               mp['z'-(s[i]-'a')].pop_back();
+               score+=(i-j);
             }
+         else mp[s[i]].push_back(i);
         }
         return score;
     }
