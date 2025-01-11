2117e6f0b076bcc74ef41774b4d3d4ab33a4ca47
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-01-11 11:19:52 +0530

    Time: 0 ms (100%), Space: 8.7 MB (89.4%) - LeetHub

---
+++ 0768-partition-labels/0768-partition-labels.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        int n=s.size();
+        vector<int>last_occurence(26);
+        for(int i=0;i<n;i++){
+            last_occurence[s[i]-'a']=i;
+        }
+        vector<int>result;
+        int start=0;
+        int end=0;
+       for(int i=0;i<n;i++){
+         end=max(end,last_occurence[s[i]-'a']);
+         if(i==end){
+            result.push_back(end-start+1);
+            start=end+1;
+         }
+       }
+        return result;
+    }
+};
