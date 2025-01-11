a2c7d76ce43bf1fe405874dcd01e582377c82313
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-01-11 19:56:53 +0530

    Time: 407 ms (5.08%), Space: 17.7 MB (42.27%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -6,7 +6,7 @@ public:
         for(int i=0;i<n;i++){
             int pro=1;
             for(int j=i;j<n;j++){
-                 pro=pro*nums[j];
+                pro=pro*nums[j];
                 maxi=max(maxi,pro);
             }
         }
