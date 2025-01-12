8f34a5022593596d26eea6771b7c7df02f7ba0c5
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-01-12 19:10:10 +0530

    Time: 9 ms (85.79%), Space: 30 MB (70.56%) - LeetHub

--- 2221-check-if-a-parentheses-string-can-be-valid/2221-check-if-a-parentheses-string-can-be-valid.cpp
+++ 2221-check-if-a-parentheses-string-can-be-valid/2221-check-if-a-parentheses-string-can-be-valid.cpp
@@ -3,37 +3,23 @@ public:
     bool canBeValid(string s, string locked) {
         int n = s.size();
         if (n % 2 != 0) return false;  
-        int open = 0, locker = 0;
+        int open = 0;
         for (int i = 0; i < n; i++) {
-            if (s[i] == '(') {
+            if (s[i] == '(' || locked[i]=='0') {
                 open++; 
-            } else if (locked[i]=='0') {
-                locker++; 
-            } else { 
-                if (open > 0) {
-                    open--; 
-                } else if (locker > 0) {
-                    locker--;
-                } else {
-                    return false; 
-                }
+            } else{
+                 open--;
             } 
+            if(open<0) return false;
         }
-        int close = 0, locker_back = 0;
-        for (int i = n - 1; i >= 0; i--) {
-            if (s[i] == ')') {
+        int close = 0;
+        for (int i = n-1; i >=0; i--) {
+            if (s[i] == ')' || locked[i]=='0') {
                 close++; 
-            } else if (locked[i]=='0') {
-                locker_back++; 
-            } else { 
-                if (close > 0) {
-                    close--; 
-                } else if (locker_back > 0) {
-                    locker_back--; 
-                } else {
-                    return false; 
-                }
-            }
+            } else{
+                 close--;
+            } 
+            if(close<0) return false;
         }
         return true;
     }
