3c68c114c0aa3287142422327f6e03656181b098
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-01-12 16:37:44 +0530

    Time: 6 ms (97.46%), Space: 30.1 MB (53.05%) - LeetHub

--- 2221-check-if-a-parentheses-string-can-be-valid/2221-check-if-a-parentheses-string-can-be-valid.cpp
+++ 2221-check-if-a-parentheses-string-can-be-valid/2221-check-if-a-parentheses-string-can-be-valid.cpp
@@ -2,43 +2,36 @@ class Solution {
 public:
     bool canBeValid(string s, string locked) {
         int n = s.size();
-        if (n % 2 != 0) return false; // Odd length cannot be valid
-        
-        int open = 0, flexible = 0;
-        
-        // Left to right pass
-        for (int i = 0; i < n; ++i) {
-            if (locked[i] == '0') {
-                flexible++; // Flexible character
-            } else if (s[i] == '(') {
-                open++; // Open parenthesis
-            } else { // s[i] == ')'
+        if (n % 2 != 0) return false;  
+        int open = 0, locker = 0;
+        for (int i = 0; i < n; i++) {
+            if (s[i] == '(') {
+                open++; 
+            } else if (locked[i]=='0') {
+                locker++; 
+            } else { 
                 if (open > 0) {
-                    open--; // Match with an open parenthesis
-                } else if (flexible > 0) {
-                    flexible--; // Use a flexible character
+                    open--; 
+                } else if (locker > 0) {
+                    locker--;
                 } else {
-                    return false; // No match available
+                    return false; 
                 }
-            }
+            } 
         }
-
-        // Reset for right to left pass
-        open = 0, flexible = 0;
-
-        // Right to left pass
-        for (int i = n - 1; i >= 0; --i) {
-            if (locked[i] == '0') {
-                flexible++; // Flexible character
-            } else if (s[i] == ')') {
-                open++; // Close parenthesis
-            } else { // s[i] == '('
-                if (open > 0) {
-                    open--; // Match with a close parenthesis
-                } else if (flexible > 0) {
-                    flexible--; // Use a flexible character
+        int close = 0, locker_back = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            if (s[i] == ')') {
+                close++; 
+            } else if (locked[i]=='0') {
+                locker_back++; 
+            } else { 
+                if (close > 0) {
+                    close--; 
+                } else if (locker_back > 0) {
+                    locker_back--; 
                 } else {
-                    return false; // No match available
+                    return false; 
                 }
             }
         }
