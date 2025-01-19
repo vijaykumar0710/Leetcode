8ed4879ff8a07eaa7df9dee66695524f7e935273
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-01-19 10:05:55 +0530

    Time: 359 ms (64.71%), Space: 85.4 MB (88.24%) - LeetHub

--- 3714-maximum-and-minimum-sums-of-at-most-size-k-subsequences/3714-maximum-and-minimum-sums-of-at-most-size-k-subsequences.cpp
+++ 3714-maximum-and-minimum-sums-of-at-most-size-k-subsequences/3714-maximum-and-minimum-sums-of-at-most-size-k-subsequences.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    const long long large_mod = 1e9+7;
+    long long MOD = 1e9+7;
     int minMaxSums(vector<int>& nums, int k) {
         sort(nums.begin(), nums.end());
         int n = nums.size();
@@ -11,14 +11,14 @@ public:
         for(int j = 0; j < k-1; j++){
             for(int i = 0; i < n; i++){
                 long long prev = (i == 0) ? 0L : dp[i-1];
-                dp[i] = (prev + dp[n-1] - dp[i] + large_mod) % large_mod;
-                res = (res + ((((dp[i] - prev + large_mod) % large_mod) * nums[i]) % large_mod)) % large_mod;
-                res = (res + ((((dp[i] - prev + large_mod) % large_mod) * nums[n-i-1]) % large_mod)) % large_mod;
+                dp[i] = (prev + dp[n-1] - dp[i] + MOD) % MOD;
+                res = (res + ((((dp[i] - prev + MOD) % MOD) * nums[i]) % MOD)) % MOD;
+                res = (res + ((((dp[i] - prev + MOD) % MOD) * nums[n-i-1]) % MOD)) % MOD;
             }
         }
         for(int i : nums){
-            res = (res + i) % large_mod;
-            res = (res + i) % large_mod;
+            res = (res + i) % MOD;
+            res = (res + i) % MOD;
         }
         
         return res;
