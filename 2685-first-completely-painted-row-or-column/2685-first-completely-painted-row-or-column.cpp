2e643645c621c140ba4a1ba941f17e1adc2672f4
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-01-20 14:06:38 +0530

    Time: 92 ms (71.65%), Space: 171 MB (37.8%) - LeetHub

--- 2685-first-completely-painted-row-or-column/2685-first-completely-painted-row-or-column.cpp
+++ 2685-first-completely-painted-row-or-column/2685-first-completely-painted-row-or-column.cpp
@@ -6,7 +6,7 @@ public:
         unordered_map<int,pair<int,int>>mp;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                mp[mat[i][j]]={i,j};
+                mp[mat[i][j]]=make_pair(i,j);
             }
         }
         vector<int>row_mp(m,0);
