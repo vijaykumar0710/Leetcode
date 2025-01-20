5b8a3b1f6ed784811b3d45f1d356ecb3eb088587
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-01-20 15:36:47 +0530

    Time: 0 ms (100%), Space: 12.8 MB (64%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -3,7 +3,7 @@ public:
 int n;
 int t[201][201];
 int solve(vector<vector<int>>& triangle,int i,int j){
-    if(i==n-1) return triangle[i][j];
+    if(i==n-1) return triangle[n-1][j];
     if(t[i][j]!=-1) return t[i][j];
     int down=triangle[i][j]+solve(triangle,i+1,j);
     int diagonal=triangle[i][j]+solve(triangle,i+1,j+1);
