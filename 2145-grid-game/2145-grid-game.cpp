bd148efb492fdb54eabe04bced0e9792a4dd15e3
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-01-21 14:52:09 +0530

    Time: 1 ms (66.97%), Space: 66.5 MB (92.76%) - LeetHub

--- 2145-grid-game/2145-grid-game.cpp
+++ 2145-grid-game/2145-grid-game.cpp
@@ -6,12 +6,11 @@ public:
     long sumRow0 = accumulate(grid[0].begin(), grid[0].end(), 0L);
     long sumRow1 = 0;
 
-    for (int i = 0; i < n; ++i) {
+    for (int i = 0; i < n; i++) {
       sumRow0 -= grid[0][i];
       ans = min(ans, max(sumRow0, sumRow1));
       sumRow1 += grid[1][i];
     }
-
     return ans;
   }
 };
