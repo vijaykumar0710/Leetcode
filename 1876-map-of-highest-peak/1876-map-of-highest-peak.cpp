06ff225dd3e242fefe23f68df1590518c99fb797
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-01-22 20:01:40 +0530

    Time: 91 ms (36.29%), Space: 120.3 MB (43.08%) - LeetHub

---
+++ 1876-map-of-highest-peak/1876-map-of-highest-peak.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+vector<int> directions = {-1, 0, 1, 0, -1};
+    vector<vector<int>> highestPeak(vector<vector<int>>& isWater) {
+        int m=isWater.size();
+        int n=isWater[0].size();
+        vector<vector<int>>ans(m,vector<int>(n));
+        vector<vector<int>>mat(m,vector<int>(n));
+        queue<pair<int,int>>que;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(isWater[i][j]==1){
+                    mat[i][j]=0;
+                }else{
+                    mat[i][j]=1;
+                }
+            }
+        }
+
+         for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(mat[i][j]==1){
+                    ans[i][j]=-1;
+                }else{
+                    que.push({i,j});
+                    ans[i][j]=0;
+                }
+            }
+        }
+
+        while(!que.empty()){
+           pair<int,int> curr=que.front();
+           int idx=curr.first;
+           int jdx=curr.second;
+            que.pop();
+         for(int i=0;i<4;i++){
+            int nextRow=directions[i]+idx;
+            int nextCol=directions[i+1]+jdx;
+            if(nextRow<m && nextCol<n && nextRow>=0 && nextCol>=0 && ans[nextRow][nextCol]==-1){
+                ans[nextRow][nextCol]=ans[idx][jdx]+1;
+                que.push({nextRow,nextCol});
+            }
+         }
+        }
+        return ans;
+    }
+};
