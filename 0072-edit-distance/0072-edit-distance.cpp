9740a518a1d7c3827627a988669798c7ce81f229
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-01-22 22:38:42 +0530

    Time: 8 ms (44.98%), Space: 13.2 MB (30.38%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -10,15 +10,14 @@ public:
      for(int j=1;j<=n;j++){
            t[0][j]=j;
        }
-       int cost;
     for(int i=1;i<=m;i++){
         for(int j=1;j<=n;j++){
          if(word1[i-1]==word2[j-1]){
-            cost=0;
+            t[i][j]=min({t[i][j-1]+1,t[i-1][j]+1,t[i-1][j-1]});
          }else{
-            cost=1;
+            t[i][j]=min({t[i][j-1]+1,t[i-1][j]+1,t[i-1][j-1]+1});
            }
-            t[i][j]=min({t[i][j-1]+1,t[i-1][j]+1,t[i-1][j-1]+cost});
+            
         }
     }
         return t[m][n];
