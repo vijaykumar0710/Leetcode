6a30b14f95a34f03dd5ec86294fb143c73b64ecb
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-01-25 20:14:08 +0530

    Time: 379 ms (24.29%), Space: 243.8 MB (20.19%) - LeetHub

--- 2505-number-of-good-paths/2505-number-of-good-paths.cpp
+++ 2505-number-of-good-paths/2505-number-of-good-paths.cpp
@@ -1,80 +1,104 @@
 class Solution {
 public:
-vector<int>parent;
-vector<int>rank;
+    vector<int> topologicalSort(int n, vector<vector<int>>& edges) {
+        vector<vector<int>> graph(n);
+        vector<int> indegree(n, 0);
 
-int find(int x){
-if(x==parent[x])
-return x;
-return parent[x]=find(parent[x]);
-}
-void Union(int x,int y){
-    int x_parent=find(x);
-    int y_parent=find(y);
-    if(x_parent==y_parent) return;
-    if(rank[x_parent]>rank[y_parent]){
-        parent[y_parent]=x_parent;
-    }else if(rank[x_parent]<rank[y_parent]){
-           parent[x_parent]=y_parent;
-    }else{
-        parent[x_parent]=y_parent;
-        rank[y_parent]++;
+        // Build graph and calculate in-degrees
+        for (auto edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+            graph[u].push_back(v);
+            indegree[v]++;
+        }
+
+        // Perform topological sort
+        queue<int> q;
+        for (int i = 0; i < n; ++i) {
+            if (indegree[i] == 0) {
+                q.push(i);
+            }
+        }
+
+        vector<int> topo_order;
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            topo_order.push_back(node);
+
+            for (int neighbor : graph[node]) {
+                indegree[neighbor]--;
+                if (indegree[neighbor] == 0) {
+                    q.push(neighbor);
+                }
+            }
+        }
+
+        return topo_order;
     }
-}
-    int numberOfGoodPaths(vector<int>& vals, vector<vector<int>>& edges) {
-        int n=vals.size();
-        parent.resize(n);
-        rank.resize(n,1);
 
-        for(int i=0;i<n;i++){
-            parent[i]=i;
+    int numberOfGoodPaths(vector<int>& vals, vector<vector<int>>& edges) {
+        int n = vals.size();
+
+        // Build an adjacency list for the undirected graph
+        vector<vector<int>> graph(n);
+        for (auto edge : edges) {
+            graph[edge[0]].push_back(edge[1]);
+            graph[edge[1]].push_back(edge[0]);
         }
-        unordered_map<int,vector<int>>adj;
-        for(auto &vec:edges){
-            int u=vec[0];
-            int v=vec[1];
-            adj[u].push_back(v);
-            adj[v].push_back(u);
+
+        // Group nodes by their values
+        map<int, vector<int>> value_to_nodes;
+        for (int i = 0; i < n; ++i) {
+            value_to_nodes[vals[i]].push_back(i);
         }
-        //val to nodes
-         map<int,vector<int>>val_to_nodes;
-         for(int i=0;i<n;i++){
-            int value=vals[i];
-            val_to_nodes[value].push_back(i);
-         }
-         int result=n;
-         vector<bool> is_active(n,false);
-         
-         for(auto &it:val_to_nodes){
-            vector<int>nodes=it.second;
-            for(int &u:nodes){
-                for(int &v:adj[u]){
-                    if(is_active[v]){
-                        Union(u,v);
+
+        // Initialize DSU
+        vector<int> parent(n), size(n, 1);
+        iota(parent.begin(), parent.end(), 0);
+
+        // Find function for DSU
+        function<int(int)> find = [&](int x) {
+            if (x != parent[x]) {
+                parent[x] = find(parent[x]);
+            }
+            return parent[x];
+        };
+
+        // Union function for DSU
+        auto unite = [&](int u, int v) {
+            int pu = find(u), pv = find(v);
+            if (pu != pv) {
+                if (size[pu] < size[pv]) swap(pu, pv);
+                parent[pv] = pu;
+                size[pu] += size[pv];
+            }
+        };
+
+        int good_paths = 0;
+
+        // Process nodes grouped by value
+        for (auto& [val, nodes] : value_to_nodes) {
+            // Unite nodes with the same value
+            for (int node : nodes) {
+                for (int neighbor : graph[node]) {
+                    if (vals[neighbor] <= val) {
+                        unite(node, neighbor);
                     }
                 }
-                is_active[u]=true;
             }
-            vector<int>tumhare_parent;
-            for(int &u:nodes){
-                int parent_kaun_hain=find(u);
-                tumhare_parent.push_back(parent_kaun_hain);
-            }
-            sort(begin(tumhare_parent),end(tumhare_parent));
-            int sz=tumhare_parent.size();
-            for(int j=0;j<sz;j++){
-                long long count=0;
-                int curr_parent=tumhare_parent[j];
 
-                while(j<sz && tumhare_parent[j]==curr_parent){
-                    count++;
-                    j++;
-                }
-                j--;
-                int formula_wala_ans=(count*(count-1)/2);
-                result+=formula_wala_ans;
+            // Count good paths for this value group
+            unordered_map<int, int> group_count;
+            for (int node : nodes) {
+                group_count[find(node)]++;
             }
-         }
-         return result;
+
+            for (auto& [group, count] : group_count) {
+                good_paths += count * (count - 1) / 2 + count;
+            }
+        }
+
+        return good_paths;
     }
 };
