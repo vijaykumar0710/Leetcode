8c5237efbd3af54228c7e4040464e54b4d94ebc4
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-01-27 01:14:31 +0530

    Time: 1774 ms (24.02%), Space: 12.1 MB (49.65%) - LeetHub

--- 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.cpp
+++ 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
-int n;
-bool canPartition(vector<int>& nums,int target,int start,vector<int>&vis,int currSum,int k){
+bool canPartition(vector<int>& nums,int target,int start,vector<int>&vis,int currSum,int k,int n){
    if(k==0) return 1;
    if(currSum>target) return 0;
    if(currSum==target){
-    return canPartition(nums,target,0,vis,0,k-1);
+    return canPartition(nums,target,0,vis,0,k-1,n);
     }
    for(int i=start;i<n;i++){
     if(!vis[i]){
         vis[i]=1;
-        if(canPartition(nums,target,i+1,vis,currSum+nums[i],k)){
+        if(canPartition(nums,target,i+1,vis,currSum+nums[i],k,n)){
             return 1;
         }
         vis[i]=0;
@@ -19,11 +18,11 @@ bool canPartition(vector<int>& nums,int target,int start,vector<int>&vis,int cur
    return 0;
   }
     bool canPartitionKSubsets(vector<int>& nums, int k) {
-         n=nums.size();
+        int n=nums.size();
         int totalSum=accumulate(begin(nums),end(nums),0);
         if(totalSum%k!=0) return false;
         sort(nums.rbegin(), nums.rend());
         vector<int>vis(n,0);
-       return canPartition(nums,totalSum/k,0,vis,0,k);
+       return canPartition(nums,totalSum/k,0,vis,0,k,n);
     }
 };
