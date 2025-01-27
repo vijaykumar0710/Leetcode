a28977be2eedae8d5dd64ff1e2abb1b1f73189c9
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-01-27 15:27:50 +0530

    Time: 80 ms (40.08%), Space: 77.6 MB (21.86%) - LeetHub

---
+++ 1558-course-schedule-iv/1558-course-schedule-iv.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<bool> checkIfPrerequisite(int numCourses, vector<vector<int>>& prerequisites, vector<vector<int>>& queries) {
+        unordered_map<int, vector<int>> adj;
+        vector<int> inDegree(numCourses, 0);
+        for (const auto& pre : prerequisites) {
+            adj[pre[0]].push_back(pre[1]);
+            inDegree[pre[1]]++;
+        }
+        queue<int> q;
+        for (int i = 0; i < numCourses; i++) {
+            if (inDegree[i] == 0) q.push(i);
+        }
+       unordered_map<int,unordered_set<int>>topo;
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            for (int neighbor : adj[node]) {
+                topo[neighbor].insert(node);
+                for(auto &prereq:topo[node]){
+                    topo[neighbor].insert(prereq);
+                }
+                inDegree[neighbor]--;
+                if (inDegree[neighbor] == 0) q.push(neighbor);
+            }
+        }
+       vector<bool>result;
+       for(auto query:queries){
+        int a=query[0];
+        int b=query[1];
+        bool isReachable=topo[b].contains(a);
+        result.push_back(isReachable);
+       }
+       return result;
+    }
+};
