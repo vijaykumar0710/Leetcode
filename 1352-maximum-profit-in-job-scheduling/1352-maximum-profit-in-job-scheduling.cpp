53b75004d693263d864126ec26479599868a3d0a
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-01-29 00:09:39 +0530

    Time: 20 ms (94.75%), Space: 80.3 MB (81.78%) - LeetHub

---
+++ 1352-maximum-profit-in-job-scheduling/1352-maximum-profit-in-job-scheduling.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int jobScheduling(vector<int>& startTime,vector<int>& endTime,vector<int>& profit) {
+        int n = startTime.size();
+        vector<pair<int,pair<int,int>>>jobs;
+        for (int i = 0; i < n; ++i) {
+            jobs.push_back({profit[i], {startTime[i], endTime[i]}});
+        }
+        sort(jobs.begin(),jobs.end(), [](auto& a, auto& b) {
+            return a.second.second < b.second.second;
+        });
+       vector<int> dp(n + 1, 0);
+       for (int i = 1; i <= n; ++i) {
+            int currentProfit = jobs[i - 1].first;
+            int currentStart = jobs[i - 1].second.first;
+            int left = 0, right = i - 1;
+            int latestNonConflictJobIndex = 0;
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+                if (jobs[mid].second.second <= currentStart) {
+                    latestNonConflictJobIndex = mid + 1;
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+            dp[i] = std::max(dp[i - 1], dp[latestNonConflictJobIndex] + currentProfit);
+        }
+        return dp[n];
+    }
+};
