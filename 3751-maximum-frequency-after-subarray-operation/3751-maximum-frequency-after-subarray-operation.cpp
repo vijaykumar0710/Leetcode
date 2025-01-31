db9c6a813d6d26595b4142aeaa12b5a76f5c118a
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-02-01 00:26:44 +0530

    Time: 46 ms (83.58%), Space: 96.5 MB (71.77%) - LeetHub

--- 3751-maximum-frequency-after-subarray-operation/3751-maximum-frequency-after-subarray-operation.cpp
+++ 3751-maximum-frequency-after-subarray-operation/3751-maximum-frequency-after-subarray-operation.cpp
@@ -1,12 +1,20 @@
 class Solution {
 public:
-        int maxFrequency(vector<int>& A, int k) {
-        unordered_map<int, int> count;
-        int res = 0;
-        for (int a : A) {
-            count[a] = max(count[a], count[k]) + 1;
-            res = max(res, count[a] - count[k]);
+    int maxFrequency(vector<int>& nums, int k) {
+        int n=nums.size();
+        int no_k=count(nums.begin(),nums.end(),k);
+        int mx=0;
+        for(int i=1;i<=50;i++){
+          if(i==k) continue;
+          int s=0,l=0;
+          for(auto &it:nums){
+            if(i==it) s++;
+            else if(it==k) s--;
+            if(s<0) s=0;
+            l=max(l,s);
+          }
+          mx=max(mx,l);
         }
-        return count[k] + res;
+        return no_k+mx;
     }
 };
