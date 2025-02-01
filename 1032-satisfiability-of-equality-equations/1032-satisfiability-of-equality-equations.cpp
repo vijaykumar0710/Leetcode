6b247e4194f5c0d1a79433c3bcdfc5c9ef720dc1
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-02-01 20:55:17 +0530

    Time: 0 ms (100%), Space: 15.1 MB (30.73%) - LeetHub

--- 1032-satisfiability-of-equality-equations/1032-satisfiability-of-equality-equations.cpp
+++ 1032-satisfiability-of-equality-equations/1032-satisfiability-of-equality-equations.cpp
@@ -2,45 +2,48 @@ class Solution {
 public:
 vector<int>parent;
 vector<int>rank;
-int find(int x){
-if(x==parent[x])
-return x;
-return parent[x]=find(parent[x]);
-}
-void Union(int x,int y){
-    int x_parent=find(x);
-    int y_parent=find(y);
-    if(x_parent==y_parent) return;
-    if(rank[x_parent]>rank[y_parent]){
-        parent[y_parent]=x_parent;
-    }else if(rank[x_parent]<rank[y_parent]){
-           parent[x_parent]=y_parent;
-    }else{
-        parent[x_parent]=y_parent;
-        rank[y_parent]++;
+
+ int find(int i){
+    if(parent[i]!=i){
+        parent[i]=find(parent[i]);
     }
-}
+    return parent[i];
+ }
+
+   void Union(int x,int y){
+    int p_x=find(x);
+    int p_y=find(y);
+    if(p_x==p_y) return;
+    if(p_x!=p_y){
+        if(rank[p_x]>rank[p_y]){
+          parent[p_y]=p_x;
+        }else if(rank[p_y]>rank[p_x]){
+            parent[p_x]=p_y;
+        }else{
+            parent[p_x]=p_y;
+            rank[p_y]++;
+        }
+      }
+   }
+
     bool equationsPossible(vector<string>& equations) {
         parent.resize(26);
-        rank.resize(26,0);
+        rank.resize(26,1);
         for(int i=0;i<26;i++){
             parent[i]=i;
         }
-        for(string &s:equations){
-            if(s[1]=='='){
-                Union(s[0]-'a',s[3]-'a');
-            }
+       for(string &s:equations){
+        if(s[1]=='='){
+            Union(s[0]-'a',s[3]-'a');
         }
+       }
         for(string &s:equations){
-            if(s[1]=='!'){
-                char first=s[0];
-                char second=s[3];
-                int parent_first=find(first-'a');
-                int parent_second=find(second-'a');
-                if(parent_first==parent_second)
+        if(s[1]=='!'){
+            if(find(s[0]-'a')==find(s[3]-'a')){
                 return false;
             }
-        }
-        return true;
+         }
+       }
+       return true;
     }
 };
