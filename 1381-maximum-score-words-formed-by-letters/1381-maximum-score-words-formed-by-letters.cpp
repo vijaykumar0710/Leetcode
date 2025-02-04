15435fcda84fde38c50080df64cbcd615491d7e5
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-02-05 01:04:03 +0530

    Time: 0 ms (100%), Space: 18.3 MB (51.78%) - LeetHub

---
+++ 1381-maximum-score-words-formed-by-letters/1381-maximum-score-words-formed-by-letters.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+int n,maxScore;
+   void solve(int i,vector<int>&score,vector<string>& words,int currScore,vector<int>&freq){
+    maxScore=max(maxScore,currScore);
+    if(i>=n) return;
+    vector<int>tempFreq=freq;
+
+    int j=0;
+    int tempScore=0;
+    while(j<words[i].length()){
+        char ch=words[i][j];
+        tempFreq[ch-'a']--;
+        tempScore+=score[ch-'a'];
+        if(tempFreq[ch-'a']<0) break;
+        j++;
+    }
+    if(j==words[i].length()){
+        solve(i+1,score,words,currScore+tempScore,tempFreq);
+    }
+    solve(i+1,score,words,currScore,freq);
+   }
+    int maxScoreWords(vector<string>& words, vector<char>& letters, vector<int>& score) {
+        vector<int>freq(26,0);
+        for(char &ch:letters){
+            freq[ch-'a']++;
+        }
+          n=words.size();
+          maxScore=INT_MIN;
+          solve(0,score,words,0,freq);
+          return maxScore;
+    }
+};
