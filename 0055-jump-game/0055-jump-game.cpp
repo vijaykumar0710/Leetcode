b1383b3d6373bd297aa9b5f321d56875fae668ab
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-02-04 19:23:44 +0530

    Time: 628 ms (6.97%), Space: 53.1 MB (16.93%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,12 +1,22 @@
 class Solution {
 public:
+int t[10001];
+bool canjump(vector<int>&nums,int i,int n){
+    if(i>=n-1){
+        return true;
+    }
+    if(nums[i]==0) return false;
+    if(t[i]!=-1) return t[i];
+    for(int j=i+1;j<=i+nums[i];j++){
+        if(canjump(nums,j,n)){
+            return t[i]=true;
+        }
+    }
+    return t[i]=false;
+}
     bool canJump(vector<int>& nums) {
         int n=nums.size();
-        int max_go=0;
-        for(int i=0;i<n;i++){
-          if(i>max_go) return false;
-          max_go=max(max_go,nums[i]+i);
-        }
-        return true;
+        memset(t,-1,sizeof(t));
+        return canjump(nums,0,n);
     }
 };
