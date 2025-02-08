018c1c24d8001698834fafe4e3b52dd40ae0dd57
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-02-08 22:03:45 +0530

    Time: 0 ms (100%), Space: 7.8 MB (53.21%) - LeetHub

---
+++ 3371-harshad-number/3371-harshad-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int sumOfTheDigitsOfHarshadNumber(int x) {
+      int n=x;
+      int sum=0;
+      while(n){
+        sum+=n%10;
+        n/=10;
+      }  
+      if(x%sum==0){
+        return sum;
+      }
+      return -1;
+    }
+};
