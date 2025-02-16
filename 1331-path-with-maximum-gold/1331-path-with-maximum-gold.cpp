558bd0e9efb12712e38a70f3b79368d5a9603835
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-02-16 12:24:02 +0530

    Time: 142 ms (46.94%), Space: 10 MB (83.74%) - LeetHub

--- 1331-path-with-maximum-gold/1331-path-with-maximum-gold.cpp
+++ 1331-path-with-maximum-gold/1331-path-with-maximum-gold.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
 int m,n;
-int res;
 vector<vector<int>>directions={{0,1},{0,-1},{1,0},{-1,0}};
-void backtrack(vector<vector<int>>& grid,int i,int j,int sum){
+void backtrack(vector<vector<int>>& grid,int i,int j,int sum,int &res){
     if(i<0 || i>=m || j<0 || j>=n || grid[i][j]==0){
            res=max(res,sum);
            return;
@@ -14,18 +13,18 @@ void backtrack(vector<vector<int>>& grid,int i,int j,int sum){
     for(auto &dir:directions){
         int x=i+dir[0];
         int y=j+dir[1];
-        backtrack(grid,x,y,sum);
+        backtrack(grid,x,y,sum,res);
     }
     grid[i][j]=gold;
 }
     int getMaximumGold(vector<vector<int>>& grid) {
         m=grid.size();
         n=grid[0].size();
-        res=0;
+        int res=0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
               if(grid[i][j]!=0){
-                backtrack(grid,i,j,0);
+                backtrack(grid,i,j,0,res);
                } 
             }
         }
