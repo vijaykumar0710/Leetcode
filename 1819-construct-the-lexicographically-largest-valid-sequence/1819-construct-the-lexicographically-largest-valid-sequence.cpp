5c39ceb0852f3f5ed1c02fa0a1e98b53bf4903b8
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-02-16 12:34:33 +0530

    Time: 0 ms (100%), Space: 8.7 MB (36.8%) - LeetHub

--- 1819-construct-the-lexicographically-largest-valid-sequence/1819-construct-the-lexicographically-largest-valid-sequence.cpp
+++ 1819-construct-the-lexicographically-largest-valid-sequence/1819-construct-the-lexicographically-largest-valid-sequence.cpp
@@ -3,35 +3,31 @@ public:
     int m;
     
     bool backtrack(vector<int>& res, vector<bool>& used, int idx, int n) {
-        if (idx == m) return true; // If we filled the sequence, return true
+        if (idx == m) return true;
         
-        if (res[idx] != 0) return backtrack(res, used, idx + 1, n); // Skip filled positions
+        if (res[idx] != 0) return backtrack(res, used, idx + 1, n); 
         
-        for (int num = n; num >= 1; num--) { // Place larger numbers first
+        for (int num = n; num >= 1; num--) { 
             if (!used[num]) {
-                int j = (num > 1) ? idx + num : idx; // Find second position
-                
-                if (j < m && res[j] == 0) { // Check if placement is valid
+                int j = (num > 1) ? idx + num : idx; 
+                if (j < m && res[j] == 0) { 
                     res[idx] = num;
                     res[j] = num;
                     used[num] = true;
-
-                    if (backtrack(res, used, idx + 1, n)) return true; // Found valid sequence
-                    
-                    // Undo (Backtrack)
+                    if (backtrack(res, used, idx + 1, n)) return true; 
                     res[idx] = 0;
                     res[j] = 0;
                     used[num] = false;
                 }
             }
         }
-        return false; // No valid placement found
+        return false; 
     }
 
     vector<int> constructDistancedSequence(int n) {
         m = 2 * n - 1;
         vector<int> res(m, 0);
-        vector<bool> used(n + 1, false); // Track used numbers
+        vector<bool> used(n + 1, false); 
 
         backtrack(res, used, 0, n);
         return res;
