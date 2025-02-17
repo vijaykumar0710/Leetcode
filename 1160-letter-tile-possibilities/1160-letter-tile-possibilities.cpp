550c585055b6c770b37d86412a8b93598ff84cb0
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-02-17 17:50:19 +0530

    Time: 742 ms (5.23%), Space: 18 MB (25.71%) - LeetHub

---
+++ 1160-letter-tile-possibilities/1160-letter-tile-possibilities.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+ void backtrack(string &tiles,int idx,unordered_set<string>&st,int n,string &curr,vector<bool>&used){
+    if(idx==n){
+        st.insert(curr);
+        return;
+    } 
+    for(int i=0;i<n;i++){
+     if(used[i]==false){
+        used[i]=true;
+        backtrack(tiles,idx+1,st,n,curr,used);
+        used[i]=false;
+     }
+      if(used[i]==false){ 
+      curr+=tiles[i];
+      used[i]=true;
+      backtrack(tiles,idx+1,st,n,curr,used);
+      used[i]=false;
+      curr.pop_back();
+      }
+    }
+ }
+    int numTilePossibilities(string tiles) {
+        int n=tiles.length();
+        unordered_set<string>st;
+        vector<bool>used(n,false);
+        string curr;
+        backtrack(tiles,0,st,n,curr,used);
+        for(auto &word:st){
+            cout<<word<<" ";
+        }
+        return st.size()-1;
+    }
+};
