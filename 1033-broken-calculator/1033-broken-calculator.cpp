892327dc76e04b2a8021c1598569bbe38add12b3
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-02-19 18:07:50 +0530

    Time: 2 ms (11.63%), Space: 8.5 MB (0.63%) - LeetHub

---
+++ 1033-broken-calculator/1033-broken-calculator.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+   unordered_map<int, int> dp;
+    int solve(int x, int y) {
+        if (x >= y) return x - y; // Direct decrements
+        if (dp.count(y)) return dp[y];
+
+        if (y % 2 == 0)
+            return dp[y] = 1 + solve(x, y / 2);
+        else
+            return dp[y] = 1 + solve(x, y + 1);
+    }
+    int brokenCalc(int startValue, int target) {
+        return solve(startValue, target);
+    }
+};
