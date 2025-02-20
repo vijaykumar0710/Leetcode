933f9b5779b6e356c5f7fbb92902dcf05604b515
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-02-20 09:22:53 +0530

    Time: 0 ms (100%), Space: 13.1 MB (38.12%) - LeetHub

--- 2107-find-unique-binary-string/2107-find-unique-binary-string.cpp
+++ 2107-find-unique-binary-string/2107-find-unique-binary-string.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
 string backtrack(string &num,int idx,unordered_set<string>&st){
-        if(st.find(num)==st.end()){
-            return num;
-    }
+    //     if(st.find(num)==st.end()){
+    //         return num;
+    // }
     for(int i=idx;i<num.size();i++){
         if(num[i]=='0'){
             num[i]='1';
             if(st.find(num)==st.end()){
             return num;
-    }
+        }
             backtrack(num,idx+1,st);
         }
          if(num[i]=='1'){
             num[i]='0';
             if(st.find(num)==st.end()){
             return num;
-    }
+       }
             backtrack(num,idx+1,st);
         }
     }
