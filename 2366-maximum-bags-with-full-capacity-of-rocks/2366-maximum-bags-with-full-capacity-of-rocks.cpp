8db906413597a67f4e02aeb85aa75da2131b546c
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-02-24 01:37:48 +0530

    Time: 4 ms (99.48%), Space: 90.4 MB (87.63%) - LeetHub

--- 2366-maximum-bags-with-full-capacity-of-rocks/2366-maximum-bags-with-full-capacity-of-rocks.cpp
+++ 2366-maximum-bags-with-full-capacity-of-rocks/2366-maximum-bags-with-full-capacity-of-rocks.cpp
@@ -5,7 +5,7 @@ public:
         long long capacitySum=accumulate(begin(capacity),end(capacity),0LL);
         long long rocksSun=accumulate(begin(rocks),end(rocks),0LL);
         if(rocksSun+additionalRocks>capacitySum) return n;
-        vector<long long >helper(n);
+        vector<int>helper(n);
         for(int i=0;i<n;i++){
             helper[i]=capacity[i]-rocks[i];
         }
@@ -15,7 +15,7 @@ public:
             if(additionalRocks>=helper[i]){
                 cnt++;
                 additionalRocks-=helper[i];
-                if(additionalRocks<0) break;
+                //if(additionalRocks<0) break;
             }
         }
         return cnt;
