a6b74b3ab885e31827dc74178794b0ac1e165f23
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-02-26 00:16:52 +0530

    Time: 16 ms (80.72%), Space: 70.6 MB (2.01%) - LeetHub

--- 1930-maximum-number-of-consecutive-values-you-can-make/1930-maximum-number-of-consecutive-values-you-can-make.cpp
+++ 1930-maximum-number-of-consecutive-values-you-can-make/1930-maximum-number-of-consecutive-values-you-can-make.cpp
@@ -3,7 +3,6 @@ public:
     int getMaximumConsecutive(vector<int>& coins) {
         int n=coins.size();
         sort(begin(coins),end(coins));
-        if(coins[0]!=1) return 1;
         int sum=0;
         for(int i=0;i<n;i++){
             if(coins[i]<=sum+1){
