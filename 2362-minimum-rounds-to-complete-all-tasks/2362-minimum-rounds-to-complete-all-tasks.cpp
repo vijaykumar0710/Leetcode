f1bf204a73499b969dd3b9345b49e58a6bd1deb8
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-02-26 02:46:37 +0530

    Time: 32 ms (54.41%), Space: 107.6 MB (58.56%) - LeetHub

--- 2362-minimum-rounds-to-complete-all-tasks/2362-minimum-rounds-to-complete-all-tasks.cpp
+++ 2362-minimum-rounds-to-complete-all-tasks/2362-minimum-rounds-to-complete-all-tasks.cpp
@@ -7,7 +7,7 @@ public:
             mp[num]++;
         }
         int minRounds=0;
-           for(auto [task,freq]:mp){ 
+           for(auto [_,freq]:mp){ 
               if(freq==1)return -1;
                minRounds+=(freq/3);
                if(freq%3!=0){
