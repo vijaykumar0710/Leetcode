549d2f2f5806cbe190099bba844998c13ea10cc9
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-02-25 22:47:02 +0530

    Time: 0 ms (100%), Space: 29.7 MB (93.21%) - LeetHub

---
+++ 3494-minimum-cost-for-cutting-cake-i/3494-minimum-cost-for-cutting-cake-i.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int minimumCost(int m, int n, vector<int>& horizontalCut, vector<int>& verticalCut) {
+        int x=horizontalCut.size();
+        int y=verticalCut.size();
+        sort(begin(horizontalCut),end(horizontalCut),greater<int>());
+        sort(begin(verticalCut),end(verticalCut),greater<int>());
+        long long HCut=1,VCut=1;
+        int i=0,j=0;
+        long long cost=0;
+        while(i<x && j<y){
+            if(horizontalCut[i]>=verticalCut[j]){
+              cost+=(VCut*horizontalCut[i]);
+              HCut++;
+              i++;
+            }else{
+                cost+=(HCut*verticalCut[j]);
+                VCut++;
+                j++;
+            }
+        }
+            while(j<y){ 
+            cost+=(HCut*verticalCut[j]);
+                VCut++;
+                j++;
+            }
+            while(i<x){ 
+             cost+=(VCut*horizontalCut[i]);
+              HCut++;
+              i++;
+        }
+        return cost;
+    }
+};
