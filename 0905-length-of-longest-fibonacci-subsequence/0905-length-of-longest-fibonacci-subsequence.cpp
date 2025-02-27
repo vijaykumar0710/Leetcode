8c835a25493a326d48fcf6f8c033e06c6f42d307
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-02-27 23:09:28 +0530

    Time: 340 ms (41.45%), Space: 149.9 MB (29.91%) - LeetHub

--- 0905-length-of-longest-fibonacci-subsequence/0905-length-of-longest-fibonacci-subsequence.cpp
+++ 0905-length-of-longest-fibonacci-subsequence/0905-length-of-longest-fibonacci-subsequence.cpp
@@ -1,29 +1,22 @@
 class Solution {
 public:
-int t[1001][1001];
-int solve(int j,int k,vector<int>&arr,unordered_map<int,int>&mp){
-    if(t[j][k]!=-1) return t[j][k];
-    int target=arr[k]-arr[j];
-    if(mp.count(target) && mp[target]<j){
-        int i=mp[target];
-        return t[j][k]=solve(i,j,arr,mp)+1;
-    }
-    return t[j][k]=2;
-}
     int lenLongestFibSubseq(vector<int>& arr) {
        int n=arr.size();
        unordered_map<int,int>mp;
        for(int i=0;i<n;i++){
         mp[arr[i]]=i;
        }
-      memset(t,-1,sizeof(t));
+
+       vector<vector<int>>t(n,vector<int>(n,2));
        int max_len=0;
        for(int j=1;j<n;j++){
         for(int k=j+1;k<n;k++){
-            int len=solve(j,k,arr,mp);
-            if(len>=3){
-              max_len=max(max_len,len);
+         int target=arr[k]-arr[j];
+        if(mp.count(target) && mp[target]<j){
+        int i=mp[target];
+         t[j][k]=t[i][j]+1;
             }
+            max_len=max(max_len,t[j][k]);
           }
        } 
        return max_len>=3?max_len:0;
