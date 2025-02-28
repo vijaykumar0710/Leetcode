550727cbf4c9e558c6ba50daa0d9efbb0c9a67f2
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-03-01 03:23:49 +0530

    Time: 0 ms (100%), Space: 15.6 MB (43.19%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,17 +1,29 @@
 class Solution {
 public:
-int n;
-int solve(vector<int>&nums,int i,long target,long long sum,unordered_map<string ,int>&mp){
-    if (i == n) { 
-    return sum == target ? 1 : 0; 
-}
-   string key=to_string(i)+"_"+to_string(sum);
-   if(mp.count(key)) return mp[key];
-   return mp[key]=solve(nums,i+1,target,(sum+nums[i]),mp)+solve(nums,i+1,target,(sum-nums[i]),mp);   
+int knapsack(vector<int>&nums,int s1,int n){
+    vector<vector<int>>t(n+1,vector<int>(s1+1));
+    for(int i=0;i<n+1;i++){
+        for(int j=0;j<s1+1;j++){
+            if(i==0) t[i][j]=0;
+            if(j==0) t[i][j]=1;
+        }
+    }
+    for(int i=1;i<n+1;i++){
+        for(int j=0;j<s1+1;j++){
+            if(nums[i-1]<=j){
+                t[i][j]=(t[i-1][j-nums[i-1]] + t[i-1][j]);
+            }else{
+                t[i][j]=t[i-1][j];
+            }
+        }
+    }
+    return t[n][s1];
 }
     int findTargetSumWays(vector<int>& nums, int target) {
-        n=nums.size();
-        unordered_map<string ,int>mp;
-        return solve(nums,0,target,0,mp);
+        int n=nums.size();
+        int sum=accumulate(begin(nums),end(nums),0);
+        if(target>sum || sum+target<0 || (sum+target)%2!=0) return 0;
+        int s1=(target+sum)/2;
+        return knapsack(nums,s1,n);
     }
 };
