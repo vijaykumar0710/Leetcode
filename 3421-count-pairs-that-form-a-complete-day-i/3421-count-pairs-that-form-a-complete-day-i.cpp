0ada4fdba402c72f72ca6a967919eb40739eae07
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-03-02 00:19:46 +0530

    Time: 3 ms (17.55%), Space: 24.7 MB (8.15%) - LeetHub

---
+++ 3421-count-pairs-that-form-a-complete-day-i/3421-count-pairs-that-form-a-complete-day-i.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int countCompleteDayPairs(vector<int>& hours) {
+        vector<long long>vec;
+    for (int num : hours) {
+        vec.push_back(num%24);
+    }
+    unordered_map<long long,long long>mp;
+    int n=vec.size();
+    long long cnt=0;
+    for(int i=n-1;i>=0;i--){
+      if(vec[i]!=0 && mp.find(24-vec[i])!=mp.end()){
+        cnt+=mp[24-vec[i]];
+       }
+      if(vec[i]==0 && mp.find(vec[i])!=mp.end()){
+        cnt+=mp[vec[i]];
+       }
+       mp[vec[i]]++;
+      }
+      return cnt;
+    }
+};
