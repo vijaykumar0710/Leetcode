e6e9abdccc2e0b63cd12a2888c1a3dcbe0cf5195
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-03-02 01:40:55 +0530

    Time: 38 ms (57.98%), Space: 83.3 MB (57.19%) - LeetHub

--- 1620-check-if-array-pairs-are-divisible-by-k/1620-check-if-array-pairs-are-divisible-by-k.cpp
+++ 1620-check-if-array-pairs-are-divisible-by-k/1620-check-if-array-pairs-are-divisible-by-k.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     bool canArrange(vector<int>& arr, int k) {
-        unordered_map<int,int> mp;
-        for(int ele : arr){
-            ele=((ele%k)+k)%k;
-            mp[ele]++;
+        int m=arr.size();
+        unordered_map<int,int>mp;
+        for(auto &num:arr){
+            mp[((num % k) + k) % k]++;
         }
-        if(mp.find(0)!=mp.end()){
-        if(mp[0]%2!=0) return false;
-        mp.erase(0);
-        }
-        for(auto x:mp){
-            int ele=x.first;
-            int rem=k-ele;
-            if(mp.find(rem)==mp.end()) return false;
-            if(mp[ele]!=mp[rem]) return false;
+        for(auto &[rem,freq]:mp){
+            if(rem==0) {
+                if(freq%2!=0)
+                return false;
+            }
+            else{
+                if(mp[k-rem]!=mp[rem]){
+                    return false;
+                }
+            }
         }
         return true;
     }
