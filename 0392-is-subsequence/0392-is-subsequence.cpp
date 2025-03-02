d88ce4e1b880132b00e088d1d2f8436504c9a7ee
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-03-03 00:33:25 +0530

    Time: 3 ms (5.11%), Space: 10.5 MB (5.32%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isSubsequence(string s1, string s2) {
+       int m=s1.length();
+       int  n=s2.length();
+      vector<vector<int>> t(m+1,vector<int>(n+1));
+      for(int row=0;row<m+1;row++){
+        t[row][0]=0;
+      }
+      for(int col=0;col<n+1;col++){
+        t[0][col]=0;
+      }
+    for(int i=1;i<m+1;i++){
+        for(int j=1;j<n+1;j++){
+            if(s1[i-1]==s2[j-1]){
+                t[i][j]=1+t[i-1][j-1];
+            }else{
+                t[i][j]=max(t[i-1][j],t[i][j-1]);
+            }
+        }
+      }
+      return t[m][n]==m;
+    }
+};
