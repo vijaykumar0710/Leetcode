82de2778e183d8e159577ebfc4f0a4b32389834a
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-03-02 19:58:36 +0530

    Time: 23 ms (83.54%), Space: 26.8 MB (66.09%) - LeetHub

--- 1170-shortest-common-supersequence/1170-shortest-common-supersequence.cpp
+++ 1170-shortest-common-supersequence/1170-shortest-common-supersequence.cpp
@@ -1,46 +1,44 @@
 class Solution {
 public:
-    string shortestCommonSupersequence(string str1, string str2) {
-        int m=str1.size();
-        int n=str2.size();
-        vector<vector<int>>t(m+1,vector<int>(n+1));
-        for(int i=0;i<m+1;i++){
-            for(int j=0;j<n+1;j++){
-                if(i==0 || j==0){
-                    t[i][j]=i+j;
-                }else if(str1[i-1]==str2[j-1]){
-                    t[i][j]=1+t[i-1][j-1];
-                }else{
-                    t[i][j]=1+min(t[i][j-1],t[i-1][j]);
-                }
+    string shortestCommonSupersequence(string s1, string s2) {
+         int m=s1.length();
+       int  n=s2.length();
+      vector<vector<int>> t(m+1,vector<int>(n+1,0));
+     for(int i=1;i<m+1;i++){
+        for(int j=1;j<n+1;j++){
+            if(s1[i-1]==s2[j-1]){
+                t[i][j]=1+t[i-1][j-1];
+            }else{
+                t[i][j]=max(t[i-1][j],t[i][j-1]);
             }
         }
-            string result="";
-            int i=m,j=n;
-            while(i>0 && j>0){
-                if(str1[i-1]==str2[j-1]){
-                    result+=str1[i-1];
-                    i--;
-                    j--;
-                }else{
-                    if(t[i-1][j]<t[i][j-1]){
-                        result+=str1[i-1];
-                        i--;
-                    }else{
-                        result+=str2[j-1];
-                        j--;
-                    }
-                }
-            }
-        while(i>0){
-            result+=str1[i-1];
+      }
+      string scs="";
+      int i=m,j=n;
+      while(i>0 && j>0){
+        if(s1[i-1]==s2[j-1]){
+            scs+=s1[i-1];
             i--;
-        }
-         while(j>0){
-            result+=str2[j-1];
             j--;
+        }else{
+            if(t[i-1][j]>t[i][j-1]){
+                scs+=s1[i-1];
+                i--;
+            }else{
+                scs+=s2[j-1];
+                j--;
+            }
         }
-        reverse(result.begin(),result.end());
-        return result;
+      }
+      while(i>0){
+        scs+=s1[i-1];
+          i--;
+      }
+      while(j>0){
+        scs+=s2[j-1];
+          j--;
+      }
+      reverse(begin(scs),end(scs));
+     return scs;
     }
 };
