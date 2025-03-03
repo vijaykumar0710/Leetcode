bb7b2a3e6550e4de810d689b6db1e6a232bde78c
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-03-03 19:10:17 +0530

    Time: 2212 ms (12.25%), Space: 8.9 MB (99.32%) - LeetHub

--- 1871-palindrome-partitioning-iv/1871-palindrome-partitioning-iv.cpp
+++ 1871-palindrome-partitioning-iv/1871-palindrome-partitioning-iv.cpp
@@ -12,10 +12,10 @@ public:
   }
     bool checkPartitioning(string s) {
         int n=s.length();
-         for (int i = 1; i < n - 1; i++) {  // First cut
-            if (isPalindrome(s, 0, i - 1)) {
-                for (int j = i; j < n - 1; j++) {  // Second cut
-                    if (isPalindrome(s, i, j) && isPalindrome(s, j + 1, n - 1)) {
+        for(int i=0;i<n-1;i++){
+            if(isPalindrome(s,0,i)){
+                for(int j=i+1;j<n-1;j++){
+                    if(isPalindrome(s,i+1,j) && (isPalindrome(s,j+1,n-1))){
                         return true;
                     }
                 }
