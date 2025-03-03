70a70ecafeb4a4312906dc223a1ca0ba865530da
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-03-03 17:43:39 +0530

    Time: 1646 ms (22.99%), Space: 26.5 MB (21.89%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -1,29 +1,31 @@
 class Solution {
 public:
-bool isPalindrome(int i,int j,string &s){
-    while(i<j){
-        if(s[i]!=s[j]) return false;
-        i++;
-        j--;
-    }
-    return true;
-} 
-    int minCut(string s) {
-        int n=s.size();
-       vector<int>t(n+1,0);
-       t[n]=0;
-      for(int i=n-1;i>=0;i--){
-      
-      int min_partition=INT_MAX; 
-
-      for(int j=i;j<n;j++){
-      if(isPalindrome(i,j,s)){
-      int partition=1+t[j+1];
-      min_partition=min(min_partition,partition);
-      }
-    }
-     t[i]=min_partition;
+  int t[2001][2001];
+  bool isPalindrome(string &s,int i,int j){
+     while(i<j){
+       if(s[i]!=s[j]){
+        return false;
        }
-        return t[0]-1;
+       i++;
+       j--;
+     }
+      return true;
+  }
+  int solve(string &s,int i,int j){
+    if(t[i][j]!=-1) return t[i][j];
+    if(i>=j || isPalindrome(s,i,j)) return 0;
+     int mini=INT_MAX;
+     for(int k=i;k<j;k++){ 
+        if(isPalindrome(s,i,k)){ 
+        mini=min(mini,1+solve(s,k+1,j));
+         t[i][j]=mini;
+        }
+     }
+     return t[i][j]=mini;
+  }
+    int minCut(string s) {
+        int i=0,j=s.length()-1;
+        memset(t,-1,sizeof(t));
+        return solve(s,i,j);
     }
 };
