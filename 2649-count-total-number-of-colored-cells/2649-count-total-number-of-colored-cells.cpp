553ae9a13958a91e04a802678d107a2d9047d671
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-03-05 18:33:54 +0530

    Time: 0 ms (100%), Space: 7.8 MB (61.42%) - LeetHub

--- 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.cpp
+++ 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     long long coloredCells(int n) {
-        long long ans=2*(long long)n*n-2*(long long)n+1;
-        return ans;
+        return 2*(long long)n*n-2*(long long)n+1;
     }
 };
