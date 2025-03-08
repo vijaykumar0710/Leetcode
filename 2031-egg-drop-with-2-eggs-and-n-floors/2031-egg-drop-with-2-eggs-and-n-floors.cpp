1cc6dea4707621d7dce229df7754b01bdef36f82
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-03-08 09:17:31 +0530

    Time: 3 ms (53.17%), Space: 7.7 MB (99.62%) - LeetHub

--- 2031-egg-drop-with-2-eggs-and-n-floors/2031-egg-drop-with-2-eggs-and-n-floors.cpp
+++ 2031-egg-drop-with-2-eggs-and-n-floors/2031-egg-drop-with-2-eggs-and-n-floors.cpp
@@ -2,33 +2,25 @@ class Solution {
 public:
 int t[3][1001];
 int MCM(int egg,int floor){
-    if(floor==0 || floor==1)
-       return floor;
-    if(egg==1)
-      return floor;
-
+    if(floor==0 || floor==1 || egg==1)
+       return t[egg][floor]=floor;
+    
     if(t[egg][floor]!=-1)
         return t[egg][floor];
 
     int mini=INT_MAX;
-    for(int k=1;k<=floor;k++){
-        int low,high;
-        if(t[egg-1][k-1]!=-1){
-            low=t[egg-1][k-1];
-        }else{
-            low=MCM(egg-1,k-1);
-            t[egg-1][k-1]=low;
-        }
-        if(t[egg][floor-k]!=-1){
-            high=t[egg][floor-k];
-        }else{
-            high=MCM(egg,floor-k);
-            t[egg][floor-k]=high;
-        }
-        int temp=1+max(low,high);
-        mini=min(mini,temp);
-    }
-    return t[egg][floor]=mini;
+    int l=1,r=floor;
+    while(l<=r){
+      int mid=l+(r-l)/2;
+      int below=MCM(egg-1,mid-1);
+      int up=MCM(egg,floor-mid);
+      int temp=1+max(below,up);
+      mini=min(mini,temp);
+      if(below>up) r=mid-1;
+      else l=mid+1;
+    }   
+     t[egg][floor]=mini;
+     return mini;
 }
     int twoEggDrop(int n) {
         memset(t,-1,sizeof(t));
