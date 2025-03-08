4847471ae2b08537a94a478c2427c5d930ae8883
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-03-08 19:51:21 +0530

    Time: 0 ms (100%), Space: 17.5 MB (53.01%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,42 +1,30 @@
 class Solution {
 public:
-int solveLeft(vector<int>& nums,int target,int n){
-    int l=0,r=n-1;
-    int left_most=-1;
-    while(l<=r){
-        int mid=l+(r-l)/2;
-        if(nums[mid]==target){
-            left_most=mid;
-            r=mid-1;
-        }else if(nums[mid]<target){
-            l=mid+1;
-        }else{
-            r=mid-1;
+    vector<int> searchRange(vector<int>& arr, int x) {
+         int n=arr.size();
+        int l=0,r=n-1;
+        int first=-1,last=-1;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(arr[mid]==x){
+                first=mid;
+                r=mid-1;
+            }
+            else if(arr[mid]>x){
+                r=mid-1;
+            }else l=mid+1;
         }
-    }
-    return left_most;
-}
-
-int solveLRight(vector<int>& nums,int target,int n){
-    int l=0,r=n-1;
-    int right_most=-1;
-    while(l<=r){
-        int mid=l+(r-l)/2;
-        if(nums[mid]==target){
-            right_most=mid;
-            l=mid+1;
-        }else if(nums[mid]<target){
-            l=mid+1;
-        }else{
-            r=mid-1;
+         l=0,r=n-1;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(arr[mid]==x){
+                last=mid;
+                l=mid+1;
+            }
+            else if(arr[mid]>x){
+                r=mid-1;
+            }else l=mid+1;
         }
-    }
-    return right_most;
-}
-    vector<int> searchRange(vector<int>& nums, int target) {
-        int n=nums.size();
-        int left_most=solveLeft(nums,target,n);
-        int right_most=solveLRight(nums,target,n);
-        return {left_most,right_most};
+        return {first,last};
     }
 };
