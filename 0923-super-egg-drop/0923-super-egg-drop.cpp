2f4935854401871684b094cb1582ef38c505d9d1
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-03-08 08:54:29 +0530

    Time: 50 ms (58.47%), Space: 12.4 MB (74.78%) - LeetHub

---
+++ 0923-super-egg-drop/0923-super-egg-drop.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+int t[101][10001];
+ int MCM(int eggs,int floors){
+        if(floors==0 || floors==1) return t[eggs][floors]=floors;
+        
+        if(eggs==1) return t[eggs][floors]=floors;
+    
+        if(t[eggs][floors]!=-1) return t[eggs][floors];
+
+          int l=1,h=floors;
+          int mn=INT_MAX;
+          while(l<=h) {
+              int mid=l+(h-l)/2;
+              int a=MCM(eggs-1,mid-1);
+              int b=MCM(eggs,floors-mid);
+              int x=max(a,b) + 1;
+              mn=min(mn,x);
+              if(a>b) h=mid-1;
+              else l=mid+1;
+          }
+         t[eggs][floors]=mn;   
+        return t[eggs][floors];
+    } 
+    int superEggDrop(int k, int n) {
+        memset(t,-1,sizeof(t));
+        return MCM(k,n);
+    }
+};
