5a94c13f2eb64c288bf5a48031f508938dd8456e
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-03-13 13:03:26 +0530

    Time: 54 ms (16.32%), Space: 109.7 MB (5.47%) - LeetHub

--- 2465-shifting-letters-ii/2465-shifting-letters-ii.cpp
+++ 2465-shifting-letters-ii/2465-shifting-letters-ii.cpp
@@ -6,12 +6,7 @@ public:
         for(int i=0;i<n;i++){
             nums[i]=s[i]-'a';
         }
-        vector<int>diffArr(n+1,0);
-        diffArr[0]=nums[0];
-        for(int i=1;i<n;i++){
-         diffArr[i]=nums[i]-nums[i-1];
-        }
-
+        vector<int>diffArr(n,0);
         for(auto query:shifts){
             int l=query[0];
             int r=query[1];
@@ -26,10 +21,14 @@ public:
                    diffArr[r+1]-=1; 
             }
         }
+
          for(int i=1;i<n;i++){
-            diffArr[i]+=diffArr[i-1];
+           if(i>0) diffArr[i]+=diffArr[i-1];
          }
         
+        for(int i=0;i<n;i++){
+         diffArr[i]=nums[i]+diffArr[i];
+         } 
         for (int i = 0; i < n; i++) {
             int shift = ((diffArr[i]+26)%26+ 26) % 26; 
             s[i] = shift + 'a';
