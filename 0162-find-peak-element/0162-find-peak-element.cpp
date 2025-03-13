2ecc6238c0adeee2737f154b8c6d8644d3b3b090
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-03-13 16:43:19 +0530

    Time: 0 ms (100%), Space: 12.3 MB (92.15%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -18,12 +18,12 @@ public:
         else if(mid==0){
             if(arr[0]>arr[1])
             return 0;
-            else return 1;
+            else l=mid+1;
         }
         else if(mid==n-1){
             if(arr[n-1]>arr[n-2])
                return n-1;
-               else return n-2;
+               else r=mid+1;
         }
        }
        return -1;
