68e46dc1ed3c02d761e02b639dd3388b1893bc02
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-03-15 15:22:16 +0530

    Time: 1 ms (74.05%), Space: 10.2 MB (31.15%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,29 +1,37 @@
 class Solution {
 public:
-    bool checkInclusion(string s1, string s2) {
-        int m=s1.size();
-        int n=s2.size();
-
-        if(m>n) return false;
-
-        vector<int>s1_freq(26,0);
-        vector<int>s2_freq(26,0);
-
-        for(char &ch:s1){
-            s1_freq[ch-'a']++;
+    bool checkInclusion(string pat, string txt) {
+        int n=txt.size();
+     int k=pat.size();
+     unordered_map<char,int>mp;
+     for(auto &ch:pat){
+         mp[ch]++;
+     }
+     int i=0,j=0,cnt=mp.size();
+     while(j<n){
+        if(mp.find(txt[j])!=mp.end()){
+          mp[txt[j]]--;
+             if(mp[txt[j]]==0){
+                 cnt--;
+             }
         }
-        int i=0,j=0;
-        while(j<n){
-           s2_freq[s2[j]-'a']++;
-           if(j-i+1>m){
-            s2_freq[s2[i]-'a']--;
-            i++;
-           }
-           if(s1_freq==s2_freq){
-            return true;
-           }
-           j++;
-        }
-        return false;
+         if(j-i+1<k){
+             j++;
+         }
+         else if(j-i+1==k){
+              if(cnt==0){
+                 return true;
+             }
+             if(mp.find(txt[i])!=mp.end()){
+                if(mp[txt[i]]==0){
+                    cnt++;
+                }
+                mp[txt[i]]++;
+             }
+             i++;
+             j++;
+         }
+     }
+     return false; 
     }
 };
