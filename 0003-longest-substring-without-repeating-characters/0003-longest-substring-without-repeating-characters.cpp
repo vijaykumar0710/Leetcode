5cedcf03e819c3fbf49478afcf94cb11a67bc586
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-03-16 20:48:39 +0530

    Time: 10 ms (54.95%), Space: 12.6 MB (40.83%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -10,8 +10,8 @@ public:
            if(mp.size()==(j-i+1)){
               ans=max(ans,(j-i+1));
           }
-          else if(mp.size()!=(j-i+1)){
-              while(mp.size()!=(j-i+1)){
+          else if(mp.size()<(j-i+1)){
+              while(mp.size()<(j-i+1)){
               if(mp.find(s[i])!=mp.end()){
                   mp[s[i]]--;
                   if(mp[s[i]]==0){
