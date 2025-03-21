0ad7781767cc490e497b6f57eaa22ddd52d926ac
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-03-21 14:44:37 +0530

    Time: 792 ms (6.93%), Space: 98.8 MB (99.8%) - LeetHub

---
+++ 2220-find-all-possible-recipes-from-given-supplies/2220-find-all-possible-recipes-from-given-supplies.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<string> findAllRecipes(vector<string>& recipes, vector<vector<string>>& ingredients, vector<string>& supplies) {
+        unordered_set<string> st(supplies.begin(), supplies.end());
+        vector<string> res;
+        unordered_set<string> made; // to avoid rechecking already made recipes
+        bool changed = true;
+
+        while (changed) {
+            changed = false;
+            for (int i = 0; i < recipes.size(); i++) {
+                string recipe = recipes[i];
+                if (st.find(recipe) != st.end()) continue; // already made
+
+                bool canMake = true;
+                for (string& ing : ingredients[i]) {
+                    if (st.find(ing) == st.end()) {
+                        canMake = false;
+                        break;
+                    }
+                }
+
+                if (canMake) {
+                    st.insert(recipe);
+                    res.push_back(recipe);
+                    changed = true;
+                }
+            }
+        }
+        return res;
+    }
+};
