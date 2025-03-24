9ba12a8092e06b08ebc11a0b8a17a3a7d675b23e
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-03-24 13:27:12 +0530

    Time: 1804 ms (5.08%), Space: 313.8 MB (6.35%) - LeetHub

--- 0993-tallest-billboard/0993-tallest-billboard.cpp
+++ 0993-tallest-billboard/0993-tallest-billboard.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-int t[21][10001];
+unordered_map<string,int>dp;
 int solve(vector<int>&rods,int idx,int diff,int n){
     if(idx==n){
      if(diff==0){
@@ -8,15 +8,15 @@ int solve(vector<int>&rods,int idx,int diff,int n){
      }
      return INT_MIN;
     }
-   if(t[idx][diff+5000]!=-1) return t[idx][diff+5000];
+    string key=to_string(idx)+"$"+to_string(diff);
+    if(dp.count(key)) return dp[key];
     int option1=rods[idx]+solve(rods,idx+1,diff+rods[idx],n);
     int option2=rods[idx]+solve(rods,idx+1,diff-rods[idx],n);
     int option3=solve(rods,idx+1,diff,n);
-    return t[idx][diff+5000]=max({option1,option2,option3});
+    return dp[key]=max({option1,option2,option3});
 }
     int tallestBillboard(vector<int>& rods) {
         int n=rods.size();
-        memset(t,-1,sizeof(t));
         return solve(rods,0,0,n)/2;
     }
 };
