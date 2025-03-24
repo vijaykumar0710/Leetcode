56cdf6a1f762b7fb9aadbb4b6d7abad58a7d5793
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-03-24 14:59:06 +0530

    Time: 0 ms (100%), Space: 10 MB (55.21%) - LeetHub

--- 0770-couples-holding-hands/0770-couples-holding-hands.cpp
+++ 0770-couples-holding-hands/0770-couples-holding-hands.cpp
@@ -1,41 +1,27 @@
 class Solution {
 public:
-int minSwapsToMakePairs(vector<int>& row) {
-    int n = row.size();
-    unordered_map<int, int> pos;
-    for(int i = 0; i < n; i++) {
-        pos[row[i]] = i;
-    }
-
-    int swaps = 0;
-    for(int i = 0; i < n; i += 2) {
-        int first = row[i];
-        int expected_pair;
-
-        // We want pair such that difference is 1
-        if (first % 2 == 0)
-            expected_pair = first + 1;
-        else
-            expected_pair = first - 1;
-
-        // If already paired correctly, continue
-        if(row[i+1] == expected_pair) continue;
-
-        // Swap expected pair to the correct position
-        int idx = pos[expected_pair];
-
-        // Update positions in map after swap
-        swap(row[i+1], row[idx]);
-        pos[row[idx]] = idx;
-        pos[row[i+1]] = i+1;
-
-        swaps++;
-    }
-
-    return swaps;
-}
     int minSwapsCouples(vector<int>& row) {
         int n=row.size();
-        return minSwapsToMakePairs(row);
+        unordered_map<int,int>mp;
+        for(int i=0;i<n;i++){
+            mp[row[i]]=i;
+        }
+        int minSwap=0;
+        for(int i=0;i<n;i+=2){
+            int first=row[i];
+            int expected_pair;
+            if(first%2==0)
+              expected_pair=first+1;
+            else
+              expected_pair=first-1;
+            if(expected_pair==row[i+1]) 
+               continue;
+            int idx=mp[expected_pair];
+            swap(row[i+1],row[idx]);
+            mp[row[i+1]]=i+1;
+            mp[row[idx]]=idx;
+            minSwap++;
+        }
+        return minSwap;
     }
 };
