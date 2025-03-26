dd3426814f8f211cc92236ebbd62e997a7d36c01
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-03-27 02:20:32 +0530

    Time: 13 ms (77.15%), Space: 183.3 MB (67.84%) - LeetHub

--- 3744-minimum-operations-to-make-array-elements-zero/3744-minimum-operations-to-make-array-elements-zero.cpp
+++ 3744-minimum-operations-to-make-array-elements-zero/3744-minimum-operations-to-make-array-elements-zero.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
 long long minOperations(vector<vector<int>>& queries) {
     long long res = 0;
-    for (const auto &q : queries) {
+    for ( auto &q : queries) {
         long long l = q[0], r = q[1], sum = 0, p = 0;
         for (long long p4 = 1; p4 <= r; p4 *= 4) { // 1, 4, 16, 64, ...
             int pl = max(l, p4), pr = min(r, p4 * 4 - 1);
