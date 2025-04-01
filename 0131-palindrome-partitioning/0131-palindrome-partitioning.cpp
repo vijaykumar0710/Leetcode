d075012bfb46cd7c59aa225b050fb0f53c2e0bc2
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-04-01 12:33:33 +0530

    Time: 26 ms (96.62%), Space: 53.3 MB (92.42%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,34 +1,32 @@
 class Solution {
 public:
-int n;
-bool isPalindrome(string &s,int l,int r){
-   while(l<r){
-    if(s[l]!=s[r]){
-        return false;
+bool ispalindrome(string &str,int l,int r){
+    while(l<=r){
+        if(str[l]!=str[r])
+           return false;
+           l++;
+           r--;
     }
-    l++;
-    r--;
-   }
-   return true;
+    return true;
 }
-void backtrack(string &s,int idx,vector<string>curr,vector<vector<string>>&result){
-    if(idx==n){
-        result.push_back(curr);
+void solve(string &s,int start,vector<vector<string>>&res,vector<string>&curr,int n){
+    if(start==n){
+        res.push_back(curr);
         return;
     }
-    for(int i=idx;i<n;i++){
-        if(isPalindrome(s,idx,i)){
-            curr.push_back(s.substr(idx,i-idx+1));
-            backtrack(s,i+1,curr,result);
-            curr.pop_back();
-        }
+    for(int i=start;i<n;i++){
+       if(ispalindrome(s,start,i)){
+        curr.push_back(s.substr(start,i-start+1));
+        solve(s,i+1,res,curr,n);
+        curr.pop_back();
+       }
     }
 }
     vector<vector<string>> partition(string s) {
-      n=s.length();
-      vector<vector<string>> result;
-      vector<string>curr;
-      backtrack(s,0,curr,result);
-      return result;  
+        int n=s.size();
+        vector<vector<string>>res;
+        vector<string>curr;
+        solve(s,0,res,curr,n);
+        return res;
     }
 };
