a912565679d3f45ccb00efad40b812236eb49387
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-04-05 00:30:11 +0530

    Time: 1172 ms (6.29%), Space: 9.4 MB (74.63%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,44 +1,29 @@
 class Solution {
 public:
+bool isPalindrome(string &s,int i,int j){
+    int l=i,r=j;
+    while(l<=r){
+        if(s[l]!=s[r]){
+            return false;
+        }
+        l++,r--;
+    }
+    return true;
+}
     string longestPalindrome(string s) {
-         string s1=s;
-        string s2=s;
-        reverse(s2.begin(),s2.end());
-        int m=s1.length();
-       int  n=s2.length();
-      vector<vector<int>> t(m+1,vector<int>(n+1));
-      for(int row=0;row<m+1;row++){
-        t[row][0]=0;
-      }
-      for(int col=0;col<n+1;col++){
-        t[0][col]=0;
-      }
-      
-    for(int i=1;i<m+1;i++){
-        for(int j=1;j<n+1;j++){
-            if(s1[i-1]==s2[j-1]){
-                t[i][j]=1+t[i-1][j-1];
-            }else{
-                t[i][j]=0;
+       int n=s.size();
+       int maxi=0;
+        int l,r;
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                if(isPalindrome(s,i,j)){ 
+                    if(maxi<(j-i+1)){ 
+                        maxi=j-i+1;
+                      l=i,r=j;
+                    }
+                }
             }
         }
-      }
-       string lcs="";
-      int i=m,j=n;
-      while(i>0 && j>0){
-        if(s1[i-1]==s2[j-1]){
-            lcs.push_back(s1[i-1]);
-            i--;
-            j--;
-        }else{
-            if(t[i-1][j]>t[i][j-1]){
-                i--;
-            }else{
-                j--;
-            }
-        }
-      }
-      reverse(begin(lcs),end(lcs));
-      return lcs;
+        return s.substr(l,(r-l+1));
     }
 };
