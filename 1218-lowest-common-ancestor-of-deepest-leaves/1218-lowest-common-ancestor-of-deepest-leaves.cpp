117c2416fee16d34e3d1160581af8ab961059b67
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-04-04 09:32:44 +0530

    Time: 0 ms (100%), Space: 22.2 MB (39.6%) - LeetHub

---
+++ 1218-lowest-common-ancestor-of-deepest-leaves/1218-lowest-common-ancestor-of-deepest-leaves.cpp
@@ -0,0 +1,25 @@
+
+class Solution {
+public:
+pair<int, TreeNode*> dfs(TreeNode* root) {
+        if (root == NULL) return {0, NULL};
+        
+        // Recurse for left and right subtrees
+        auto left = dfs(root->left);
+        auto right = dfs(root->right);
+        
+        // If depths are the same, root is the LCA
+        if (left.first == right.first) {
+            return {left.first + 1, root};
+        }
+        // Return the subtree with greater depth
+        if (left.first > right.first) {
+            return {left.first + 1, left.second};
+        } else {
+            return {right.first + 1, right.second};
+        }
+    }
+    TreeNode* lcaDeepestLeaves(TreeNode* root) {
+         return dfs(root).second;
+    }
+};
