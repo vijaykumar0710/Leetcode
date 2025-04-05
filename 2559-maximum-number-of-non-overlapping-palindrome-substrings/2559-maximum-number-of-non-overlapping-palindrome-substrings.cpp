c65e47aa3b2e0ed8afd1daf9e7ee983dff429593
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-04-05 22:35:46 +0530

    Time: 19 ms (67.91%), Space: 56.7 MB (32.46%) - LeetHub

--- 2559-maximum-number-of-non-overlapping-palindrome-substrings/2559-maximum-number-of-non-overlapping-palindrome-substrings.cpp
+++ 2559-maximum-number-of-non-overlapping-palindrome-substrings/2559-maximum-number-of-non-overlapping-palindrome-substrings.cpp
@@ -1,34 +1,31 @@
 class Solution {
-public:
-int t[2001];
-bool isPalindrome(string &s,int i,int j){
-    int l=i,r=j;
-    while(l<=r){
-        if(s[l]!=s[r])
-         return false;
-        l++;
-        r--;
-    }
-    return true;
-}
-int solve(string &s,int k,int idx,int n){
-    if(idx>=n){
-        return 0;
-    }
-    if(t[idx]!=-1) return t[idx];
-    int skip=solve(s,k,idx+1,n);
-    int take=0;
-    for(int j=idx;j<n;j++){
-        if(j-idx+1>=k && isPalindrome(s,idx,j)){
-            take=1+solve(s,k,j+1,n);
-            break;
+    public:
+    bool isPalindrome(string s, int i, int j){
+        while(i<j){
+            if(s[i]!=s[j]){
+                return false;
+            }
+            i++;
+            j--;
         }
+        return true;
     }
-    return t[idx]=max(take,skip);
-}
     int maxPalindromes(string s, int k) {
         int n=s.size();
-        memset(t,-1,sizeof(t));
-        return solve(s,k,0,n);
+        int ans = 0;
+        for(int i=0; i<n; i++){
+            for(int j=i; j<n; j++){
+                int len= j-i+1;
+                if(len>k+1){
+                    break;
+                }
+                if((len==k||len == k+1) && isPalindrome(s, i, j)){
+                    ans++; 
+                    i=j;
+                    break;
+                }
+            }
+        }
+        return ans;
     }
 };
