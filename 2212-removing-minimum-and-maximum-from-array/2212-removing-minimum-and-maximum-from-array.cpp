faddb3dc78614753d4c9a50906a550f63e1a0c0f
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-04-07 12:46:03 +0530

    Time: 0 ms (100%), Space: 92 MB (55.96%) - LeetHub

---
+++ 2212-removing-minimum-and-maximum-from-array/2212-removing-minimum-and-maximum-from-array.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int minimumDeletions(vector<int>& nums) {
+        int n=nums.size();
+        cout<<n<<endl;
+        int mini=nums[0],min_idx=0;
+        int maxi=nums[0],max_idx=0;
+        for(int i=1;i<n;i++){
+            if(nums[i]<mini){
+                mini=nums[i];
+                min_idx=i;
+            }
+            if(nums[i]>maxi){
+                maxi=nums[i];
+                max_idx=i;
+            }
+        }
+        cout<<max_idx<<endl;
+        cout<<min_idx;
+        if(min_idx<n/2 && max_idx<n/2){
+            return max(min_idx,max_idx)+1;
+        }
+       else if(min_idx>n/2 && max_idx>n/2){
+            return n-min(min_idx,max_idx);
+        }
+        else if(max_idx==n/2 || min_idx==n/2){
+            if(n%2==0 && (max_idx>n/2 || min_idx>n/2)) return n/2;
+            else return (n/2)+1;
+        }
+        else{
+            int t1=min(min_idx,max_idx)+1+n-max(min_idx,max_idx);
+            int t2=max(min_idx,max_idx)+1;
+            int t3=n-min(min_idx,max_idx);
+            return min({t1,t2,t3});
+        }
+        return -1;
+    }
+};
