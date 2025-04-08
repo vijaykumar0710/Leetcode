8665dedc3cc0a4b1b05301867546d6ddb8dd1071
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-04-08 22:14:28 +0530

    Time: 3 ms (77.19%), Space: 15.7 MB (50.8%) - LeetHub

--- 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.cpp
+++ 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.cpp
@@ -1,73 +1,69 @@
 class Solution {
 public:
-   long long sumOfMinimums(vector<int>& heights) {
-    int n = heights.size();
-    stack<int> st;
-    vector<int> pse(n), nse(n);
-
-    for (int i = 0; i < n; ++i) {
-        while (!st.empty() && heights[st.top()] >= heights[i]) {
-            st.pop();
-        }
-        pse[i] = st.empty() ? -1 : st.top();
-        st.push(i);
+long long maxi(vector<int>&nums){
+    int n=nums.size();
+    stack<int>st;
+    vector<int>pge(n),nge(n);
+    
+    for(int i=0;i<n;i++){
+      while(!st.empty() && nums[st.top()]<=nums[i]){
+        st.pop();
+      }
+      pge[i]=st.empty()?-1:st.top();
+      st.push(i);
     }
-
+   
     while (!st.empty()) st.pop();
 
-    for (int i = n - 1; i >= 0; --i) {
-        while (!st.empty() && heights[st.top()] > heights[i]) {
-            st.pop();
-        }
-        nse[i] = st.empty() ? n : st.top();
-        st.push(i);
+    for(int i=n-1;i>=0;i--){
+      while(!st.empty() && nums[st.top()]<nums[i]){
+        st.pop();
+      }
+      nge[i]=st.empty()?n:st.top();
+      st.push(i);
     }
-
-    long long res = 0;
-    for (int i = 0; i < n; ++i) {
-        long long left = i - pse[i];
-        long long right = nse[i] - i;
-        res = (res + heights[i] * left * right );
-    }
-
-    return res;
+   long long sum=0;
+   for(int i=0;i<n;i++){
+    long long left=i-pge[i];
+    long long right=nge[i]-i;
+    sum+=(nums[i]*left*right);
+   }
+   return sum;
 }
 
-    long long sumOfMaximums(vector<int>& heights) {
-        int n = heights.size();
-        vector<int> pge(n), nge(n);
-        stack<int> st;
-
-        for (int i = 0; i < n; i++) {
-            while (!st.empty() && heights[st.top()] <= heights[i]) {
-                st.pop();
-            }
-            pge[i] = st.empty() ? -1 : st.top();
-            st.push(i);
-        }
-
-        while (!st.empty()) st.pop();
-
-        for (int i = n - 1; i >= 0; i--) {
-            while (!st.empty() && heights[st.top()] < heights[i]) {
-                st.pop();
-            }
-            nge[i] = st.empty() ? n : st.top();
-            st.push(i);
-        }
-
-        long long res = 0;
-        for (int i = 0; i < n; i++) {
-            long long left = i - pge[i];
-            long long right = nge[i] - i;
-            res = (res + 1LL * heights[i] * left * right);
-        }
-        return res;
+long long mini(vector<int>&nums){
+    int n=nums.size();
+    stack<int>st;
+    vector<int>pse(n),nse(n);
+    
+    for(int i=0;i<n;i++){
+      while(!st.empty() && nums[st.top()]>=nums[i]){
+        st.pop();
+      }
+      pse[i]=st.empty()?-1:st.top();
+      st.push(i);
     }
+   
+    while (!st.empty()) st.pop();
 
+    for(int i=n-1;i>=0;i--){
+      while(!st.empty() && nums[st.top()]>nums[i]){
+        st.pop();
+      }
+      nse[i]=st.empty()?n:st.top();
+      st.push(i);
+    }
+   long long sum=0;
+   for(int i=0;i<n;i++){
+    long long left=i-pse[i];
+    long long right=nse[i]-i;
+    sum+=(nums[i]*left*right);
+   }
+   return sum;
+}
     long long subArrayRanges(vector<int>& nums) {
-        long long max_sum = sumOfMaximums(nums);
-        long long min_sum = sumOfMinimums(nums);
-        return (max_sum - min_sum);
+        long long max_sum=maxi(nums);
+        long long min_sum=mini(nums);
+        return max_sum-min_sum;
     }
 };
