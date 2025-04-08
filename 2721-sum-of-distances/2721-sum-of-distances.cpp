29e1048e7ad35bf7e6570e20413657927ee56a02
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-04-08 12:15:10 +0530

    Time: 46 ms (90.72%), Space: 118.7 MB (45.48%) - LeetHub

--- 2721-sum-of-distances/2721-sum-of-distances.cpp
+++ 2721-sum-of-distances/2721-sum-of-distances.cpp
@@ -1,32 +1,38 @@
 class Solution {
 public:
-    vector<long long> distance(vector<int>& nums) 
-    {
+    void solve(vector<long long>& indices, vector<long long>& ans) {
+        int n = indices.size();
+        vector<long long> prefix(n);
+        vector<long long> suffix(n);
+        prefix[0] = indices[0];
+        suffix[n - 1] = indices[n - 1];
+        for (int i = 1; i < n; i++) {
+            prefix[i] = prefix[i - 1] + indices[i];
+        }
+        for (int i = n - 2; i >= 0; i--) {
+            suffix[i] = suffix[i + 1] + indices[i];
+        }
+        ans[indices[0]] = suffix[1] - (n - 1) * indices[0];
+        ans[indices[n - 1]] = (n - 1) * indices[n - 1] - prefix[n - 2];
+        for (int i = 1; i < n - 1; i++) {
+            long long left = i * indices[i] - prefix[i - 1];
+            long long right = suffix[i + 1] - (n - i - 1) * indices[i];
+            ans[indices[i]] = left + right;
+        }
+    }
+
+    vector<long long> distance(vector<int>& nums) {
         int n = nums.size();
-        vector<long long>ans(n);
-        unordered_map<int, vector<int>>mp;
-        for (int i = 0; i < n; i++) mp[nums[i]].push_back(i);
-        for (auto it : mp)
-        {
-            int num = it.first;
-            vector<int>& indexes = it.second;
-            long long totalSum = 0;
-            for (int index : indexes) totalSum += (long long)index;
-            
-            long long preSum = 0;
-            for (int i = 0; i < indexes.size(); i++)
-            {
-                int index = indexes[i];
-                long long postSum = totalSum - preSum - index;
-                
-                ans[index] += (index * (long long)i);
-                ans[index] -= (preSum);
-                ans[index] -= (index * (long long)(indexes.size() - i - 1));
-                ans[index] += (postSum);
-                
-                preSum += index;
+        unordered_map<long long, vector<long long>> mp;
+        vector<long long> ans(n, 0);
+        for (int i = 0; i < n; i++) {
+            mp[nums[i]].push_back(i);
+        }
+        for (auto& [num, vec] : mp) {
+            if (vec.size() > 1) {
+                solve(vec, ans);
             }
         }
-        return ans;   
+        return ans;
     }
 };
