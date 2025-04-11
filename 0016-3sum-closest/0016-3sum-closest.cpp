a23202456cf33435a93360012619172791f2fc1e
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-04-11 15:05:08 +0530

    Time: 16 ms (26.56%), Space: 13.9 MB (92.64%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.cpp
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -1,21 +1,29 @@
 class Solution {
 public:
-    int threeSumClosest(vector<int>& nums, int target) {
-       int n=nums.size();
-       int mini=INT_MAX;
-       int res=0;
-       for(int i=0;i<n;i++){
-        for(int j=i+1;j<n;j++){
-            for(int k=j+1;k<n;k++){
-               int sum=nums[i]+nums[j]+nums[k];
-               int diff=abs(sum-target);
-               if(diff<mini){
-                mini=diff;
-                res=sum;
-               }
+int res=0;
+int mini=INT_MAX;
+ int twoSum(vector<int>& nums, int k,int x,int target) {
+        int i = k, j = nums.size()-1;
+        while(i < j) {
+            int diff=abs((nums[i]+nums[j]-x));
+                if(diff<mini){
+                    mini=diff;
+                    res=nums[i]+nums[j]+(target-x);
+                }
+            if(nums[i]+nums[j] > x) j--;
+            else if(nums[i] + nums[j] < x) i++;
+            else {
+               return target;
             }
         }
-       } 
-       return res;
+        return -1;
+    }
+    int threeSumClosest(vector<int>& nums, int target) {
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+         for(int i = 0; i<nums.size()-2; i++) {  //(i<nums.size()-2)Things you must not forget in interviews
+            twoSum(nums, i+1, target-nums[i],target);
+        }
+        return res;
     }
 };
