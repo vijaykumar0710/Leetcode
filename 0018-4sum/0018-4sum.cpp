9304fefba99d9b313a67db32d74d9ae2ab16e826
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-04-11 17:43:16 +0530

    Time: 17 ms (83.85%), Space: 18.4 MB (19.87%) - LeetHub

---
+++ 0018-4sum/0018-4sum.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+void twoSum(vector<int>&nums,int k,long long x,vector<vector<int>>&res,int first,int second){
+    int i=k,j=nums.size()-1;
+    while(i<j){
+         long long sum=1LL*nums[i]+nums[j];
+        if(sum<x) i++;
+        else if(sum>x) j--;
+        else{
+            res.push_back({nums[i],nums[j],nums[first],nums[second]});
+            while(i<j && nums[i]==nums[i+1]) i++;
+            while(i<j && nums[j]==nums[j-1]) j--;
+            i++;
+            j--;
+        }
+    }
+}
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        vector<vector<int>>res;
+        if(n<4) return res;
+        for(int i=0;i<n;i++){
+            if(i!=0 && nums[i]==nums[i-1]) continue;
+            for(int j=i+1;j<n;j++){
+                if(j>i+1 && nums[j]==nums[j-1]) continue;
+                 long long x=1LL*target-(nums[i]+nums[j]);
+               twoSum(nums,j+1,x,res,i,j);
+            }
+        }
+        return res;
+    }
+};
