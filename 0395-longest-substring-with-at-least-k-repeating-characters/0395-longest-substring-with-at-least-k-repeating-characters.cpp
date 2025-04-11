6b680567dd433fb4d4f630a446c358107036b39b
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-04-12 02:10:45 +0530

    Time: 323 ms (22.49%), Space: 62 MB (11.28%) - LeetHub

--- 0395-longest-substring-with-at-least-k-repeating-characters/0395-longest-substring-with-at-least-k-repeating-characters.cpp
+++ 0395-longest-substring-with-at-least-k-repeating-characters/0395-longest-substring-with-at-least-k-repeating-characters.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-int solve(string s,int k){
-     int n=s.size();
+    int longestSubstring(string s, int k) {
+      int n=s.size();
      if(n==0) return 0;
      unordered_map<char,int>freq;
      for(auto ch:s){
@@ -9,14 +9,11 @@ int solve(string s,int k){
         }
         for(int i=0;i<n;i++){
             if(freq[s[i]]<k){
-                int left=solve(s.substr(0,i),k);
-                int right=solve(s.substr(i+1),k);
+                int left=longestSubstring(s.substr(0,i),k);
+                int right=longestSubstring(s.substr(i+1),k);
                  return max(left,right);
             }
         }
-        return n;
-}
-    int longestSubstring(string s, int k) {
-        return solve(s,k); 
+        return n; 
     }
 };
