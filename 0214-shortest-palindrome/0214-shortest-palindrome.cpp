83a0a382c260349ffd5a500d1109439edcd7fcdf
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-04-15 01:25:41 +0530

    Time: 2 ms (79.13%), Space: 15.5 MB (57.44%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.cpp
+++ 0214-shortest-palindrome/0214-shortest-palindrome.cpp
@@ -1,14 +1,33 @@
 class Solution {
 public:
+void computeLPS(string &pattern,vector<int>&LPS){
+    int m=pattern.size();
+    int len=0;
+    LPS[0]=0;
+    int i=1;
+    while(i<m){
+        if(pattern[i]==pattern[len]){
+            len++;
+            LPS[i]=len;
+            i++;
+        }else{
+            if(len!=0){
+                len=LPS[len-1];
+            }else{
+                LPS[i]=0;
+                i++;
+            }
+        }
+    }
+}
+
     string shortestPalindrome(string s) {
         int n=s.size();
         string rev=s;
         reverse(rev.begin(),rev.end());
-        for(int i=0;i<n;i++){
-            if(memcmp(s.c_str()+0,rev.c_str()+i,n-i)==0){
-                return rev.substr(0,i)+s;
-            }
-        }
-        return rev+s;
+        string pattern=s+"_"+rev;
+        vector<int>LPS(pattern.size());
+        computeLPS(pattern,LPS);
+        return rev.substr(0,n-LPS[pattern.size()-1])+s;
     }
 };
