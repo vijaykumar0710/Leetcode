f81a1d93d3cf4f6f384900d255fbdd063c03cb59
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-04-14 15:24:07 +0530

    Time: 19 ms (66.83%), Space: 19.2 MB (34.15%) - LeetHub

--- 3245-find-beautiful-indices-in-the-given-array-i/3245-find-beautiful-indices-in-the-given-array-i.cpp
+++ 3245-find-beautiful-indices-in-the-given-array-i/3245-find-beautiful-indices-in-the-given-array-i.cpp
@@ -1,59 +1,69 @@
 class Solution {
 public:
-    vector<int> beautifulIndices(string s, string a, string b, int k) {
-        vector<int> posA = kmpSearch(s, a);
-        vector<int> posB = kmpSearch(s, b);
-        vector<int> res;
-
-        int i = 0, j = 0;
-        while (i < posA.size()) {
-            while (j < posB.size() && posB[j] < posA[i] - k) j++;
-            int temp = j;
-            while (temp < posB.size() && posB[temp] <= posA[i] + k) {
-                res.push_back(posA[i]);
-                break;
-            }
+void computeLPS(string &pattern,int m,vector<int>&LPS){
+    int len=0;
+    LPS[0]=0;
+    int i=1;
+    while(i<m){
+        if(pattern[i]==pattern[len]){
+            len++;
+            LPS[i]=len;
             i++;
-        }
-
-        sort(res.begin(), res.end());
-        return res;
-    }
-
-    // KMP prefix function
-    vector<int> kmpSearch(string& text, string& pattern) {
-        int n = text.size(), m = pattern.size();
-        vector<int> lps(m, 0), res;
-        int len = 0, i = 1;
-
-        // Build LPS array
-        while (i < m) {
-            if (pattern[i] == pattern[len]) {
-                lps[i++] = ++len;
-            } else if (len > 0) {
-                len = lps[len - 1];
-            } else {
-                lps[i++] = 0;
-            }
-        }
-
-        // Search using LPS
-        i = 0;
-        int j = 0;
-        while (i < n) {
-            if (text[i] == pattern[j]) {
-                i++; j++;
-                if (j == m) {
-                    res.push_back(i - j);
-                    j = lps[j - 1];
-                }
-            } else if (j > 0) {
-                j = lps[j - 1];
-            } else {
+        }else{
+            if(len!=0){
+                len=LPS[len-1];
+            }else{
+                LPS[i]=0;
                 i++;
             }
         }
+    }
+}
 
+void computeKMP(string &txt,string &pattern,vector<int>&LPS,vector<int>&vec){
+    int n=txt.size(),m=pattern.size();
+    int i=0,j=0;
+    while(i<n){
+        if(txt[i]==pattern[j]){
+            i++;
+            j++;
+        }
+        if(j==m){
+            vec.push_back(i-m);
+            j=LPS[j-1];
+        }else if(i<n && pattern[j]!=txt[i]){
+            if(j!=0){
+                j=LPS[j-1];
+            }else{
+                i++;
+            }
+        }
+    }
+}
+    vector<int> beautifulIndices(string s, string a, string b, int k) {
+        int size=s.size();
+        int asize=a.size();
+        int bsize=b.size();
+
+        vector<int>aLPS(asize),bLPS(bsize);
+        computeLPS(a,asize,aLPS);
+        computeLPS(b,bsize,bLPS);
+
+        vector<int>aKMP,bKMP;
+        computeKMP(s,a,aLPS,aKMP);
+        computeKMP(s,b,bLPS,bKMP);
+
+        vector<int>res;
+        for(int i=0;i<aKMP.size();i++){
+            int left=aKMP[i]-k;
+            int right=aKMP[i]+k;
+
+            auto it=lower_bound(bKMP.begin(),bKMP.end(),left);
+
+            if(it!=bKMP.end() && *it<=right){
+                res.push_back(aKMP[i]);
+            }
+        }
         return res;
     }
 };
