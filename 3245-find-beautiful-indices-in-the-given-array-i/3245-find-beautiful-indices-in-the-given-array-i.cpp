12640548bbaa8b6b25e3d0672fd482b7dd20f112
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-04-14 13:28:06 +0530

    Time: 8 ms (88.7%), Space: 19 MB (54.05%) - LeetHub

--- 3245-find-beautiful-indices-in-the-given-array-i/3245-find-beautiful-indices-in-the-given-array-i.cpp
+++ 3245-find-beautiful-indices-in-the-given-array-i/3245-find-beautiful-indices-in-the-given-array-i.cpp
@@ -1,35 +1,59 @@
 class Solution {
 public:
     vector<int> beautifulIndices(string s, string a, string b, int k) {
-        int size = s.size();
-        int asize = a.size();
-        int bsize = b.size();
-        
-        vector<int> sti, stj;
-
-        // Find all positions of substring a
-        for (int i = 0; i <= size - asize; ++i) {
-            if (s.compare(i, asize, a) == 0) {
-                sti.push_back(i);
-            }
-        }
-
-        // Find all positions of substring b
-        for (int i = 0; i <= size - bsize; ++i) {
-            if (s.compare(i, bsize, b) == 0) {
-                stj.push_back(i);
-            }
-        }
-
+        vector<int> posA = kmpSearch(s, a);
+        vector<int> posB = kmpSearch(s, b);
         vector<int> res;
-     for(int i=0;i<sti.size();i++){
-    int left=sti[i]-k;
-    int right=sti[i]+k;
-    auto it=lower_bound(stj.begin(),stj.end(),left);
-    if(it!=stj.end() && *it<=right){
-         res.push_back(sti[i]);
+
+        int i = 0, j = 0;
+        while (i < posA.size()) {
+            while (j < posB.size() && posB[j] < posA[i] - k) j++;
+            int temp = j;
+            while (temp < posB.size() && posB[temp] <= posA[i] + k) {
+                res.push_back(posA[i]);
+                break;
             }
-         }
+            i++;
+        }
+
+        sort(res.begin(), res.end());
+        return res;
+    }
+
+    // KMP prefix function
+    vector<int> kmpSearch(string& text, string& pattern) {
+        int n = text.size(), m = pattern.size();
+        vector<int> lps(m, 0), res;
+        int len = 0, i = 1;
+
+        // Build LPS array
+        while (i < m) {
+            if (pattern[i] == pattern[len]) {
+                lps[i++] = ++len;
+            } else if (len > 0) {
+                len = lps[len - 1];
+            } else {
+                lps[i++] = 0;
+            }
+        }
+
+        // Search using LPS
+        i = 0;
+        int j = 0;
+        while (i < n) {
+            if (text[i] == pattern[j]) {
+                i++; j++;
+                if (j == m) {
+                    res.push_back(i - j);
+                    j = lps[j - 1];
+                }
+            } else if (j > 0) {
+                j = lps[j - 1];
+            } else {
+                i++;
+            }
+        }
+
         return res;
     }
 };
