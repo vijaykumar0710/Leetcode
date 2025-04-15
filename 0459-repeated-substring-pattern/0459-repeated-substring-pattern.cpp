f89bbdbb324c4ee39278dfa28dfcab79b3c01bc5
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-04-15 17:48:24 +0530

    Time: 48 ms (37.58%), Space: 94.7 MB (23.73%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
@@ -1,31 +1,19 @@
 class Solution {
 public:
-void computeLPS(string &s,vector<int>&LPS){
-    int n=s.size();
-    LPS[0]=0;
-    int len=0;
-    int i=1;
-    while(i<n){
-        if(s[i]==s[len]){
-            len++;
-            LPS[i]=len;
-            i++;
-        }else{
-            if(len!=0){
-                len = LPS[len - 1];
-            }else{
-                LPS[i]=0;
-                i++;
+    bool repeatedSubstringPattern(string s) {
+        int n=s.size();
+        for(int l=1;l<=n/2;l++){
+            if(n%l==0){ 
+            int times=n/l;
+            string append=s.substr(0,l);
+            string temp="";
+            while(times--){
+                 temp+=append;
+               }
+               if(temp==s)
+                 return true;
             }
         }
-    }
-}
-    bool repeatedSubstringPattern(string s) {
-       int n=s.size();
-       vector<int>LPS(n);
-       computeLPS(s,LPS);
-       int longestprefix=LPS[n-1];
-       if(longestprefix>0 && (n%(n-longestprefix))==0) return true;
         return false;
     }
 };
