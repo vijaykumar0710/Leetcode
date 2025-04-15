850eea8405e576093cb78b727f571f036163df14
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-04-16 01:35:58 +0530

    Time: 0 ms (100%), Space: 8.3 MB (3.09%) - LeetHub

--- 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
@@ -1,13 +1,32 @@
 class Solution {
 public:
+void binaryRepresentation(int num,vector<int>&vec){
+       if (num == 0) {
+            vec.push_back(0);
+            return;
+        }
+    while(num>1){
+        vec.push_back(num%2);
+        num/=2;
+    }
+    vec.push_back(1);
+    reverse(vec.begin(),vec.end());
+}
     int minBitFlips(int start, int goal) {
-        int n=start^goal;
-        int count=0;
-        for(int i=0;i<32;i++){
-            if((n&(1<<i))!=0){
-                count++;
-            }
-        }  
-        return count;
+        vector<int>vecs,vece;
+        binaryRepresentation(start,vecs);
+        binaryRepresentation(goal,vece);
+        int n=vecs.size(),m=vece.size(); 
+        int cnt=0;
+        int minLen=min(m,n);
+        for(int i=1;i<=minLen;i++){
+          if(vecs[n-i]!=vece[m-i])
+             cnt++;
+        }
+         for (int i = minLen + 1; i <= max(m, n); i++) {
+            if (m > n) cnt += vece[m - i];
+            else cnt += vecs[n - i];
+        }
+        return cnt;
     }
 };
