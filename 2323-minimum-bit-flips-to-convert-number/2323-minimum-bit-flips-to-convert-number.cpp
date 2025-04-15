a04ab5dd769e7adc98c556dc8bce2af5a4a8614d
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-04-16 01:51:51 +0530

    Time: 0 ms (100%), Space: 8.1 MB (25.27%) - LeetHub

--- 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
@@ -1,30 +1,6 @@
 class Solution {
 public:
-void binaryRepresentation(int num,vector<int>&vec){
-       if (num == 0) {
-            vec[0]=0;
-            return;
-        }
-        int i=0;
-    while(num>1){
-        vec[i]=(num%2);
-        i++;
-        num/=2;
-    }
-    vec[i]=1;
-    reverse(vec.begin(),vec.end());
-}
     int minBitFlips(int start, int goal) {
-        vector<int>vecs(65,0),vece(65,0);
-        binaryRepresentation(start,vecs);
-        binaryRepresentation(goal,vece); 
-        int cnt=0;
-        int i=64;
-        while(i>=0){
-          if(vecs[i]!=vece[i])
-             cnt++;
-             i--;
-        }
-        return cnt;
+        return __builtin_popcount(start^goal);
     }
 };
