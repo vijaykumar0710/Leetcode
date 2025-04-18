9de38598ed2038cb289c5128f6c60e1e5ad112c7
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-04-18 16:33:49 +0530

    Time: 3 ms (83.88%), Space: 10 MB (61.25%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -3,19 +3,19 @@ public:
     string countAndSay(int n) {
         if(n==1) return "1";
         string str=countAndSay(n-1);
-        string ztr="";
+        string res="";
         int freq=1;
         char ch=str[0];
         for(int i=1;i<str.size();i++){
-            if(str[i]==ch){
-                freq++;
-            }else{
-                ztr+=(to_string(freq)+ch);
-                freq=1;
-                ch=str[i];
-            }
+            if(str[i]==ch){ 
+               freq++;
+            } else{
+            res+=(to_string(freq)+ch);
+            freq=1;
+            ch=str[i];
+           }
         }
-        ztr+=(to_string(freq)+ch);
-        return ztr;
+        res+=(to_string(freq)+ch);
+        return res;
     }
 };
