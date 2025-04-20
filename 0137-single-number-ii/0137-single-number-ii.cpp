dd26f3abb65f9eecae6886bfee2c663b439851da
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-04-21 02:35:12 +0530

    Time: 0 ms (100%), Space: 13.4 MB (38.4%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -4,11 +4,10 @@ public:
         int res=0;
         for(int k=0;k<=31;k++){
             int temp=(1<<k);
-            int zeros=0,ones=0;
+            int ones=0;
             for(auto &num:nums){
                 if((num & temp)!=0)
                    ones++;
-                else zeros++;
             }
             if(ones%3==1){
                 res=(res|temp);
