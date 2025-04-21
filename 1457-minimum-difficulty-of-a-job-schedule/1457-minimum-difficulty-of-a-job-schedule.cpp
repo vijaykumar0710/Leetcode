8a6962d144602287e557ea333cf66e553900d8e0
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-04-22 02:10:04 +0530

    Time: 16 ms (68.14%), Space: 10.3 MB (86.96%) - LeetHub

--- 1457-minimum-difficulty-of-a-job-schedule/1457-minimum-difficulty-of-a-job-schedule.cpp
+++ 1457-minimum-difficulty-of-a-job-schedule/1457-minimum-difficulty-of-a-job-schedule.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
-int dfs(int i, int d, vector<int>& jobs, vector<vector<int>>& dp) {
+int t[301][11];
+int dfs(int i, int d, vector<int>& jobs) {
     if (d == 0 && i == jobs.size()) return 0;
     if (d == 0 || i == jobs.size()) return 1e9;
-    if (dp[i][d] != -1) return dp[i][d];
+    if (t[i][d] != -1) return t[i][d];
 
     int maxJob = 0, res = 1e9;
 
     for (int j = i; j <= jobs.size() - d; j++) {
         maxJob = max(maxJob, jobs[j]);
-        res = min(res, maxJob + dfs(j + 1, d - 1, jobs, dp));
+        res = min(res, maxJob + dfs(j + 1, d - 1, jobs));
     }
 
-    return dp[i][d] = res;
+    return t[i][d] = res;
 }
     int minDifficulty(vector<int>& jobDifficulty, int d) {
     int n = jobDifficulty.size();
     if (n < d) return -1;
-    vector<vector<int>> dp(n + 1, vector<int>(d + 1, -1));
-    return dfs(0, d, jobDifficulty, dp);
+    memset(t,-1,sizeof(t));
+    return dfs(0, d, jobDifficulty);
     }
 };
