f1a8d1ba2a2864e2383439f95c7250ca76c03601
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-04-29 17:17:27 +0530

    Time: 0 ms (100%), Space: 9.7 MB (53.24%) - LeetHub

--- 3811-reverse-degree-of-a-string/3811-reverse-degree-of-a-string.cpp
+++ 3811-reverse-degree-of-a-string/3811-reverse-degree-of-a-string.cpp
@@ -3,7 +3,7 @@ public:
     int reverseDegree(string s) {
         int sum=0;
     for(int i=0;i<s.size();i++){
-        sum+=((123-s[i])*(i+1));
+        sum+=(('z'+1-s[i])*(i+1));
        }
        return sum;
     }
