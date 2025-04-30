e67a60dcb299aeedb62b128cac2520d884583e64
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-04-30 10:26:38 +0530

    Time: 1 ms (57.27%), Space: 9.5 MB (28.82%) - LeetHub

---
+++ 2886-faulty-keyboard/2886-faulty-keyboard.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string finalString(string s) {
+      string res="";
+      for(auto ch:s){
+        if(ch=='i'){
+            reverse(res.begin(),res.end());
+        }else{
+            res+=ch;
+        }
+      }
+      return res;
+    }
+};
