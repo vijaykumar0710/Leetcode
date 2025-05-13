7f60dba625b83021381e23fa03c37422cea0890c
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-05-13 20:38:25 +0530

    Time: 750 ms (47.78%), Space: 494.7 MB (18.72%) - LeetHub

---
+++ 3629-total-characters-in-string-after-transformations-i/3629-total-characters-in-string-after-transformations-i.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+int M=1e9+7;
+    int lengthAfterTransformations(string s, int t) {
+        vector<int>mp(26,0);
+        for(auto &ch:s){
+            mp[ch-'a']++;
+        }
+        for(int i=0;i<t;i++){
+            vector<int>temp(26,0);
+            for(int i=0;i<26;i++){
+                char ch=i+'a';
+                int freq=mp[i];
+                if(ch!='z'){
+                    temp[(ch+1)-'a']=(temp[(ch+1)-'a']+freq)%M;
+                }else{
+                    temp['a'-'a']=(temp['a'-'a']+freq)%M;
+                    temp['b'-'a']=(temp['b'-'a']+freq)%M;
+                }
+            }
+            mp=move(temp);
+        }
+  int res=0;
+  for(int i=0;i<26;i++){
+    res=(res+mp[i])%M;
+  }
+  return res;
+    }
+};
