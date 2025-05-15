325703a421bc115ba9c34b826c82fe87f6855d73
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-05-15 20:05:02 +0530

    Time: 2 ms (3.92%), Space: 18.1 MB (20.13%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-int findPivot(vector<int>& nums){
+   int findPivot(vector<int>& nums){
         int n = nums.size();
         int l = 0, r = n - 1;
 
@@ -16,26 +16,24 @@ int findPivot(vector<int>& nums){
         }
              return r;
     }
+
     bool search(vector<int>& nums, int target) {
         int n = nums.size();
         int pivotIdx = findPivot(nums);
-        cout<<pivotIdx;
-        if(nums[pivotIdx]==target) return true;
-        int idx = -1;
 
+        // Direct hit
+        if (nums[pivotIdx] == target) return true;
+
+        // Search in left half
         auto lo1 = lower_bound(nums.begin(), nums.begin() + pivotIdx, target);
-        if (lo1 != nums.begin() + pivotIdx && *lo1 == target) {
-            idx = lo1 - nums.begin();
-        }
+        if (lo1 != nums.begin() + pivotIdx && *lo1 == target)
+            return true;
 
-        if (idx == -1) {
-            auto lo2 = lower_bound(nums.begin() + pivotIdx+1, nums.end(), target);
-            if (lo2 != nums.end() && *lo2 == target) {
-                idx = lo2 - nums.begin();
-            }
-        }
+        // Search in right half
+        auto lo2 = lower_bound(nums.begin() + pivotIdx, nums.end(), target);
+        if (lo2 != nums.end() && *lo2 == target)
+            return true;
 
-        if(idx==-1) return false;
-        return true;
+        return false;
     }
 };
