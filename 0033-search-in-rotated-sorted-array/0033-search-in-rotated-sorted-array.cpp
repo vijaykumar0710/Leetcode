b3870fef5665cb4e793e8f57e529e99cc7914909
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-05-15 19:26:47 +0530

    Time: 0 ms (100%), Space: 15.3 MB (33.23%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,42 +1,46 @@
 class Solution {
 public:
-  int findPivot(vector<int>& nums,int n){
-    int l=0,r=n-1;
-    while(l<r){
-        int mid=l+(r-l)/2;
-        if(nums[mid]>nums[r]){
-            l=mid+1;
-        }else{
-            r=mid;
-        }
-    }
-    return r;
-  }
+    int findPivot(vector<int>& nums){
+        int n = nums.size();
+        int l = 0, r = n - 1;
 
-  int binarySearch(int l,int r,vector<int>& nums,int target){
-      int idx=-1;
-      while(l<=r){
-        int mid=l+(r-l)/2;
-        if(nums[mid]==target){
-            idx=mid;
-            break;
-        }else if(nums[mid]>target){
-            r=mid-1;
-        }else{
-            l=mid+1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+             if (nums[l] <= nums[r]) return l;
+            // Handle edge cases safely
+            int next = (mid + 1) % n;
+            int prev = (mid - 1 + n) % n;
+
+            if (nums[mid] < nums[prev] && nums[mid] < nums[next])
+                return mid;
+            else if (nums[mid] >= nums[l])
+                l = mid + 1;
+            else if(nums[mid]<=nums[r])
+                r = mid - 1;
         }
-      }
-      return idx;
-  }
+        return -1;  // Fallback
+    }
+
     int search(vector<int>& nums, int target) {
-        int n=nums.size();
-        int pivot_idx=findPivot(nums,n);
-        int idx=binarySearch(0,pivot_idx-1,nums,target);
-        if(idx!=-1){
-            return idx;
-        }else{
-            idx=binarySearch(pivot_idx,n-1,nums,target);
+        int n = nums.size();
+        int pivotIdx = findPivot(nums);
+        if(nums[pivotIdx]==target) return pivotIdx;
+        int idx = -1;
+
+        // First part: from 0 to pivotIdx - 1
+        auto lo1 = lower_bound(nums.begin(), nums.begin() + pivotIdx, target);
+        if (lo1 != nums.begin() + pivotIdx && *lo1 == target) {
+            idx = lo1 - nums.begin();
         }
+
+        // Second part: from pivotIdx to n - 1
+        if (idx == -1) {
+            auto lo2 = lower_bound(nums.begin() + pivotIdx+1, nums.end(), target);
+            if (lo2 != nums.end() && *lo2 == target) {
+                idx = lo2 - nums.begin();
+            }
+        }
+
         return idx;
     }
 };
