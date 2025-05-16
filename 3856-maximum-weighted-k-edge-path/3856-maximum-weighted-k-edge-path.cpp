37608044993cff64ad1f131adb026e54b66dd06b
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-05-16 16:03:32 +0530

    Time: 1208 ms (25.19%), Space: 203.5 MB (61.59%) - LeetHub

--- 3856-maximum-weighted-k-edge-path/3856-maximum-weighted-k-edge-path.cpp
+++ 3856-maximum-weighted-k-edge-path/3856-maximum-weighted-k-edge-path.cpp
@@ -1,40 +1,33 @@
 class Solution {
 public:
-    unordered_map<string, int> memo;
-
-    int dfs(int u, int k, int sum, int t, unordered_map<int, vector<pair<int, int>>>& graph) {
-        if (k == 0) return (sum < t) ? sum : -1;
-
-        string key = to_string(u) + "," + to_string(k) + "," + to_string(sum);
-        if (memo.find(key) != memo.end()) return memo[key];
-
-        int maxSum = -1;
-            for (auto &[v, w] : graph[u]) {
-                if (sum + w < t) {
-                    int res = dfs(v, k - 1, sum + w, t, graph);
-                    if (res != -1) {
-                        maxSum = max(maxSum, res);
-                    }
-                }
-            }
-
-        return memo[key] = maxSum;
+unordered_map<string,int>memo;
+int dfs(int u,int k,int sum,int t,unordered_map<int,vector<pair<int,int>>>&adj){
+    if(k==0) return (sum<t)?sum:-1;
+   
+   string key=to_string(u)+","+to_string(k)+","+to_string(sum);
+    if(memo.find(key)!=memo.end()) return memo[key];
+    int maxSum=-1;
+    for(auto &[v,w]:adj[u]){
+        if((sum+w)<t){
+            int res=dfs(v,k-1,sum+w,t,adj);
+            if(res!=-1)
+                maxSum=max(maxSum,res);
+        }
     }
-
+    return memo[key]=maxSum;
+}
     int maxWeight(int n, vector<vector<int>>& edges, int k, int t) {
-       unordered_map<int, vector<pair<int,int>>> graph;
-        for (auto &edge : edges) {
-            int u = edge[0];
-            int v = edge[1];
-            int w = edge[2];
-            graph[u].push_back({v, w});
+        unordered_map<int,vector<pair<int,int>>>adj;
+        for(auto &edge:edges){
+            int u=edge[0];
+            int v=edge[1];
+            int w=edge[2];
+            adj[u].push_back({v,w});
         }
-
-        int ans = -1;
-        for (int i = 0; i < n; ++i) {
-            ans = max(ans, dfs(i, k, 0, t, graph));
+        int ans=-1;
+        for(int i=0;i<n;i++){//koi fix node nhi diya toh kisi bhi node se max mil skta hain
+          ans=max(ans,dfs(i,k,0,t,adj));
         }
-
         return ans;
     }
 };
