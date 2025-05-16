47c548ff8084d799f9010ee7d30d7f4d996a9513
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-05-16 13:13:09 +0530

    Time: 0 ms (100%), Space: 22.9 MB (42.22%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
-bool isValid(vector<int>& piles,  long long mid,  long long h, int n){
-   long long cnt=0;
- for(int i=0;i<n;i++){ 
- cnt+=piles[i]/mid;
- if(piles[i]%mid!=0)
-     cnt++;
- }
- return cnt<=h;
+bool isValid(vector<int>&piles,long long mid,long long h,int n){
+    long long hour=0;
+    for(auto &pile:piles){ 
+        hour+=pile/mid;
+        if(pile%mid!=0) hour++;
+    }
+    return hour<=h;
 }
     int minEatingSpeed(vector<int>& piles, int h) {
         int n=piles.size();
-          long long l=1,r=*max_element(begin(piles),end(piles));
-          long long res=0;
+        int l=1,r=*max_element(piles.begin(),piles.end());
+        int res=-1;
         while(l<=r){
-             long long mid=l+(r-l)/2;
+            int mid=l+(r-l)/2;
             if(isValid(piles,mid,h,n)){
                 res=mid;
                 r=mid-1;
-            }else{
-                l=mid+1;
-            }
-        } 
+            }else l=mid+1;
+        }
         return res;
     }
 };
