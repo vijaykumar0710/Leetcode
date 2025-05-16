ac79d51503d85873ab7ab6a47c2cf7ec24e6c3d5
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-05-16 15:27:31 +0530

    Time: 1176 ms (26.42%), Space: 203.7 MB (61.48%) - LeetHub

--- 3856-maximum-weighted-k-edge-path/3856-maximum-weighted-k-edge-path.cpp
+++ 3856-maximum-weighted-k-edge-path/3856-maximum-weighted-k-edge-path.cpp
@@ -1,29 +1,33 @@
 class Solution {
 public:
-unordered_map<string, int> memo;
+    unordered_map<string, int> memo;
 
-    int dfs(int u, int k, int sum, int t, vector<vector<pair<int, int>>>& graph) {
+    int dfs(int u, int k, int sum, int t, map<int, vector<pair<int, int>>>& graph) {
         if (k == 0) return (sum < t) ? sum : -1;
 
         string key = to_string(u) + "," + to_string(k) + "," + to_string(sum);
         if (memo.find(key) != memo.end()) return memo[key];
 
         int maxSum = -1;
-        for (auto &[v, w] : graph[u]) {
-            if (sum + w < t) {
-                int res = dfs(v, k - 1, sum + w, t, graph);
-                if (res != -1) {
-                    maxSum = max(maxSum, res);
+            for (auto &[v, w] : graph[u]) {
+                if (sum + w < t) {
+                    int res = dfs(v, k - 1, sum + w, t, graph);
+                    if (res != -1) {
+                        maxSum = max(maxSum, res);
+                    }
                 }
             }
-        }
 
         return memo[key] = maxSum;
     }
+
     int maxWeight(int n, vector<vector<int>>& edges, int k, int t) {
-    vector<vector<pair<int, int>>> graph(n);
-        for (auto &e : edges) {
-            graph[e[0]].push_back({e[1], e[2]});
+        map<int, vector<pair<int,int>>> graph;
+        for (auto &edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+            int w = edge[2];
+            graph[u].push_back({v, w});
         }
 
         int ans = -1;
