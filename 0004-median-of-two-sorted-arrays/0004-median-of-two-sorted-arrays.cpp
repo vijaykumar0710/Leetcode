eb7ed2f6e4b14a71201cd01cb6ddb1a0ef033498
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-05-19 09:44:23 +0530

    Time: 0 ms (100%), Space: 95.2 MB (62.53%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,20 +1,35 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        vector<int>temp;
-        for(int i=0;i<nums1.size();i++){
-            temp.push_back(nums1[i]);
+        if(nums1.size()>nums2.size()){
+            return findMedianSortedArrays(nums2,nums1);
         }
-        for(int i=0;i<nums2.size();i++){
-            temp.push_back(nums2[i]);
-        }
-        sort(temp.begin(),temp.end());
-        int n=temp.size();
-        if(n%2==1){
-            return temp[n/2];
-        }else{
-            return (temp[n/2]+temp[n/2-1])/2.0;
-        }
-        return -1;
+        int m=nums1.size(),n=nums2.size();
+        int l=0,r=m;
+      while(l<=r){
+        // left half
+        int px=l+(r-l)/2; // hum mid be barabar nums1 se element le rahe
+        int py=(m+n+1)/2-px; //(m+n+1)/2 = itne element ayyenge left half mein
+
+       // left half ka element
+        int x1=(px==0)?INT_MIN:nums1[px-1];
+        int x2=(py==0)?INT_MIN:nums2[py-1];
+
+        // right half ka element
+        int x3=(px==m)?INT_MAX:nums1[px];
+        int x4=(py==n)?INT_MAX:nums2[py];
+
+       if(x1<=x4 && x2<=x3){ 
+       if((m+n)%2==1){
+        return max(x1,x2);
+       }else{
+        return (max(x1,x2)+min(x3,x4))/2.0;
+       }
+       }
+       if(x1>x4){
+         r=px-1;
+          }else l=px+1;
+      }
+      return -1; 
     }
 };
