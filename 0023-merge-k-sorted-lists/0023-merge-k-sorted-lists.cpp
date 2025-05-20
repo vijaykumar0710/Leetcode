59fcce4733084ffe21b269da1ead55182bd6ce94
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-05-20 14:29:16 +0530

    Time: 99 ms (14.86%), Space: 18.5 MB (50.12%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -1,18 +1,25 @@
 class Solution {
 public:
-    ListNode* merge(ListNode* a, ListNode* b){
-        if(a==NULL) return b;
-        if(b==NULL) return a;
-        ListNode* result;
-        if(a->val<b->val){
-            result=a;
-            result->next=merge(a->next,b);
-        }else{
-            result=b;
-            result->next=merge(a,b->next);
-        }
-        return result;
+   ListNode* merge(ListNode* headA,ListNode* headB){
+    ListNode* dummy=new ListNode(-1);
+    ListNode* t=dummy,*t1=headA,*t2=headB;
+    // ListNode* t1=headA;
+    // ListNode* t2=headB;
+    while(t1!=NULL && t2!=NULL){ 
+    if(t1->val<t2->val){
+        t->next=t1;
+        t=t1;
+        t1=t1->next;
+    }else{
+        t->next=t2;
+        t=t2;
+        t2=t2->next;
+      }
     }
+    if(t1) t->next=t1;
+    else t->next=t2;
+    return dummy->next;
+}
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         int n=lists.size();
         if(n==0) return NULL;
