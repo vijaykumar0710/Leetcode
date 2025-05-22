f62c29a6d6fb812f0296fa324bc3e308e0e95c85
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-05-22 13:46:06 +0530

    Time: 830 ms (27.62%), Space: 11.8 MB (25.22%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,35 +1,31 @@
 class Solution {
 public:
-int m,n;
-vector<vector<int>> directions{{1,0},{-1,0},{0,1},{0,-1}};
-  bool find(vector<vector<char>>& board,int i,int j,int idx,string word){
-    if(idx==word.length()) return true;
+vector<vector<int>>dirs={{0,1},{0,-1},{1,0},{-1,0}};
+bool solve(vector<vector<char>>& board, string word,int row,int col,int idx,int m,int n,vector<vector<bool>>&visited){
+if(idx==word.size()) return true;
+if(row<0 || row>=m || col<0 || col>=n || visited[row][col] || board[row][col]!=word[idx]) return false;
 
-    if(i<0 || j<0 || i>=m || j>=n || board[i][j]=='$') return false;
+visited[row][col]=true;
 
-    if(board[i][j]!=word[idx]) return false;
-
-   char temp=board[i][j];
-    board[i][j]='$';
-
-    for(auto &dir:directions){
-        int new_i=i+dir[0];
-        int new_j=j+dir[1];
-
-        if(find(board,new_i,new_j,idx+1,word))
+for(auto &dir:dirs){
+    int n_r=row+dir[0];
+    int n_c=col+dir[1];
+    if(solve(board,word,n_r,n_c,idx+1,m,n,visited))
         return true;
-    }
-    board[i][j]=temp;
-    return false;
   }
+visited[row][col]=false;
+  return false;
+}
     bool exist(vector<vector<char>>& board, string word) {
-         m=board.size();
-         n=board[0].size();
-
+        int m=board.size();
+        int n=board[0].size();
+        vector<vector<bool>>visited(m,vector<bool>(n,false));
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(board[i][j]==word[0] && find(board,i,j,0,word))
-                return true;
+                if(board[i][j]==word[0]){ 
+                if(solve(board,word,i,j,0,m,n,visited))
+                   return true;
+                }
             }
         }
         return false;
