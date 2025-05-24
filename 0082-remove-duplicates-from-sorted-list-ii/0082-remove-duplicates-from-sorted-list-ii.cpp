ad1c8bcd60d7edd5124cf9410d117ebbdd5facb7
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-05-24 09:15:05 +0530

    Time: 0 ms (100%), Space: 15.7 MB (72.49%) - LeetHub

--- 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.cpp
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.cpp
@@ -2,26 +2,21 @@ class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
         if(!head || !head->next) return head;
-       unordered_map<int,int>mp;
-       ListNode *t=head;
-       while(t){
-          mp[t->val]++;
+        ListNode *dummy=new ListNode(-1);
+        dummy->next=head;
+        ListNode *t=head;
+        ListNode *j=dummy;
+        while(t){
+            if(t->next && t->val==t->next->val){ 
+            while(t->next!=NULL && t->val==t->next->val){
+                t=t->next;
+            }
+           j->next=t->next;
+          }else{
+            j=j->next;
+          }
           t=t->next;
-       }
-       ListNode *dummy=new ListNode(-1);
-       ListNode *j=dummy;
-       ListNode *t1=head;
-       while(t1){
-          if(mp[t1->val]==1){
-            j->next=t1;
-            j=t1;
-          }
-          if(t1->next==NULL){
-            j->next=NULL;
-            return dummy->next;
-          }
-          t1=t1->next;
-       }
-       return dummy->next;
+        }
+        return dummy->next;
     }
 };
