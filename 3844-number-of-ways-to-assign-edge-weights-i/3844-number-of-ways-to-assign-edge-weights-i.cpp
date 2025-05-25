842fe31106a8de7068c7617b93e221a388c28a0e
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-05-25 21:16:58 +0530

    Time: 571 ms (5.55%), Space: 397.6 MB (5.56%) - LeetHub

--- 3844-number-of-ways-to-assign-edge-weights-i/3844-number-of-ways-to-assign-edge-weights-i.cpp
+++ 3844-number-of-ways-to-assign-edge-weights-i/3844-number-of-ways-to-assign-edge-weights-i.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     long long M=1e9+7;
-    int dfs(int node, int parent, unordered_map<int, vector<int>>& adj) {
+    int dfs(int node, int parent, unordered_map<int, vector<int>>& adj, vector<bool>& visited) {
+    visited[node] = true;
     int max_depth = 0;
     for (int child : adj[node]) {
-        if (child != parent) {
-            max_depth = max(max_depth, dfs(child, node, adj));
+        if (!visited[child]) {
+            max_depth = max(max_depth, dfs(child, node, adj, visited));
         }
     }
-    return max_depth+1; 
+    return max_depth + 1;
 }
+
     long long modPower(long long base, long long exp, long long mod) {
     long long result = 1;
     base %= mod;
@@ -23,6 +25,7 @@ public:
     return result;
 }
     int assignEdgeWeights(vector<vector<int>>& edges) {
+        int m=edges.size();
         unordered_map<int,vector<int>>adj;
         for(auto &edge:edges){
             int u=edge[0];
@@ -30,7 +33,8 @@ public:
             adj[u].push_back(v);
             adj[v].push_back(u);
         }
-        int depth=dfs(1,-1,adj)-1;
+        vector<bool>visited(2*m+1,false);
+        int depth=dfs(1,-1,adj,visited)-1;
         cout<<depth;
         long long ans = modPower(2, depth - 1, M);
         return ans;
