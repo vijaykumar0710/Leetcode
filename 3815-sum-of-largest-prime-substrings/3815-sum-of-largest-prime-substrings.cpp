51f645439bd3f293031c60a988725aaa0bdb064e
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-05-27 01:09:51 +0530

    Time: 20 ms (16.71%), Space: 12.9 MB (15.31%) - LeetHub

--- 3815-sum-of-largest-prime-substrings/3815-sum-of-largest-prime-substrings.cpp
+++ 3815-sum-of-largest-prime-substrings/3815-sum-of-largest-prime-substrings.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
     bool isPrime(long long n) {
-        if (n <= 1) return false;
-        if (n <= 3) return true;
-        if (n % 2 == 0 || n % 3 == 0) return false;
-        for (long long i = 5; i * i <= n; i += 6) {
-            if (n % i == 0 || n % (i + 2) == 0)
-                return false;
-        }
-        return true;
+        if(n<=1) return false; 
+       for(long long factor=2;factor*factor<=n;factor++){
+        if(n%factor==0) return false;
+       }
+       return true;
     }
 
     long long sumOfLargestPrimes(string s) {
