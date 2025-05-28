04fdd59ce5a73e2794dc4708b53446f7cad7fd64
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-05-28 12:39:47 +0530

    Time: 547 ms (11.06%), Space: 206.2 MB (23.96%) - LeetHub

--- 3633-maximize-the-number-of-target-nodes-after-connecting-trees-i/3633-maximize-the-number-of-target-nodes-after-connecting-trees-i.cpp
+++ 3633-maximize-the-number-of-target-nodes-after-connecting-trees-i/3633-maximize-the-number-of-target-nodes-after-connecting-trees-i.cpp
@@ -1,16 +1,38 @@
 class Solution {
 public:
-  void dfs(unordered_map<int,vector<int>>&adj,int node,int step,int &cnt,int k,vector<bool>&visited){
-   if(step>k) return;
-   cnt++;
-   visited[node]=true;
-   for(auto &v:adj[node]){
-    if(!visited[v]){
-        dfs(adj,v,step+1,cnt,k,visited);
+vector<int> countReachableWithinK(unordered_map<int,vector<int>>& adj, int k) {
+    int n = adj.size();
+    vector<int> result(n,0);
+
+   if(k<0) return result;
+
+    for (int start = 0; start < n; start++) {
+        vector<bool> visited(n, false);
+        queue<pair<int, int>> q; // (node, steps)
+        visited[start] = true;
+        q.push({start, 0});
+
+        int count = 1; // count itself
+
+        while (!q.empty()) {
+            auto [node, steps] = q.front();
+            q.pop();
+
+            if (steps == k) continue;
+
+            for (int neighbor : adj[node]) {
+                if (!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    q.push({neighbor, steps + 1});
+                    count++;
+                }
+            }
+        }
+        result[start] = count;
     }
-   }
-   visited[node]=false;
-  }
+
+    return result;
+}
     vector<int> maxTargetNodes(vector<vector<int>>& edges1, vector<vector<int>>& edges2, int k) {
         unordered_map<int,vector<int>>adj1,adj2;
         for(auto &edge:edges1){
@@ -21,26 +43,8 @@ public:
            adj2[edge[0]].push_back(edge[1]);
            adj2[edge[1]].push_back(edge[0]);
         }
-        int n1=adj1.size(),n2=adj2.size();
-         vector<int>v1(n1,0),v2(n2,0);
-
-        for(int i=0;i<n1;i++){
-            vector<bool>visited(n1,false);
-            int step=0,cnt=0;
-            if(k<0) break;
-            dfs(adj1,i,step,cnt,k,visited);
-            v1[i]=cnt;
-        }
-
-          k--;
-         for(int i=0;i<n2;i++){
-            vector<bool>visited(n2,false);
-            int step=0,cnt=0;
-            if(k<0) break;
-            dfs(adj2,i,step,cnt,k,visited);
-            v2[i]=cnt;
-        }
-
+        vector<int>v1=countReachableWithinK(adj1,k);
+        vector<int>v2=countReachableWithinK(adj2,k-1);
         int maximum_v2=*max_element(v2.begin(),v2.end());
         for(int i=0;i<v1.size();i++){
             v1[i]+=maximum_v2;
