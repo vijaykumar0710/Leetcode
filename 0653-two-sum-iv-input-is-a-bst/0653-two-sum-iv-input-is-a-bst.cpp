02d4d01ded5b337ddbb988b5aede75fdc3e75c9d
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-05-30 09:47:01 +0530

    Time: 4 ms (42.89%), Space: 37.4 MB (39.97%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -1,21 +1,50 @@
+class BSTIterator {
+    stack<TreeNode*> st;
+    bool reverse; // false -> in-order, true -> reverse in-order
+
+public:
+    BSTIterator(TreeNode* root, bool isReverse) {
+        reverse = isReverse;
+        pushAll(root);
+    }
+
+    void pushAll(TreeNode* node) {
+        while (node) {
+            st.push(node);
+            node = reverse ? node->right : node->left;
+        }
+    }
+
+    int next() {
+        TreeNode* temp = st.top();
+        st.pop();
+        if (reverse) pushAll(temp->left);
+        else pushAll(temp->right);
+        return temp->val;
+    }
+
+    bool hasNext() {
+        return !st.empty();
+    }
+};
+
 class Solution {
 public:
     bool findTarget(TreeNode* root, int k) {
-        unordered_set<int>st;
-        queue<TreeNode*>q;
-       q.push(root);
-       while(!q.empty()){
-        TreeNode* node=q.front();
-        q.pop();
-        int x=node->val;
-        int y=k-x;
-        if(st.find(y)!=st.end()){ 
-           return true;
+        if (!root) return false;
+        
+        BSTIterator l(root, false); // in-order
+        BSTIterator r(root, true);  // reverse in-order
+
+        int i = l.next();
+        int j = r.next();
+
+        while (i < j) {
+            int sum = i + j;
+            if (sum == k) return true;
+            else if (sum < k) i = l.next();
+            else j = r.next();
         }
-        st.insert(x);
-        if(node->left) q.push(node->left);
-        if(node->right) q.push(node->right);
-       }
-       return false;
+        return false;
     }
 };
